ebcf9897f27df8592cbe2152ecd17f750a8bb230
Drew Dara-Abrams <drew@mapzen.com>
2014-10-29 14:26:47 -0400

    removing out-of-date annotations

--- app/serializers/stop_serializer.rb
+++ app/serializers/stop_serializer.rb
@@ -1,12 +1,3 @@
-#  id         :integer          not null, primary key
-#  onestop_id :string(255)
-#  geometry   :spatial          geometry, 4326
-#  codes      :string(255)      is an Array
-#  names      :string(255)      is an Array
-#  tags       :hstore
-#  created_at :datetime
-#  updated_at :datetime
-
 class StopSerializer < ApplicationSerializer
   attributes :onestop_id,
              :geometry,
