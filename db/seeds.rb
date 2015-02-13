bf91ea5914b83708ca6adb54a7ae42ea5ce9aa06
Drew Dara-Abrams <drew@mapzen.com>
2015-02-12 18:07:55 -0800

    seeds should no longer reference GTFS import

--- db/seeds.rb
+++
@@ -1,2 +0,0 @@
-Rake::Task['import_from_gtfs'].invoke(Rails.root.join('spec', 'support', 'example_gtfs_archives', 'sfmta_gtfs.zip'))
-Rake::Task['import_from_gtfs'].invoke(Rails.root.join('spec', 'support', 'example_gtfs_archives', 'vta_gtfs.zip'))
