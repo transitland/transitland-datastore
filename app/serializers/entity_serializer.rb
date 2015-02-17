fe7232483233a555f364167a6a0ef6bd7e16f247
Drew Dara-Abrams <drew@mapzen.com>
2015-02-17 14:59:47 -0800

    serializers should include changeset info

--- app/serializers/entity_serializer.rb
+++ app/serializers/entity_serializer.rb
@@ -1,3 +1,7 @@
 class EntitySerializer < ApplicationSerializer
+  attributes :current,
+             :created_or_updated_in_changeset_id,
+             :destroyed_in_changeset_id
+
   has_many :identifiers
 end
