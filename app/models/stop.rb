3e13db07a3d8674f1afe438330cf044fb58cf43c
Drew Dara-Abrams <drew@mapzen.com>
2015-02-18 16:55:10 -0800

    removing Stop.match_against_existing_or_initialize

--- app/models/stop.rb
+++ app/models/stop.rb
@@ -33,20 +33,6 @@ class Stop < ActiveRecord::Base
   has_many :operators_serving_stop, dependent: :destroy
   has_many :operators, through: :operators_serving_stop
 
-  def self.match_against_existing_or_initialize(attrs)
-    if attrs.has_key?(:onestop_id) && attrs[:onestop_id].present?
-      # TODO: update?
-      return Stop.find_or_create_by(onestop_id: attrs[:onestop_id])
-    end
-    radius = 5 # TODO: move to config
-    existing_stops = Stop.select{['stops.*', st_distance(geometry, attrs[:geometry]).as(distance)]}.where{st_dwithin(stops.geometry, attrs[:geometry], radius) & (name == attrs[:name]) }.order('distance')
-    if existing_stops.count == 0
-      return Stop.new(attrs)
-    else
-      return existing_stops.first
-    end
-  end
-
   before_save :clean_attributes
 
   private
--- spec/models/stop_spec.rb
+++ spec/models/stop_spec.rb
@@ -52,39 +52,6 @@ describe Stop do
     end
   end
 
-  context 'match_against_existing_or_initialize' do
-    it 'if stop with same onestop_id already exists, returns that' do
-      stop_with_same_onestop_id = create(:stop)
-      stop_with_different_onestop_id = create(:stop)
-      returned_stop = Stop.match_against_existing_or_initialize({
-        onestop_id: stop_with_same_onestop_id.onestop_id,
-      })
-      expect(returned_stop.id).to eq stop_with_same_onestop_id.id
-    end
-
-    it 'if stop with same name exists within 10 meters, returns that' do
-      stop = create(:stop, geometry: 'POINT(-122.1646481752 37.4431429028)', name: 'University Ave. Marguerite')
-      other_stop = create(:stop, geometry: 'POINT(-122.1653 37.4436)', name: 'University Ave. Caltrain Northbound')
-      returned_stop = Stop.match_against_existing_or_initialize({
-        geometry: 'POINT(-122.1646106243 37.4431386436)',
-          name: 'University Ave. Marguerite'
-      })
-      expect(returned_stop.id).to eq stop.id
-    end
-
-    it 'if no similar stop exists, creates and returns a new one' do
-      stop = create(:stop, geometry: 'POINT(-122.1646481752 37.4431429028)', name: 'University Ave. Marguerite')
-      returned_stop = Stop.match_against_existing_or_initialize({
-        onestop_id: 's-3v5-Fake',
-        geometry: 'POINT(-122.1653 37.4436)',
-          name: 'University Ave. Caltrain Northbound'
-      })
-      returned_stop.save!
-      expect(Stop.count).to eq 2
-      expect(returned_stop.id).to be > stop.id
-    end
-  end
-
   context 'diff_against' do
     pending 'write some specs'
   end
