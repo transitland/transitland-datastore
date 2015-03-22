46bef7c670e7bed2d1f77a91e071772617c6e208
Meghan <mmhade@gmail.com>
2015-03-22 16:59:49 -0700

    structure for styling table based on collection

--- public/js/views/grid.js
+++ public/js/views/grid.js
@@ -1,7 +1,7 @@
 var DeveloperPlayground = DeveloperPlayground || {};
 
 DeveloperPlayground.GridView = Backbone.View.extend({
-    el: '.backgrid-container#example-1-result',
+    el: '.backgrid-container',
 
     initialize:function(options){
         this.collection = options.collection;
@@ -10,34 +10,97 @@ DeveloperPlayground.GridView = Backbone.View.extend({
 
     render: function(model){
 
-        var columns = [{
-            name: "id", // The key of the model attribute
-            label: "ID", // The name to display in the header
-            editable: false, // By default every cell in a column is editable, but *ID* shouldn't be
-            // Defines a cell type, and ID is displayed as an integer without the ',' separating 1000s.
-            cell: Backgrid.IntegerCell.extend({
-              orderSeparator: ''
-            })
-          }, {
-            name: "name",
-            label: "Name",
-            editable: false,
-            // The cell type can be a reference of a Backgrid.Cell subclass, any Backgrid.Cell subclass instances like *id* above, or a string
-            cell: "string" // This is converted to "StringCell" and a corresponding class in the Backgrid package namespace is looked up
-          }, {
-            name: "onestop_id",
-            label: "OneStop ID",
-            editable: false,
-            cell: "string" // An integer cell is a number cell that displays humanized integers
-        }];
+        var columns;
+        var grid;
+        var $entitySelect = $('select.form-control#entity');
 
-        var grid = new Backgrid.Grid({
+
+        if ($entitySelect.val() == "operators"){
+
+            columns = [{
+                name: "id", // The key of the model attribute
+                label: "ID", // The name to display in the header
+                editable: false, // By default every cell in a column is editable, but *ID* shouldn't be
+                // Defines a cell type, and ID is displayed as an integer without the ',' separating 1000s.
+                cell: Backgrid.IntegerCell.extend({
+                  orderSeparator: ''
+                })
+              }, {
+                name: "name",
+                label: "Operator name",
+                editable: false,
+                // The cell type can be a reference of a Backgrid.Cell subclass, any Backgrid.Cell subclass instances like *id* above, or a string
+                cell: "string" // This is converted to "StringCell" and a corresponding class in the Backgrid package namespace is looked up
+              }, {
+                name: "onestop_id",
+                label: "OneStop ID",
+                editable: false,
+                cell: "string" // An integer cell is a number cell that displays humanized integers
+            }];
+            grid = new Backgrid.Grid({
             columns: columns,
             collection: this.collection
-        });
-        // Render the grid and attach the root to your HTML document
-        // $("#example-1-result").append(grid.render().el);
-        $("#example-1-result").append(grid.render().$el);
+            });
+            // Render the grid and attach the root to your HTML document
+            $("#results").append(grid.render().$el);
+        } else if ($entitySelect.val() == "stops"){
+            columns = [{
+                name: "id", // The key of the model attribute
+                label: "ID", // The name to display in the header
+                editable: false, // By default every cell in a column is editable, but *ID* shouldn't be
+                // Defines a cell type, and ID is displayed as an integer without the ',' separating 1000s.
+                cell: Backgrid.IntegerCell.extend({
+                  orderSeparator: ''
+                })
+              }, {
+                name: "name",
+                label: "Stop name",
+                editable: false,
+                // The cell type can be a reference of a Backgrid.Cell subclass, any Backgrid.Cell subclass instances like *id* above, or a string
+                cell: "string" // This is converted to "StringCell" and a corresponding class in the Backgrid package namespace is looked up
+              }, {
+                name: "onestop_id",
+                label: "OneStop ID",
+                editable: false,
+                cell: "string" // An integer cell is a number cell that displays humanized integers
+            }];
+
+            grid = new Backgrid.Grid({
+            columns: columns,
+            collection: this.collection
+            });
+            // Render the grid and attach the root to your HTML document
+            $("#results").append(grid.render().$el);
+        } else if ($entitySelect.val() == "routes"){
+            columns = [{
+                name: "id", // The key of the model attribute
+                label: "ID", // The name to display in the header
+                editable: false, // By default every cell in a column is editable, but *ID* shouldn't be
+                // Defines a cell type, and ID is displayed as an integer without the ',' separating 1000s.
+                cell: Backgrid.IntegerCell.extend({
+                  orderSeparator: ''
+                })
+              }, {
+                name: "name",
+                label: "Route name",
+                editable: false,
+                // The cell type can be a reference of a Backgrid.Cell subclass, any Backgrid.Cell subclass instances like *id* above, or a string
+                cell: "string" // This is converted to "StringCell" and a corresponding class in the Backgrid package namespace is looked up
+              }, {
+                name: "onestop_id",
+                label: "OneStop ID",
+                editable: false,
+                cell: "string" // An integer cell is a number cell that displays humanized integers
+            }];
+
+            grid = new Backgrid.Grid({
+            columns: columns,
+            collection: this.collection
+            });
+            // Render the grid and attach the root to your HTML document
+            $("#results").append(grid.render().$el);
+        }
+
     },
 
     close: function() {
