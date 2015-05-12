8a66a5707df9e6ff8853a1eb5af2a213ab03fbcc
Ian Rees <ian@ianrees.net>
2015-05-12 11:44:53 -0700

    Use download(verify=True)

--- lib/feedeater/fetch.py
+++ lib/feedeater/fetch.py
@@ -7,20 +7,24 @@ import util
 
 def run():
   parser = util.default_parser('Fetch Transitland Feed Registry GTFS feeds')
+  parser.add_argument(
+    '--noverify', 
+    action='store_true',
+    help='Do not verify downloaded feed checksums', 
+  )
   args = parser.parse_args()
+  #
   r = transitland.registry.FeedRegistry(path=args.registry)
   feedids = args.feedids
   if args.all:
-    feedids = r.feeds()
+    feedids = sorted(r.feeds())
   if len(feedids) == 0:
     raise Exception("No feeds specified! Try --all")
+  #
   for feedid in feedids:
+    print feedid
     feed = r.feed(feedid)
-    if feed.download_check_cache():
-      print "Cached:", feed.filename()
-    else:
-      print "Downloading:", feed.url(), "->", feed.filename()
-      result = feed.download()
+    feed.download(verify=(not args.noverify))
     
 if __name__ == "__main__":
   run()
