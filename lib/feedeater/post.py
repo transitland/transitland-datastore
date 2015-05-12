8e77106fe9433c35f2fc2b961e947b55565f478e
Ian Rees <ian@ianrees.net>
2015-05-12 11:44:35 -0700

    FeedEater post to datastore

--- lib/feedeater/post.py
+++ lib/feedeater/post.py
@@ -7,6 +7,8 @@ import transitland.registry
 import transitland.entities
 import transitland.datastore
 
+import util
+
 def run():
   parser = util.default_parser('Datastore Updater')
   parser.add_argument('--apitoken',
@@ -18,19 +20,28 @@ def run():
     default=os.getenv('TRANSITLAND_DATASTORE_HOST') or 'http://localhost:3000'
   )
   args = parser.parse_args()
-  # Do Stuff
-  # onestopregistry = onestop.registry.OnestopRegistry(args.onestop)
-  # for filename in args.filenames:
-  #   print "===== %s: %s ====="%(args.feedid, filename)
-  #   feed = mzgtfs.feed.Feed(filename)
-  #   onestopfeed = onestop.entities.OnestopFeed.from_gtfs(feed, feedid=args.feedid)
-  #   updater = datastore.DatastoreUpdater(
-  #     args.host,
-  #     apitoken=args.apitoken,
-  #     debug=args.debug
-  #   )
-  #   for entity in sorted(onestopfeed.stops(), key=lambda x:x.onestop()):
-  #     updater.merge_entity(entity)
+  # 
+  r = transitland.registry.FeedRegistry(path=args.registry)
+  feedids = args.feedids
+  if args.all:
+    feedids = r.feeds()
+  if len(feedids) == 0:
+    raise Exception("No feeds specified! Try --all")    
+  #
+  for feedid in args.feedids:
+    feed = r.feed(feedid)
+    gtfsfeed = mzgtfs.feed.Feed(feed.filename())
+    feed2 = transitland.entities.Feed.from_gtfs(
+      gtfsfeed,
+      feedid=feedid
+    )
+    updater = transitland.datastore.Datastore(
+      args.host,
+      apitoken=args.apitoken,
+      debug=True
+    )
+    for entity in sorted(feed2.operators(), key=lambda x:x.onestop()):
+      updater.update_operator(entity)
 
 if __name__ == "__main__":
   run()
