01c49ea716885bf3cb9a952f5557196f50829c2e
Ian Rees <ian@ianrees.net>
2015-05-14 01:10:01 -0700

    Rebirth of similarity module
    Includes various improvements as well.

---
+++ lib/feedeater/similarity.py
@@ -0,0 +1,104 @@
+"""Comparison utilities."""
+import math
+import difflib
+import collections
+
+import mzgeohash
+import util
+
+def seqratio(a, b):
+  """Sequence similarity between strings."""
+  # Use Python standard library SequenceMatcher,
+  # which is based on the Ratcliff-Obershelp algorithm, improved to
+  # ignore "junk" elements.
+  return difflib.SequenceMatcher(None, a, b).ratio()
+
+def score_distance(e1, e2):
+  """Score on the inverse distance."""
+  d = util.haversine(e1.point(), e2.point()) * 0.01
+  return 1/(d+1.0)
+
+def score_name(e1, e2):
+  """Score on name similarity."""
+  return seqratio(e1.name(), e2.name())
+
+def score_name_distance(e1, e2):
+  """Score on name and inverse distance, equally weighted."""
+  return score_distance(e1, e2) * 0.5 + score_name(e1, e2) * 0.5
+
+def filter_threshold(matches, threshold=0.5):
+  return filter(lambda x:x.score >= threshold, matches)
+
+class ScoreResult(object):
+  """Result of a comparison between two entities."""
+  def __init__(self, source, match=None, score=0.0):
+    self.source = source
+    self.match = match
+    self.score = score
+    
+  def print_match(self):
+    if self.match:
+      print "%s <-> %s (score: %0.2f, d: %0.2fm, ds: %0.2f, ss: %0.2f)"%(
+        self.source.name(), 
+        self.match.name(), 
+        self.score,
+        util.haversine(self.source.point(), self.match.point()),
+        score_distance(self.source, self.match),
+        score_name(self.source, self.match)
+      )
+    else:
+      print "%s <-> no result"%(self.source.name())
+
+class CompareEntities(object):
+  def __init__(self, entity, search_entities, score_func=None):
+    """Compare entities."""
+    self.entity = entity
+    self.search_entities = search_entities
+    self.score_func = score_func or score_name_distance    
+    self.result = []
+  
+  def find_search_entities(self):
+    """Find a set of entities to compare against."""
+    return self.search_entities
+
+  def score(self):
+    """Return the best matching entity."""  
+    results = []
+    for search_entity in self.find_search_entities():
+      result = ScoreResult(
+        self.entity, 
+        search_entity, 
+        self.score_func(self.entity, search_entity)
+      )
+      results.append(result)
+    self.results = results
+  
+  def merge(self, threshold=0.5):
+    """Merge the entity with the best result."""
+    results = sorted(self.results, key=lambda x:x.score, reverse=True)
+    if results:
+      best = results[0]
+    else:
+      best = ScoreResult(entity)
+    best.print_match()
+    
+    # Skip 100% matches for now...
+    idm = set()
+    if best.match:
+      idm = set(self.entity.identifiers()) & set(best.match.identifiers())
+    # if best.score == 1.0:
+    #   print "\tperfect match"
+    #   return self.entity
+    # elif best.score > threshold and idm:
+    #   print "\tid match"
+    #   return self.entity
+    if best.score > threshold:
+      print "\tscore above threshold, merging..."
+      # maintain relationships
+      # best.match.set_tag('foo','bar')
+      best.match.merge(self.entity)
+      self.entity.data = best.match.data
+    else:
+      print "...no match."
+    return self.entity
+
--- lib/feedeater/util.py
+++ lib/feedeater/util.py
@@ -1,6 +1,7 @@
 """FeedEater utilities."""
 import os
 import argparse
+import math
 
 def default_parser(description=None):
   parser = argparse.ArgumentParser(description=description)
@@ -30,4 +31,28 @@ def default_parser(description=None):
     type=int, 
     default=1
   )
+  parser.add_argument(
+    '--debug', 
+    help='Debug', 
+    action='store_true'
+  )
   return parser
+  
+def haversine(point1, point2):
+  """Haversine distance between two (lon,lat) points, in km."""
+  # Based on description of Haversine formula:
+  # http://www.movable-type.co.uk/scripts/latlong.html
+  radius = 6371000 # m
+  # Decimal to radians
+  lon1, lat1 = map(math.radians, point1)
+  lon2, lat2 = map(math.radians, point2)
+  # Haversine
+  dlon = lon2 - lon1
+  dlat = lat2 - lat1
+  a =  \
+    math.sin(dlat/2)**2 + \
+    math.cos(lat1) * \
+    math.cos(lat2) * \
+    math.sin(dlon/2)**2
+  c = 2 * math.atan2(a**0.5, (1-a)**0.5)
+  return c * radius
