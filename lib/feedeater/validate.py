c51bca6c0924288937ba0badc58ede2b208c297f
Ian Rees <ian@ianrees.net>
2015-05-18 03:15:31 -0700

    Use validation report system from mzgtfs

--- lib/feedeater/validate.py
+++ lib/feedeater/validate.py
@@ -4,76 +4,31 @@ import subprocess
 import argparse
 
 import mzgtfs.feed
+import mzgtfs.validation
 import transitland.registry
 
 import util
 
-class FeedEaterValidator(object):
-  def __init__(self, filename):
-    self.exceptions = []
-    self.filename = filename
-    
-  def errors(self):
-    # Filter for errors
-    return self.exceptions
-    
-  def warnings(self):
-    # Filter for warnings
-    return self.exceptions    
-    
-  def validate(self):
-    feed = mzgtfs.feed.Feed(self.filename)
-    print "Date ranges:", feed.dates()  
-    print "Internal validation..."
-    result = feed.validate()
-    print "...:", result
-    
-  def feedvalidator(self, report='report.html'):
-    self.exceptions = []
-    p = subprocess.Popen(
-      [
-        'feedvalidator.py',
-        '--memory_db',
-        '--noprompt',
-        '--output',
-        report,
-        self.filename
-      ],
-      stdout=subprocess.PIPE, 
-      stderr=subprocess.PIPE
-    )
-    stdout, stderr = p.communicate()
-    returncode = p.returncode
-    errors = [i for i in stdout.split('\n') if i.startswith('ERROR:')]
-    if returncode:
-      self.exceptions.append(Exception('Invalid feed'))
-
 def run():
   parser = util.default_parser('Validate GTFS')
   args = parser.parse_args()
-  #
+
+  # Registry
   r = transitland.registry.FeedRegistry(path=args.registry)
-  feedids = args.feedids
-  if args.all:
-    feedids = r.feeds()
-  if len(feedids) == 0:
-    raise Exception("No feeds specified! Try --all")
-  #
+
+  # Validate feeds
+  feedids = args.feedids or r.feeds()
   for feedid in feedids:
+    print "===== Feed: %s ====="%feedid
     feed = r.feed(feedid)
     filename = os.path.join(args.workdir, '%s.zip'%feed.onestop())
-    reportfilename = os.path.join(args.workdir, '%s.html'%feed.onestop())
-    # 
-    validator = FeedEaterValidator(filename)
-    validator.validate()
-    validator.feedvalidator(report=reportfilename)
-    if validator.errors():
-      print "Feed contains errors."
-    elif validator.warnings():
-      print "Feed contains warnings."
-    else:
-      print "Feed appears valid!"
-      
+    report = os.path.join(args.workdir, '%s.html'%feed.onestop())
+    print "Validating: %s"%filename
+    gtfsfeed = mzgtfs.feed.Feed(filename, debug=args.debug)    
+    validator = mzgtfs.validation.ValidationReport()
+    # gtfsfeed.validate(validator)
+    gtfsfeed.validate_feedvalidator(validator, report=report)
+    validator.report()
     
 if __name__ == "__main__":
   run()
