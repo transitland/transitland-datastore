3b72b772bfca35059ca747e54465a70ac27b7a6b
Ian Rees <ian@ianrees.net>
2015-05-18 03:16:12 -0700

    Cosmetic updates

--- lib/feedeater/post.py
+++ lib/feedeater/post.py
@@ -21,46 +21,57 @@ def run():
     default=os.getenv('TRANSITLAND_DATASTORE_HOST') or 'http://localhost:3000'
   )
   args = parser.parse_args()
-  # 
+
+  # Registry
   r = transitland.registry.FeedRegistry(path=args.registry)
-  feedids = args.feedids
-  if args.all:
-    feedids = r.feeds()
-  if len(feedids) == 0:
-    raise Exception("No feeds specified! Try --all")    
-  #
-  for feedid in args.feedids:
+  ds = transitland.datastore.Datastore(
+    args.host,
+    apitoken=args.apitoken,
+    debug=args.debug
+  )
+
+  # Update datastore.
+  feedids = args.feedids or r.feeds()
+  for feedid in feedids:
+    print "===== Feed: %s ====="%feedid
     infeed = r.feed(feedid)
     filename = args.filename or os.path.join(args.workdir, '%s.zip'%infeed.onestop())
+    print "Opening: %s"%filename
     gtfsfeed = mzgtfs.feed.Feed(filename)
+    print "Creating Onestop Entities"
     feed = transitland.entities.Feed.from_gtfs(
       gtfsfeed,
       feedid=feedid
     )
-    updater = transitland.datastore.Datastore(
-      args.host,
-      apitoken=args.apitoken,
-      debug=args.debug
-    )
-    # TODO:
-    # If two stops merge into the Onestop ID?
+
+    # Similarity search.
+    # TODO: If two stops merge into the Onestop ID?
+    for o in feed.operators():
+      o._cache_onestop()
     for stop in feed.stops():
-      search_stops = updater.stops(point=stop.point(), radius=1000)      
+      print "Similarity: %s"%stop.onestop()
+      search_stops = ds.stops(point=stop.point(), radius=1000)   
       s = similarity.CompareEntities(stop, search_stops)
       s.score()
-      s.merge()
-    # update with the merged entity...
+      s.merge(indent='  ')
+
+    # Post changesets.
     for operator in feed.operators():
-      entities = operator.stops() # | operator.routes()
-      # Note: Agencies must be created before routes/stops.
+      print "Updating operator: %s"%operator.onestop()
+      entities = sorted(
+        operator.stops() | operator.routes(), 
+        key=lambda x:x.onestop()
+      )
       # Post without relationships
-      updater.update_entity(operator, rels=False)
+      ds.update_entity(operator, rels=False)
       for entity in entities:
-        updater.update_entity(entity, rels=False)
+        print "  ... %s"%entity.onestop()
+        ds.update_entity(entity, rels=False)
       # Update relationships
-      updater.update_entity(operator)
+      ds.update_entity(operator)
       for entity in entities:
-        updater.update_entity(entity)
+        print "  ... %s"%entity.onestop()
+        ds.update_entity(entity)
 
 if __name__ == "__main__":
   run()
