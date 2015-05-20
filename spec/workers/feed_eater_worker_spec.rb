a6da4cb507a64ae2d656c499a9f0b2171cdd078d
Drew Dara-Abrams <drew@mapzen.com>
2015-05-19 17:05:57 -0700

    for now FeedEaterWorker spec will skip system calls to Python code

--- spec/workers/feed_eater_worker_spec.rb
+++ spec/workers/feed_eater_worker_spec.rb
@@ -2,6 +2,7 @@ describe FeedEaterWorker do
   it 'should pull the latest Transitland Feed Registry' do
     allow(TransitlandClient::FeedRegistry).to receive(:repo) { true }
     worker = FeedEaterWorker.new
+    allow(worker).to receive(:system) { true } # skip system calls to Python code
     worker.perform
     expect(TransitlandClient::FeedRegistry).to have_received(:repo)
   end
