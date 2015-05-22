4f3f7addd583f140ac41494590eed1bdf654e2b6
Ian Rees <ian@ianrees.net>
2015-05-22 02:09:34 -0700

    FeedEaterTask class

---
+++ lib/feedeater/task.py
@@ -0,0 +1,92 @@
+"""Base FeedEater Task."""
+import os
+import argparse
+
+import transitland.registry
+import transitland.datastore
+
+def default_parser(description=None, feedids=False):
+  parser = argparse.ArgumentParser(description=description)
+  if feedids:
+    parser.add_argument(
+      'feedids',
+      nargs='*',
+      help='Feed IDs'
+    )
+  else:
+    parser.add_argument(
+      'feedid', 
+      help='Feed ID'
+    )
+    
+  parser.add_argument(
+    '--registry', 
+    help='Feed registry path',
+    default=os.getenv('TRANSITLAND_FEED_REGISTRY_PATH')
+  )
+  parser.add_argument(
+    '--workdir',
+    help='Feed data directory',
+    default=os.getenv('TRANSITLAND_FEED_DATA_PATH')
+  )
+  parser.add_argument(
+    "--host",
+    help="Datastore host",
+    default=os.getenv('TRANSITLAND_DATASTORE_HOST') or 'http://localhost:3000'
+  )
+  parser.add_argument(
+    '--apitoken',
+    help='Datastore api token',
+    default=os.getenv('TRANSITLAND_DATASTORE_AUTH_TOKEN')
+  )
+  parser.add_argument(
+    '--debug', 
+    help='Debug', 
+    action='store_true'
+  )
+  parser.add_argument(
+    '--filename',
+    help='Specify GTFS filename manually'
+  )  
+  return parser
+
+class FeedEaterTask(object):
+  def __init__(
+      self,
+      filename=None,
+      feedid=None, 
+      feedids=None,
+      registry=None,
+      workdir=None,
+      host=None,
+      apitoken=None,
+      debug=None
+    ):
+    assert registry
+    self.filename = filename
+    self.feedid = feedid
+    self.feedids = feedids
+    self.registry = transitland.registry.FeedRegistry(path=registry)
+    self.workdir = workdir or os.path.join(registry, 'data')
+    self.datastore = transitland.datastore.Datastore(
+      host,
+      apitoken=apitoken,
+      debug=debug
+    )
+    self.debug = debug
+    
+  @classmethod
+  def from_args(cls):
+    parser = cls.parser()
+    args = parser.parse_args()  
+    return cls(**vars(args))
+
+  @classmethod
+  def parser(cls):
+    return default_parser(cls.__doc__)
+    
+  def run(self):
+    pass
+    
+if __name__ == "__main__":
+  task = FeedEaterTask.from_args()
