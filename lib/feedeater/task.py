6c703b6cff7bdeb9d159127638f6ce76cb8e3cd2
Ian Rees <ian@ianrees.net>
2015-05-27 14:55:43 -0700

    Parser simplification

--- lib/feedeater/task.py
+++ lib/feedeater/task.py
@@ -8,60 +8,11 @@ import transitland.registry
 import transitland.datastore
 import logging
 
-def default_parser(description=None, feedids=False):
-  parser = argparse.ArgumentParser(description=description)
-  if feedids:
-    parser.add_argument(
-      'feedids',
-      nargs='*',
-      help='Feed IDs'
-    )
-  else:
-    parser.add_argument(
-      'feedid',
-      help='Feed ID'
-    )
-  parser.add_argument(
-    '--registry',
-    help='Feed registry path',
-    default=os.getenv('TRANSITLAND_FEED_REGISTRY_PATH')
-  )
-  parser.add_argument(
-    '--workdir',
-    help='Feed data directory',
-    default=os.getenv('TRANSITLAND_FEED_DATA_PATH')
-  )
-  parser.add_argument(
-    "--host",
-    help="Datastore host",
-    default=os.getenv('TRANSITLAND_DATASTORE_HOST') or 'http://localhost:3000'
-  )
-  parser.add_argument(
-    '--apitoken',
-    help='Datastore api token',
-    default=os.getenv('TRANSITLAND_DATASTORE_AUTH_TOKEN')
-  )
-  parser.add_argument(
-    '--debug',
-    help='Debug',
-    action='store_true'
-  )
-  parser.add_argument(
-    '--filename',
-    help='Specify GTFS filename manually'
-  )
-  parser.add_argument(
-    '--log',
-    help='Log file'
-  )
-  return parser
-
 class FeedEaterTask(object):
   def __init__(
       self,
       filename=None,
       feedid=None,
-      feedids=None,
       registry=None,
       workdir=None,
       host=None,
@@ -72,7 +23,6 @@ class FeedEaterTask(object):
     ):
     self.filename = filename
     self.feedid = feedid
-    self.feedids = feedids
     self.registry = transitland.registry.FeedRegistry(path=registry)
     self.workdir = workdir or os.path.join(self.registry.path, 'data')
     self.datastore = transitland.datastore.Datastore(
@@ -91,7 +41,6 @@ class FeedEaterTask(object):
     else:
       logger.setLevel(logging.INFO)
     if logfile:
-      # logfile = os.path.join(self.workdir, logfile)
       fh = logging.FileHandler(logfile)
     else:
       fh = logging.StreamHandler(sys.stdout)
@@ -108,7 +57,45 @@ class FeedEaterTask(object):
     return cls(**vars(args))
 
   def parser(self):
-    return default_parser(self.__doc__)
+    parser = argparse.ArgumentParser(description=self.__doc__)
+    parser.add_argument(
+      'feedid',
+      help='Feed IDs'
+    )
+    parser.add_argument(
+      '--registry',
+      help='Feed registry path',
+      default=os.getenv('TRANSITLAND_FEED_REGISTRY_PATH')
+    )
+    parser.add_argument(
+      '--workdir',
+      help='Feed data directory',
+      default=os.getenv('TRANSITLAND_FEED_DATA_PATH')
+    )
+    parser.add_argument(
+      "--host",
+      help="Datastore host",
+      default=os.getenv('TRANSITLAND_DATASTORE_HOST') or 'http://localhost:3000'
+    )
+    parser.add_argument(
+      '--apitoken',
+      help='Datastore api token',
+      default=os.getenv('TRANSITLAND_DATASTORE_AUTH_TOKEN')
+    )
+    parser.add_argument(
+      '--debug',
+      help='Debug',
+      action='store_true'
+    )
+    parser.add_argument(
+      '--filename',
+      help='Specify GTFS filename manually'
+    )
+    parser.add_argument(
+      '--log',
+      help='Log file'
+    )
+    return parser
 
   def debug(self, msg):
     self.logger.debug(msg)
