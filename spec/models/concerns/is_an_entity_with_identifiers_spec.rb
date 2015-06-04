e0177440a5a82a2de19302ca4dcde990224f4cdf
Drew Dara-Abrams <drew@mapzen.com>
2015-06-04 16:15:34 -0700

    identifier-starts-with queries

--- spec/models/concerns/is_an_entity_with_identifiers_spec.rb
+++ spec/models/concerns/is_an_entity_with_identifiers_spec.rb
@@ -58,4 +58,23 @@ describe IsAnEntityWithIdentifiers do
       expect(Stop.find_by_onestop_id!('s-9q8yt4b-1AvHoS').identifiers).to match_array ['gtfs://sfmta/53531']
     end
   end
+
+  context 'scopes' do
+    it 'with_identifier' do
+      # TODO
+    end
+
+    it 'with_identifier_or_name' do
+      # TODO
+    end
+
+    it 'with_identifier_starting_with' do
+      sfmta1 = create(:stop, geometry: 'POINT(-122.38588 37.7366)', name: 'Cashmere St & Whitney Young Cir', identifiers: ['gtfs://f-9q8y-sanfranciscomunicipaltransportationagency/s/3922'])
+      sfmta2 = create(:stop, geometry: 'POINT(-122.405021 37.708997)', name: 'Bay Shore Blvd & Sunnydale Ave', identifiers: ['gtfs://f-9q8y-sanfranciscomunicipaltransportationagency/s/7398'])
+      bart = create(:stop, geometry: 'POINT(-122.353165 37.936887)', name: 'Richmond', identifiers: ['gtfs://f-9q9-bayarearapidtransit/s/RICH'])
+
+      expect(Stop.with_identifer_starting_with('gtfs://f-9q8y-sanfranciscomunicipaltransportationagency/s/')).to match_array([sfmta1, sfmta2])
+      expect(Stop.with_identifer_starting_with('gtfs://')).to match_array([sfmta1, sfmta2, bart])
+    end
+  end
 end
