963e062d954d5d5d2700d77629d151426431e1a9
Ian Rees <ian@ianrees.net>
2015-06-24 16:06:33 -0700

    use before(:each)

--- spec/workers/feed_eater_worker_spec.rb
+++ spec/workers/feed_eater_worker_spec.rb
@@ -2,26 +2,28 @@ require 'sidekiq/testing'
 Sidekiq::Testing.fake!
 
 describe FeedEaterWorker do
-  feedids = ['f-9q9-caltrain', 'f-9q9-bayarearapidtransit']
-  feeds = feedids.map {|feedid| Feed.new(onestop_id:feedid)}    
+  before(:each) do
+    @feedids = ['f-9q9-caltrain', 'f-9q9-bayarearapidtransit']
+    @feeds = @feedids.map {|feedid| Feed.new(onestop_id:feedid)}
+  end
 
   it 'can take a one feed_onestop_id' do
     allow(FeedEaterWorker).to receive(:perform_async) { true }
-    FeedEaterWorker.perform_async(feedids.first)
+    FeedEaterWorker.perform_async(@feedids.first)
     Sidekiq::Worker.clear_all
   end
 
   it 'can take a multiple feed_onestop_ids' do
     allow(FeedEaterWorker).to receive(:perform_async) { true }
-    FeedEaterWorker.perform_async(feedids)
+    FeedEaterWorker.perform_async(@feedids)
     Sidekiq::Worker.clear_all
   end
 
   it 'spawns child jobs' do
     allow(Feed).to receive(:update_feeds_from_feed_registry) { true }
-    allow(Feed).to receive(:where) { feeds }
+    allow(Feed).to receive(:where) { @feeds }
     expect {
-      FeedEaterWorker.perform_async(feedids)
+      FeedEaterWorker.perform_async(@feedids)
     }.to change(FeedEaterWorker.jobs, :size).by(1)
     expect {
       FeedEaterWorker.drain
