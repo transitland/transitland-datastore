a868318c855e7f9c551b4c36f76fc67a23ee59e7
Ian Rees <ian@ianrees.net>
2015-06-23 21:49:50 -0700

    Basic tests for FeedEaterWorker

--- spec/workers/feed_eater_worker_spec.rb
+++ spec/workers/feed_eater_worker_spec.rb
@@ -1,3 +1,32 @@
+require 'sidekiq/testing'
+Sidekiq::Testing.fake!
+
 describe FeedEaterWorker do
-  pending 'write some specs'
+  feedids = ['f-9q9-caltrain', 'f-9q9-bayarearapidtransit']
+
+  it 'can take a one feed_onestop_id' do
+    allow(FeedEaterWorker).to receive(:perform_async) { true }
+    FeedEaterWorker.perform_async(feedids.first)
+    Sidekiq::Worker.clear_all
+  end
+
+  it 'can take a multiple feed_onestop_ids' do
+    allow(FeedEaterWorker).to receive(:perform_async) { true }
+    FeedEaterWorker.perform_async(feedids)
+    Sidekiq::Worker.clear_all
+  end
+
+  it 'spawns child jobs' do
+    feedids = ['f-9q9-caltrain', 'f-9q9-bayarearapidtransit']
+    feeds = feedids.map {|feedid| Feed.new(onestop_id:feedid)}    
+    allow(Feed).to receive(:update_feeds_from_feed_registry) { true }
+    allow(Feed).to receive(:where) { feeds }
+    expect {
+      FeedEaterWorker.perform_async(feedids)
+    }.to change(FeedEaterWorker.jobs, :size).by(1)
+    expect {
+      FeedEaterWorker.drain
+    }.to change(FeedEaterFeedWorker.jobs, :size).by(2)
+    Sidekiq::Worker.clear_all
+  end
 end
