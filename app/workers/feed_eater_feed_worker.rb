c92683c901bece1c840f4e3c0845fd91d27ea08b
Ian Rees <ian@ianrees.net>
2015-06-23 18:14:35 -0700

    More ruby-style

--- app/workers/feed_eater_feed_worker.rb
+++ app/workers/feed_eater_feed_worker.rb
@@ -9,23 +9,16 @@ class FeedEaterFeedWorker
     feed = Feed.find_by(onestop_id: feed_onestop_id)
     logger.info "FeedEaterFeedWorker #{feed_onestop_id}: Downloading #{feed.url}"
     updated = feed.fetch_and_check_for_updated_version
-    if !updated
-      logger.info "FeedEaterFeedWorker #{feed_onestop_id}: No change"
-      # return
-    end
-
-    # Create import record
-    feed_import = FeedImport.create(feed: feed)
+    return unless updated
 
     # Clear out old log files
     log_file_path = FeedEaterFeedWorker.artifact_file_path("#{feed_onestop_id}.log")
     validation_report_path = FeedEaterFeedWorker.artifact_file_path("#{feed_onestop_id}.html")
-    if File.exist?(log_file_path)
-      FileUtils.rm log_file_path
-    end
-    if File.exist?(validation_report_path)
-      FileUtils.rm validation_report_path
-    end
+    FileUtils.rm(log_file_path) if File.exist?(log_file_path)
+    FileUtils.rm(validation_report_path) if File.exist?(validation_report_path)
+
+    # Create import record
+    feed_import = FeedImport.create(feed: feed)
 
     # Validate and import feed
     begin
@@ -41,7 +34,8 @@ class FeedEaterFeedWorker
         UploadFeedEaterArtifactsToS3Worker.perform_async(feed_onestop_id)
       end
     rescue Exception => e
-      # NOTE: we're catching all exceptions, including Interrupt, SignalException, and SyntaxError
+      # NOTE: we're catching all exceptions, including Interrupt,
+      #   SignalException, and SyntaxError
       exception_log = "\n#{e}\n#{e.backtrace}\n"
       logger.error exception_log
       feed_import.update(success: false)
@@ -51,18 +45,16 @@ class FeedEaterFeedWorker
       feed.has_been_fetched_and_imported!(on_feed_import: feed_import)
     ensure
       # Cleanup
+      import_log = ''
       if File.exist?(log_file_path)
         import_log = File.open(log_file_path, 'r').read
-      else
-        import_log = ''
       end
       if exception_log.present?
         import_log << exception_log
       end
+      validation_report = nil
       if File.exist?(validation_report_path)
         validation_report = File.open(validation_report_path, 'r').read
-      else
-        validation_report = nil
       end
       # Save logs and reports
       logger.info "FeedEaterFeedWorker #{feed_onestop_id}: Saving log & report"
