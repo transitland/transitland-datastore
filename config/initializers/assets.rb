90b651cb39859aacdccc24472768cd3ae0d33314
Drew Dara-Abrams <drew@mapzen.com>
2015-07-01 17:05:05 -0700

    nest CSS and JS assets under /admin/assets
    they're not necessary anywhere else

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,6 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+# NOTE: the only assets we use are for the admin interface
+Rails.application.config.assets.prefix = '/admin/assets'
