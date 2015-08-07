9bfed34e713803c9987949391a5c1a45fe82b03b
Ian Rees <ian@ianrees.net>
2015-08-07 01:10:39 -0700

    Moved controller methods to scopes

--- app/controllers/api/v1/schedules_controller.rb
+++ app/controllers/api/v1/schedules_controller.rb
@@ -8,30 +8,25 @@ class Api::V1::SchedulesController < Api::V1::BaseApiController
 
     # Service on a date
     if params[:date].present?
-      date = Date.parse(params[:date])
-      d = date.strftime('%Y%m%d')
-      dw = date.strftime("%A").downcase
-      @ssps = @ssps.where("(service_start_date <= ? AND service_end_date >= ?) AND (service_#{dw} = true OR ? = ANY(service_added)) AND NOT (? = ANY(service_except))", d, d, d, d)
+      @ssps = @ssps.where_service_on_date(params[:date])
     end
     
     # Service between stops
     if params[:origin_onestop_id].present?
-      @ssps = @ssps.where(origin_id: Stop.find_by(onestop_id: params[:origin_onestop_id]).id)
+      @ssps = @ssps.where(origin_id: Stop.find_by!(onestop_id: params[:origin_onestop_id]).id)
     end
     if params[:destination_onestop_id].present?
-      @ssps = @ssps.where(destination_id: Stop.find_by(onestop_id: params[:destination_onestop_id]).id)
+      @ssps = @ssps.where(destination_id: Stop.find_by!(onestop_id: params[:destination_onestop_id]).id)
     end
     
     # Service on a route
     if params[:route_onestop_id].present?
-      @ssps = @ssps.where(route_id: Route.find_by(onestop_id: params[:route_onestop_id]).id)
+      @ssps = @ssps.where(route_id: Route.find_by!(onestop_id: params[:route_onestop_id]).id)
     end
 
     # Stops in a bounding box
-    if params[:bbox].present? && params[:bbox].split(',').length == 4
-      bbox_coordinates = params[:bbox].split(',')
-      stops = Stop.where{geometry.op('&&', st_makeenvelope(bbox_coordinates[0], bbox_coordinates[1], bbox_coordinates[2], bbox_coordinates[3], Stop::GEOFACTORY.srid))}
-      @ssps = @ssps.where(origin_id: stops.ids)      
+    if params[:bbox].present?
+      @ssps = @ssps.where_origin_bbox(params[:bbox])
     end
 
     per_page = params[:per_page].blank? ? ScheduleStopPair::PER_PAGE : params[:per_page].to_i
