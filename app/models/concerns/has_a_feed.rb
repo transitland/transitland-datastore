e3dd0ed94222b742e45cb0fbb8a9dac16b309170
Ian Rees <ian@ianrees.net>
2015-09-11 13:06:58 -0700

    Move instance method outside of included block

--- app/models/concerns/has_a_feed.rb
+++ app/models/concerns/has_a_feed.rb
@@ -2,14 +2,13 @@ module HasAFeed
   extend ActiveSupport::Concern
 
   included do
-
     has_many :entities_imported_from_feed, as: :entity
     has_many :feeds, through: :entities_imported_from_feed
-
-    def imported_from_feed_onestop_id=(value)
-      feed = Feed.find_by!(onestop_id: value)
-      self.feeds << feed unless self.feeds.exists?(feed)
-    end
-
   end
+  
+  def imported_from_feed_onestop_id=(value)
+    feed = Feed.find_by!(onestop_id: value)
+    self.feeds << feed unless self.feeds.exists?(feed)
+  end
+  
 end
