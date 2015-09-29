8e45f7c986879d471c17a428531d30ca7ed7da9a
Ian Rees <ian@ianrees.net>
2015-09-28 17:36:33 -0700

    Create SSPs directly; Interpolate SSPs; use gtfs.trip_stop_times

--- lib/gtfsgraph.rb
+++ lib/gtfsgraph.rb
@@ -222,7 +222,19 @@ class GTFSGraph
         log "  trips: #{trip_counter} - #{trip_counter+trip_chunk.size}"
         trip_counter += trip_chunk.size
         ssp_chunk = []
-        @gtfs.stop_time_pairs(trip_chunk) { |route,trip,origin,destination| ssp_chunk << make_ssp(route,trip,origin,destination) }
+        @gtfs.trip_stop_times(trip_chunk) do |trip,stop_times|
+          route = @gtfs.route(trip.route_id)
+          ssps = []
+          stop_times[0..-2].zip(stop_times[1..-1]).each do |origin,destination|
+            ssp = make_ssp(route,trip,origin,destination)
+            ssps << ssp
+          end
+          # Interpolate stop_times
+          ScheduleStopPair.interpolate(ssps)
+          # Add to chunk
+          ssp_chunk += ssps
+        end
+        # Create changeset
         ssp_chunk.each_slice(CHANGE_PAYLOAD_MAX_ENTITIES) do |chunk|
           log "    ssps: #{ssp_counter} - #{ssp_counter+ssp_chunk.size}"
           ssp_counter += ssp_chunk.size
@@ -232,7 +244,7 @@ class GTFSGraph
               changes: chunk.map { |entity|
                 {
                   action: action,
-                  scheduleStopPair: entity
+                  scheduleStopPair: make_change_ssp(entity)
                 }
               }
             }
@@ -324,45 +336,69 @@ class GTFSGraph
     }
   end
 
+  def make_change_ssp(entity)
+    {
+      imported_from_feed_onestop_id: @feed.onestop_id,
+      originOnestopId: entity.origin.onestop_id,
+      originTimezone: entity.origin_timezone,
+      originArrivalTime: entity.origin_arrival_time,
+      originDepartureTime: entity.origin_departure_time,
+      destinationOnestopId: entity.destination.onestop_id,
+      destinationTimezone: entity.destination_timezone,
+      destinationArrivalTime: entity.destination_arrival_time,
+      destinationDepartureTime: entity.destination_departure_time,
+      routeOnestopId: entity.route.onestop_id,
+      trip: entity.trip,
+      tripHeadsign: entity.trip_headsign,
+      tripShortName: entity.trip_short_name,
+      wheelchairAccessible: entity.wheelchair_accessible,
+      dropOffType: entity.drop_off_type,
+      pickupType: entity.pickup_type,
+      shapeDistTraveled: entity.shape_dist_traveled,
+      serviceStartDate: entity.service_start_date,
+      serviceEndDate: entity.service_end_date,
+      serviceDaysOfWeek: entity.service_days_of_week,
+      serviceAddedDates: entity.service_added_dates,
+      serviceExceptDates: entity.service_except_dates
+    }
+  end
+
   def make_ssp(route, trip, origin, destination)
     # Generate an edge between an origin and destination for a given route/trip
     route = @gtfs_tl[route]
     origin_stop = @gtfs_tl[@gtfs.stop(origin.stop_id)]
     destination_stop = @gtfs_tl[@gtfs.stop(destination.stop_id)]
     service_period = @gtfs.service_period(trip.service_id)
-    ssp = {
+    ScheduleStopPair.new(
       # Origin
-      originOnestopId: origin_stop.onestop_id,
-      originTimezone: origin_stop.timezone,
-      originArrivalTime: origin.arrival_time,
-      originDepartureTime: origin.departure_time,
+      origin: origin_stop,
+      origin_timezone: origin_stop.timezone,
+      origin_arrival_time: origin.arrival_time,
+      origin_departure_time: origin.departure_time,
       # Destination
-      destinationOnestopId: destination_stop.onestop_id,
-      destinationTimezone: destination_stop.timezone,
-      destinationArrivalTime: destination.arrival_time,
-      destinationDepartureTime: destination.departure_time,
+      destination: destination_stop,
+      destination_timezone: destination_stop.timezone,
+      destination_arrival_time: destination.arrival_time,
+      destination_departure_time: destination.departure_time,
       # Route
-      routeOnestopId: route.onestop_id,
+      route: route,
       # Trip
       trip: trip.id,
-      tripHeadsign: (origin.stop_headsign || trip.trip_headsign),
-      tripShortName: trip.trip_short_name,
-      wheelchairAccessible: trip.wheelchair_accessible.to_i,
-      # bikes_allowed: trip.bikes_allowed.to_i,
+      trip_headsign: (origin.stop_headsign || trip.trip_headsign),
+      trip_short_name: trip.trip_short_name,
+      wheelchair_accessible: trip.wheelchair_accessible.to_i,
       # Stop Time
-      dropOffType: origin.drop_off_type.to_i,
-      pickupType: origin.pickup_type.to_i,
+      drop_off_type: origin.drop_off_type.to_i,
+      pickup_type: origin.pickup_type.to_i,
       # timepoint: origin.timepoint.to_i,
-      shapeDistTraveled: origin.shape_dist_traveled.to_f,
-      importedFromFeedOnestopId: @feed.onestop_id,
+      shape_dist_traveled: origin.shape_dist_traveled.to_f,
       # service period
-      serviceStartDate: service_period.start_date,
-      serviceEndDate: service_period.end_date,
-      serviceDaysOfWeek: service_period.iso_service_weekdays,
-      serviceAddedDates: service_period.added_dates,
-      serviceExceptDates: service_period.except_dates
-    }
-    ssp
+      service_start_date: service_period.start_date,
+      service_end_date: service_period.end_date,
+      service_days_of_week: service_period.iso_service_weekdays,
+      service_added_dates: service_period.added_dates,
+      service_except_dates: service_period.except_dates
+    )
   end
 end
 
