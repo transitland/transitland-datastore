663d26bdcbc34873a3b84c184aeb7413da190274
Ian Rees <ian@ianrees.net>
2015-09-29 12:31:10 -0700

    Add required origin/destination timezone to ScheduleStopPair factory

--- spec/factories/schedule_stop_pair_factory.rb
+++ spec/factories/schedule_stop_pair_factory.rb
@@ -56,13 +56,15 @@ FactoryGirl.define do
     association :created_or_updated_in_changeset, factory: :changeset
     version 1
     trip "1234"
+    origin_timezone "America/Los_Angeles"
+    destination_timezone "America/Los_Angeles"
     origin_arrival_time "10:00:00"
     origin_departure_time "10:00:10"
     destination_arrival_time "10:10:00"
     destination_departure_time "10:10:10"
     service_start_date "2000-01-01"
     service_end_date "2100-01-01"
-    service_added_dates [] 
+    service_added_dates []
     service_except_dates []
     service_days_of_week [true, true, true, true, true, false, false] # M - F
   end
