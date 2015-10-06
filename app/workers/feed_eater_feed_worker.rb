bf1899ec9b98119c333bfc4a5e47d2b8e8dd9e30
Ian Rees <ian@ianrees.net>
2015-10-06 16:33:51 -0700

    Remove operators as argument from change_create_osr

--- app/workers/feed_eater_feed_worker.rb
+++ app/workers/feed_eater_feed_worker.rb
@@ -39,9 +39,7 @@ class FeedEaterFeedWorker < FeedEaterWorker
     graph = nil
     begin
       graph = GTFSGraph.new(feed.file_path, feed)
-      graph.load_gtfs
-      operators = graph.load_tl
-      graph.create_change_osr(operators, import_level)
+      graph.create_change_osr(import_level)
     rescue Exception => e
       # NOTE: we're catching all exceptions, including Interrupt,
       #   SignalException, and SyntaxError
