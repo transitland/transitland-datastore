1096830ed4e1582c6cb9985c71b274d1bf5c506a
Ian Rees <ian@ianrees.net>
2015-10-06 12:27:02 -0700

    Add method load_gtfs_id_map, inverse of make_gtfs_id_map

--- lib/gtfsgraph.rb
+++ lib/gtfsgraph.rb
@@ -35,17 +35,7 @@ class GTFSGraph
   end
 
   def ssp_perform_async(trip_ids, agency_map, route_map, stop_map)
-    @gtfs_to_onestop_id.clear
-    # Populate GTFS entity to Onestop ID maps
-    agency_map.each do |agency_id,onestop_id|
-      @gtfs_to_onestop_id[@gtfs.agency(agency_id)] = onestop_id
-    end
-    route_map.each do |route_id,onestop_id|
-      @gtfs_to_onestop_id[@gtfs.route(route_id)] = onestop_id
-    end
-    stop_map.each do |stop_id,onestop_id|
-      @gtfs_to_onestop_id[@gtfs.stop(stop_id)] = onestop_id
-    end
+    load_gtfs_id_map(agency_map, route_map, stop_map)
     # Create SSPs
     trips = trip_ids.map { |trip_id| @gtfs.trip(trip_id) }
     changeset = Changeset.create()
@@ -57,7 +47,6 @@ class GTFSGraph
     raise ArgumentError.new('At least one operator required') if operators.nil? || operators.empty?
     raise ArgumentError.new('import_level must be 0, 1, or 2.') unless (0..2).include?(import_level)
     log "Create Changeset"
-    operators = operators
     routes = operators.map { |operator| operator.serves }.reduce(Set.new, :+)
     stops = routes.map { |route| route.serves }.reduce(Set.new, :+)
     changeset = Changeset.create()
@@ -251,6 +240,20 @@ class GTFSGraph
     @gtfs.routes.each   { |e| route_map[e.id]  = @gtfs_to_onestop_id[e]}
     @gtfs.stops.each    { |e| stop_map[e.id]   = @gtfs_to_onestop_id[e]}
     [agency_map, route_map, stop_map]
+  end
+
+  def load_gtfs_id_map(agency_map, route_map, stop_map)
+    @gtfs_to_onestop_id.clear
+    # Populate GTFS entity to Onestop ID maps
+    agency_map.each do |agency_id,onestop_id|
+      @gtfs_to_onestop_id[@gtfs.agency(agency_id)] = onestop_id
+    end
+    route_map.each do |route_id,onestop_id|
+      @gtfs_to_onestop_id[@gtfs.route(route_id)] = onestop_id
+    end
+    stop_map.each do |stop_id,onestop_id|
+      @gtfs_to_onestop_id[@gtfs.stop(stop_id)] = onestop_id
+    end
   end
 
   ##### Create change payloads ######
