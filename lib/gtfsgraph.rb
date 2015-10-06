366efb687c70cf82c164a987ebf603843ee74953
Ian Rees <ian@ianrees.net>
2015-10-06 12:44:55 -0700

    Minor reorganization of changeset create/apply

--- lib/gtfsgraph.rb
+++ lib/gtfsgraph.rb
@@ -38,17 +38,16 @@ class GTFSGraph
     load_gtfs_id_map(agency_map, route_map, stop_map)
     # Create SSPs
     trips = trip_ids.map { |trip_id| @gtfs.trip(trip_id) }
-    changeset = Changeset.create()
-    create_change_ssps(changeset, trips)
-    changeset.apply!
+    create_change_ssps(trips)
   end
 
   def create_change_osr(operators, import_level=0)
     raise ArgumentError.new('At least one operator required') if operators.nil? || operators.empty?
     raise ArgumentError.new('import_level must be 0, 1, or 2.') unless (0..2).include?(import_level)
-    log "Create Changeset"
     routes = operators.map { |operator| operator.serves }.reduce(Set.new, :+)
     stops = routes.map { |route| route.serves }.reduce(Set.new, :+)
+    log "Create: Operators, Stops, Routes"
+    log "  create changeset"
     changeset = Changeset.create()
     if import_level >= 0
       log "  operators: #{operators.size}"
@@ -60,7 +59,42 @@ class GTFSGraph
       log "  routes: #{routes.size}"
       create_change_payloads(changeset, 'route', routes.map { |e| make_change_route(e) })
     end
-    # Apply changeset
+    log "  changeset apply"
+    changeset.apply!
+    log "  changeset apply done"
+  end
+
+  def create_change_ssps(trips)
+    log "Create: SSPs"
+    log "  create changeset"
+    changeset = Changeset.create()
+    total = 0
+    ssps = []
+    @gtfs.trip_stop_times(trips) do |trip,stop_times|
+      log "    trip id: #{trip.trip_id}, stop_times: #{stop_times.size}"
+      route = @gtfs.route(trip.route_id)
+      # Create SSPs for all stop_time edges
+      ssp_trip = []
+      stop_times[0..-2].zip(stop_times[1..-1]).each do |origin,destination|
+        ssp_trip << make_ssp(route, trip, origin, destination)
+      end
+      # Interpolate stop_times
+      ScheduleStopPair.interpolate(ssp_trip)
+      # Add to chunk
+      ssps += ssp_trip
+      # If chunk is big enough, create change payloads.
+      if ssps.size >= CHANGE_PAYLOAD_MAX_ENTITIES
+        log  "    ssps: #{total} - #{total+ssps.size}"
+        total += ssps.size
+        create_change_payloads(changeset, 'scheduleStopPair', ssps.map { |e| make_change_ssp(e) })
+        ssps = []
+      end
+    end
+    # Create any remaining change payloads.
+    if ssps.size > 0
+      log  "    ssps: #{total} - #{total+ssps.size}"
+      create_change_payloads(changeset, 'scheduleStopPair', ssps.map { |e| make_change_ssp(e) })
+    end
     log "  changeset apply"
     changeset.apply!
     log "  changeset apply done"
@@ -254,36 +288,6 @@ class GTFSGraph
   end
 
   ##### Create change payloads ######
-
-  def create_change_ssps(changeset, trips)
-    total = 0
-    ssps = []
-    @gtfs.trip_stop_times(trips) do |trip,stop_times|
-      log "    trip id: #{trip.trip_id}, stop_times: #{stop_times.size}"
-      route = @gtfs.route(trip.route_id)
-      # Create SSPs for all stop_time edges
-      ssp_trip = []
-      stop_times[0..-2].zip(stop_times[1..-1]).each do |origin,destination|
-        ssp_trip << make_ssp(route, trip, origin, destination)
-      end
-      # Interpolate stop_times
-      ScheduleStopPair.interpolate(ssp_trip)
-      # Add to chunk
-      ssps += ssp_trip
-      # If chunk is big enough, create change payloads.
-      if ssps.size >= CHANGE_PAYLOAD_MAX_ENTITIES
-        log  "    ssps: #{total} - #{total+ssps.size}"
-        total += ssps.size
-        create_change_payloads(changeset, 'scheduleStopPair', ssps.map { |e| make_change_ssp(e) })
-        ssps = []
-      end
-    end
-    # Create any remaining change payloads.
-    if ssps.size > 0
-      log  "    ssps: #{total} - #{total+ssps.size}"
-      create_change_payloads(changeset, 'scheduleStopPair', ssps.map { |e| make_change_ssp(e) })
-    end
-  end
 
   def create_change_payloads(changeset, entity_type, entities)
     # Operators
