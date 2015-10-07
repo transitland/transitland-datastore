6904f31a7d283a4ab04549189934cdf3f1c1e0f9
Ian Rees <ian@ianrees.net>
2015-10-06 18:03:02 -0700

    Add feed_import_id to FESW job; Create and update FeedScheduleImport log record

--- app/workers/feed_eater_schedule_worker.rb
+++ app/workers/feed_eater_schedule_worker.rb
@@ -1,11 +1,11 @@
 require 'gtfsgraph'
 
 class FeedEaterScheduleWorker < FeedEaterWorker
-  def perform(feed_onestop_id, trip_ids, agency_map, route_map, stop_map)
-    feed = Feed.find_by(onestop_id: feed_onestop_id)
-
+  def perform(feed_onestop_id, feed_import_id, trip_ids, agency_map, route_map, stop_map)
     logger.info "FeedEaterScheduleWorker #{feed_onestop_id}: Importing #{trip_ids.size} trips"
-    # feed_import = FeedImport.create(feed: feed)
+    feed = Feed.find_by(onestop_id: feed_onestop_id)
+    feed_import = FeedImport.find(feed_import_id)
+    feed_schedule_import = FeedScheduleImport.create(feed_import: feed_import)
     graph = nil
     begin
       graph = GTFSGraph.new(feed.file_path, feed)
@@ -18,15 +18,14 @@ class FeedEaterScheduleWorker < FeedEaterWorker
     rescue Exception => e
       exception_log = "\n#{e}\n#{e.backtrace}\n"
       logger.error exception_log
-      # feed_import.update(success: false, exception_log: exception_log)
+      feed_schedule_import.update(success: false, exception_log: exception_log)
       # Raven.capture_exception(e) if defined?(Raven)
     else
       logger.info "FeedEaterScheduleWorker #{feed_onestop_id}: Saving successful import"
-      # feed.has_been_fetched_and_imported!(on_feed_import: feed_import)
+      feed_schedule_import.update(success: true)
     ensure
       logger.info "FeedEaterScheduleWorker #{feed_onestop_id}: Saving log & report"
-      # feed_import.update(import_log: graph.try(:import_log))
+      feed_schedule_import.update(import_log: graph.try(:import_log))
     end
-
   end
 end
