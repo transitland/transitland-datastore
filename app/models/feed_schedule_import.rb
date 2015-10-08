b690e6672d7454f4005ca1f9e27e65a9338a1dfd
Ian Rees <ian@ianrees.net>
2015-10-07 18:10:58 -0700

    Add FeedScheduleImport#failed and FeedScheduleImport#succeeded methods
    Set status, and call failed/succeeded of parent FeedImport

--- app/models/feed_schedule_import.rb
+++ app/models/feed_schedule_import.rb
@@ -16,5 +16,26 @@
 #
 
 class FeedScheduleImport < ActiveRecord::Base
+  PER_PAGE = 1
+
   belongs_to :feed_import
+  validates :feed_import, presence: true
+
+  def failed(exception_log)
+    # Mark as failed, bubble to parent feed_import
+    self.update(
+      success: false,
+      exception_log: exception_log
+    )
+    self.feed_import.failed(exception_log)
+  end
+
+  def succeeded
+    # Mark as succeeded, bubble to parent feed_import
+    self.update(success: true)
+    siblings = self.feed_import.feed_schedule_imports.pluck('success')
+    if siblings.all?
+      self.feed_import.succeeded
+    end
+  end
 end
