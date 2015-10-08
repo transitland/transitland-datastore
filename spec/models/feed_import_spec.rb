b41371be3dff23b2c3477880c17dbeb57837c1b9
Ian Rees <ian@ianrees.net>
2015-10-08 15:41:31 -0700

    Specs for FeedImport

--- spec/models/feed_import_spec.rb
+++ spec/models/feed_import_spec.rb
@@ -19,5 +19,32 @@
 #
 
 describe FeedImport do
-  pending 'write some specs'
+  context 'succeed or fail' do
+    it '#failed' do
+      feed_import = create(:feed_import)
+      feed_import.failed('error')
+      expect(feed_import.success).to eq(false)
+      expect(feed_import.exception_log).to eq('error')
+    end
+
+    it '#succeeded' do
+      feed_import = create(:feed_import)
+      feed_import.succeeded
+      expect(feed_import.success).to eq(true)
+    end
+
+    it '#succeeded updates sha1 of parent feed' do
+      feed_import = create(:feed_import)
+      feed_import.succeeded
+      expect(feed_import.sha1).to be_truthy
+      expect(feed_import.feed.last_sha1).to eq(feed_import.sha1)
+    end
+
+    it '#succeeded updates last_imported_at / last_fetched_at of parent feed' do
+      feed_import = create(:feed_import)
+      feed_import.succeeded
+      expect(feed_import.feed.last_fetched_at).to eq(feed_import.created_at)
+      expect(feed_import.feed.last_imported_at).to eq(feed_import.updated_at)
+    end
+  end
 end
