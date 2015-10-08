4d93cdf0bb8af86e0ab998a47a8b248911cc7c59
Ian Rees <ian@ianrees.net>
2015-10-08 15:41:41 -0700

    Specs for FeedSchedlueImport

--- spec/models/feed_schedule_import_spec.rb
+++ spec/models/feed_schedule_import_spec.rb
@@ -15,6 +15,49 @@
 #  index_feed_schedule_imports_on_feed_import_id  (feed_import_id)
 #
 
-RSpec.describe FeedScheduleImport, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe FeedScheduleImport do
+  context 'succeed or fail' do
+    it '#failed' do
+      feed_schedule_import = create(:feed_schedule_import)
+      feed_schedule_import.failed('error')
+      expect(feed_schedule_import.success).to eq(false)
+      expect(feed_schedule_import.exception_log).to eq('error')
+    end
+
+    it '#failed updates parent FeedImport' do
+      feed_schedule_import = create(:feed_schedule_import)
+      feed_schedule_import.failed('error')
+      expect(feed_schedule_import.feed_import.success).to eq(false)
+    end
+
+    it '#succeeded' do
+      feed_schedule_import = create(:feed_schedule_import)
+      feed_schedule_import.succeeded
+      expect(feed_schedule_import.success).to eq(true)
+    end
+
+    it '#succeeded updates parent FeedImport' do
+      feed_schedule_import = create(:feed_schedule_import)
+      feed_schedule_import.succeeded
+      expect(feed_schedule_import.feed_import.success).to eq(true)
+    end
+
+    it '#succeeded updates parent FeedImport only if all siblings succeed' do
+      feed_schedule_import = create(:feed_schedule_import)
+      feed_schedule_import2 = create(:feed_schedule_import, feed_import: feed_schedule_import.feed_import)
+      feed_schedule_import.succeeded
+      expect(feed_schedule_import.feed_import.success).to_not eq(true)
+      feed_schedule_import2.succeeded
+      expect(feed_schedule_import.feed_import.success).to eq(true)
+    end
+
+    it '#succeeded does not update parent FeedImport if any sibling fails' do
+      feed_schedule_import = create(:feed_schedule_import)
+      feed_schedule_import2 = create(:feed_schedule_import, feed_import: feed_schedule_import.feed_import)
+      feed_schedule_import2.failed('error')
+      feed_schedule_import.succeeded
+      expect(feed_schedule_import.feed_import.success).to_not eq(true)
+    end
+
+  end
 end
