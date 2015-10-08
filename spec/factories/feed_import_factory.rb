0a564d87c880409455b5121aca51bf067e0526b6
Ian Rees <ian@ianrees.net>
2015-10-07 18:20:26 -0700

    Add FeedScheduleImport factory

---
+++ spec/factories/feed_import_factory.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: feed_schedule_imports
+#
+#  id             :integer          not null, primary key
+#  success        :boolean
+#  import_log     :text
+#  exception_log  :text
+#  feed_import_id :integer
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+# Indexes
+#
+#  index_feed_schedule_imports_on_feed_import_id  (feed_import_id)
+#
+
+FactoryGirl.define do
+  factory :feed_import do
+    feed
+    success false
+    import_log "MyText"
+    exception_log "MyText"
+  end
+end
