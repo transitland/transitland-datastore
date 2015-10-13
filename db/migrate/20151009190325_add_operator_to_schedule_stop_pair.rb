3065dcf95f8c223ea6cf9e93b209608e844946df
Ian Rees <ian@ianrees.net>
2015-10-13 10:13:31 -0700

    Don't add Operators to SSPs during migration

--- db/migrate/20151009190325_add_operator_to_schedule_stop_pair.rb
+++ db/migrate/20151009190325_add_operator_to_schedule_stop_pair.rb
@@ -2,10 +2,6 @@ class AddOperatorToScheduleStopPair < ActiveRecord::Migration
   def up
     add_reference :current_schedule_stop_pairs, :operator, index: true
     add_reference :old_schedule_stop_pairs, :operator, index: true
-    ScheduleStopPair.find_each do |ssp|
-      ssp.operator = ssp.route.operator
-      ssp.save!
-    end
   end
 
   def down
