24f6a16125312931d8312fbdf8218feaa9c897b3
Ian Rees <ian@ianrees.net>
2015-11-06 15:06:08 -0800

    More gentle imported_from_feed=

--- app/models/concerns/is_an_entity_imported_from_feeds.rb
+++ app/models/concerns/is_an_entity_imported_from_feeds.rb
@@ -7,9 +7,12 @@ module IsAnEntityImportedFromFeeds
     has_many :imported_from_feed_versions, through: :entities_imported_from_feed, source: :feed_version
   end
 
-  def imported_from_feed=(params)
+  def imported_from_feed=(imported_from_feed_params)
+    params = HashHelpers::update_keys(imported_from_feed_params, :underscore)
     feed = params[:feed] || Feed.find_by!(onestop_id: params[:onestop_id])
-    feed_version = params[:feed_version] || feed.feed_versions.find_by!(sha1: params[:sha1])
+    if params[:sha1].present?
+      feed_version = params[:feed_version] || feed.feed_versions.find_by!(sha1: params[:sha1])
+    end
     self.entities_imported_from_feed.find_or_initialize_by(feed: feed, feed_version: feed_version)
   end
 end
