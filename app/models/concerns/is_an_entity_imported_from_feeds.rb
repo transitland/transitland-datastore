6322c2a192011ff0062458517674d38dcd795d9d
Ian Rees <ian@ianrees.net>
2015-11-06 14:42:54 -0800

    imported_from_feed= checks for entities before looking up

--- app/models/concerns/is_an_entity_imported_from_feeds.rb
+++ app/models/concerns/is_an_entity_imported_from_feeds.rb
@@ -7,15 +7,9 @@ module IsAnEntityImportedFromFeeds
     has_many :imported_from_feed_versions, through: :entities_imported_from_feed, source: :feed_version
   end
 
-  def imported_from_feed=(imported_from_feed_params)
-    imported_from_feed_params = HashHelpers::update_keys(imported_from_feed_params, :underscore)
-    eiff_params = {}
-    feed = Feed.find_by!(onestop_id: imported_from_feed_params[:onestop_id])
-    eiff_params[:feed] = feed
-    if imported_from_feed_params[:sha1].present?
-      feed_version = feed.feed_versions.find_by!(sha1: imported_from_feed_params[:sha1])
-      eiff_params[:feed_version] = feed_version
-    end
-    self.entities_imported_from_feed.find_or_initialize_by(eiff_params)
+  def imported_from_feed=(params)
+    feed = params[:feed] || Feed.find_by!(onestop_id: params[:onestop_id])
+    feed_version = params[:feed_version] || feed.feed_versions.find_by!(sha1: params[:sha1])
+    self.entities_imported_from_feed.find_or_initialize_by(feed: feed, feed_version: feed_version)
   end
 end
