3703d4c6d97ed22ea2106543b4b080f8009075ba
Ian Rees <ian@ianrees.net>
2015-11-10 15:19:51 -0800

    FeedVersionImport bubbles success/failure to parent FeedVersion

--- app/models/feed_version_import.rb
+++ app/models/feed_version_import.rb
@@ -31,12 +31,11 @@ class FeedVersionImport < ActiveRecord::Base
       success: false,
       exception_log: exception_log
     )
-    self.feed_version.delete_schedule_stop_pairs!
+    self.feed_version.failed
   end
 
   def succeeded
     self.update(success: true)
-    self.feed_version.update(imported_at: self.updated_at)
-    self.feed.activate_feed_version(self.feed_version.sha1)
+    self.feed_version.succeeded(self.updated_at)
   end
 end
