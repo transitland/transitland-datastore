21ccb25adf60016784d3869b62ec9605ba8f76c0
Ian Rees <ian@ianrees.net>
2015-11-10 14:58:47 -0800

    Activate feed version on successful import

--- app/models/feed_version_import.rb
+++ app/models/feed_version_import.rb
@@ -31,11 +31,12 @@ class FeedVersionImport < ActiveRecord::Base
       success: false,
       exception_log: exception_log
     )
+    self.feed_version.delete_schedule_stop_pairs!
   end
 
   def succeeded
     self.update(success: true)
     self.feed_version.update(imported_at: self.updated_at)
-    self.feed.update(last_imported_at: self.updated_at)
+    self.feed.activate_feed_version(self.feed_version.sha1)
   end
 end
