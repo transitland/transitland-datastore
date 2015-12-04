8cf38c520f9b568b179e80f9603e6e8f28f21327
Alexander Smith <alex@mapzen.com>
2015-12-04 14:39:59 -0800

    renamed column in RouteStopPattern to created_or_updated_in_changeset_id

--- app/models/route_stop_pattern.rb
+++ app/models/route_stop_pattern.rb
@@ -2,15 +2,15 @@
 #
 # Table name: current_route_stop_patterns
 #
-#  id                              :integer          not null, primary key
-#  geometry                        :geography({:srid geometry, 4326
-#  tags                            :hstore
-#  created_at                      :datetime         not null
-#  updated_at                      :datetime         not null
-#  route_id                        :string
-#  stop_pattern                    :string           default([]), is an Array
-#  version                         :integer
-#  created_or_updated_in_changeset :integer
+#  id                                 :integer          not null, primary key
+#  geometry                           :geography({:srid geometry, 4326
+#  tags                               :hstore
+#  created_at                         :datetime         not null
+#  updated_at                         :datetime         not null
+#  route_id                           :string
+#  stop_pattern                       :string           default([]), is an Array
+#  version                            :integer
+#  created_or_updated_in_changeset_id :integer
 #
 
 # TODO: uniqueness,
@@ -28,7 +28,7 @@ end
 class RouteStopPattern < BaseRouteStopPattern
   self.table_name_prefix = 'current_'
 
-  include IsAnEntityWithIdentifiers
+  #include IsAnEntityWithIdentifiers
   include HasAGeographicGeometry
   include HasTags
   include UpdatedSince
