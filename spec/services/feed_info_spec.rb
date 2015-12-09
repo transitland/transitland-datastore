c00695b68ee8aeb4e25594a579687a0ec30ea048
Ian Rees <ian@ianrees.net>
2015-12-08 16:40:30 -0800

    FeedInfo specs

--- spec/services/feed_info_spec.rb
+++ spec/services/feed_info_spec.rb
@@ -1,16 +1,67 @@
 describe FeedInfo do
-  let (:url) { 'http://www.caltrain.com/Assets/GTFS/caltrain/GTFS-Caltrain-Devs.zip' }
-  let (:path) { Rails.root.join('spec/support/example_gtfs_archives/f-9q9-caltrain.zip') }
+  let (:url) { 'http://httpbin.org/get' }
+  let (:url_redirect) { 'http://httpbin.org/redirect-to?url=http%3A%2F%2Fhttpbin.org%2Fget' }
+  let (:url_redirect_many) { 'http://httpbin.org/absolute-redirect/6' }
+  let (:url_404) { 'http://httpbin.org/status/404' }
+  let (:url_binary) { 'http://httpbin.org/stream-bytes/1024?seed=0' }
 
-  context 'fetch' do
+  context '#fetch' do
+    it 'returns a response' do
+      VCR.use_cassette('feed_info_fetch') do
+        response = {}
+        FeedInfo.fetch(url) { |resp| response = JSON.parse(resp.read_body) }
+        expect(response['url']).to eq(url)
+      end
+    end
+
+    it 'follows a redirect' do
+      VCR.use_cassette('feed_info_redirect') do
+        response = {}
+        FeedInfo.fetch(url_redirect) { |resp| response = JSON.parse(resp.read_body) }
+        expect(response['url']).to eq(url)
+      end
+    end
+
+    it 'follows a redirect no more than limit times' do
+      VCR.use_cassette('feed_info_redirect_fail') do
+        expect {
+          FeedInfo.fetch(url_redirect_many, 2) { |resp| response = JSON.parse(resp.read_body) }
+        }.to raise_error(ArgumentError)
+      end
+    end
+
+    it 'raises errors' do
+      VCR.use_cassette('feed_info_404') do
+        expect {
+          FeedInfo.fetch(url_404, 2) { |resp| response = JSON.parse(resp.read_body) }
+        }.to raise_error(Net::HTTPServerException)
+      end
+    end
 
   end
 
-  context 'download' do
+  context '#download' do
+    it 'downloads to temp file' do
+      VCR.use_cassette('feed_info_download') do
+        data = {}
+        FeedInfo.download_to_tempfile(url) { |filename| data = JSON.parse(File.read(filename))}
+        expect(data['url']).to eq(url)
+      end
+    end
 
+    it 'downloads binary data' do
+      VCR.use_cassette('feed_info_download_binary') do
+        data = nil
+        FeedInfo.download_to_tempfile(url_binary) { |filename| data = File.read(filename) }
+        expect(Digest::MD5.new.update(data).hexdigest).to eq('355c7ebd00db307b91ecd23a4215174a')
+      end
+    end
   end
 
-  context 'parse' do
+  context '#parse' do
+    let (:url) { 'http://www.caltrain.com/Assets/GTFS/caltrain/GTFS-Caltrain-Devs.zip' }
+    let (:path) { Rails.root.join('spec/support/example_gtfs_archives/f-9q9-caltrain.zip') }
+
     it 'parses feed' do
       feed, operators = FeedInfo.parse_feed_and_operators(url, path)
       expect(feed.onestop_id).to eq('f-9q9-wwwcaltraincom')
