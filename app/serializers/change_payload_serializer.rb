5722f86ea9a028d929e61140d3c98868d85bd1da
Drew Dara-Abrams <drew@mapzen.com>
2015-12-21 15:58:04 -0800

    adding a ChangePayload serializer

---
+++ app/serializers/change_payload_serializer.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: change_payloads
+#
+#  id           :integer          not null, primary key
+#  payload      :json
+#  changeset_id :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+# Indexes
+#
+#  index_change_payloads_on_changeset_id  (changeset_id)
+#
+
+class ChangePayloadSerializer < ApplicationSerializer
+  attributes :id,
+             :changeset_id,
+             :payload,
+             :created_at,
+             :updated_at
+end
