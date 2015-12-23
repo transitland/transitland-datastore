fe052b3813b84487bf89a6809c9485692b9ace71
Ian Rees <ian@ianrees.net>
2015-12-23 15:05:09 -0800

    Remove Stop columns

--- spec/models/stop_spec.rb
+++ spec/models/stop_spec.rb
@@ -3,26 +3,31 @@
 # Table name: current_stops
 #
 #  id                                 :integer          not null, primary key
-#  onestop_id                         :string
 #  geometry                           :geography({:srid geometry, 4326
-#  tags                               :hstore
 #  created_at                         :datetime
 #  updated_at                         :datetime
 #  name                               :string
 #  created_or_updated_in_changeset_id :integer
 #  version                            :integer
-#  identifiers                        :string           default([]), is an Array
 #  timezone                           :string
 #  last_conflated_at                  :datetime
+#  identifier                         :string
+#  url                                :string
+#  zone                               :string
+#  code                               :string
+#  description                        :string
+#  wheelchair_boarding                :integer
+#  location_type                      :integer
+#  station_id                         :integer
+#  parent_stop_id                     :integer
 #
 # Indexes
 #
-#  #c_stops_cu_in_changeset_id_index   (created_or_updated_in_changeset_id)
-#  index_current_stops_on_geometry     (geometry)
-#  index_current_stops_on_identifiers  (identifiers)
-#  index_current_stops_on_onestop_id   (onestop_id)
-#  index_current_stops_on_tags         (tags)
-#  index_current_stops_on_updated_at   (updated_at)
+#  #c_stops_cu_in_changeset_id_index      (created_or_updated_in_changeset_id)
+#  index_current_stops_on_geometry        (geometry)
+#  index_current_stops_on_parent_stop_id  (parent_stop_id)
+#  index_current_stops_on_station_id      (station_id)
+#  index_current_stops_on_updated_at      (updated_at)
 #
 
 describe Stop do
