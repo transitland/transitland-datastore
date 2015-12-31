cad032b9102446bc557cfbf897193f691671d17b
Ian Rees <ian@ianrees.net>
2015-12-30 21:13:18 -0800

    Annotations

--- spec/models/stop_spec.rb
+++ spec/models/stop_spec.rb
@@ -14,15 +14,18 @@
 #  identifiers                        :string           default([]), is an Array
 #  timezone                           :string
 #  last_conflated_at                  :datetime
+#  type                               :string
+#  parent_stop_id                     :integer
 #
 # Indexes
 #
-#  #c_stops_cu_in_changeset_id_index   (created_or_updated_in_changeset_id)
-#  index_current_stops_on_geometry     (geometry)
-#  index_current_stops_on_identifiers  (identifiers)
-#  index_current_stops_on_onestop_id   (onestop_id)
-#  index_current_stops_on_tags         (tags)
-#  index_current_stops_on_updated_at   (updated_at)
+#  #c_stops_cu_in_changeset_id_index      (created_or_updated_in_changeset_id)
+#  index_current_stops_on_geometry        (geometry)
+#  index_current_stops_on_identifiers     (identifiers)
+#  index_current_stops_on_onestop_id      (onestop_id)
+#  index_current_stops_on_parent_stop_id  (parent_stop_id)
+#  index_current_stops_on_tags            (tags)
+#  index_current_stops_on_updated_at      (updated_at)
 #
 
 describe Stop do
