b1b6ea0d38f1c01f69e3033d39414ce436695902
Drew Dara-Abrams <drew@mapzen.com>
2016-01-06 11:35:56 -0800

    reduce ConflateStopsWithOsmWorker memory usage
    If job receives a huge number of stop IDs, it will load them into memory in batches, rather than all at once. In theory, the garbage collector will clear out each batch when it's no longer relevant.
    
    see also: https://github.com/mperham/sidekiq/wiki/Problems-and-Troubleshooting#memory-bloat
    
    another partial solution to #291

--- app/workers/conflate_stops_with_osm_worker.rb
+++ app/workers/conflate_stops_with_osm_worker.rb
@@ -2,7 +2,8 @@ class ConflateStopsWithOsmWorker
   include Sidekiq::Worker
 
   def perform(stop_ids = [])
-    stops = Stop.where(id: stop_ids)
-    Stop.conflate_with_osm(stops)
+    stops = Stop.where(id: stop_ids).find_in_batches(batch_size: 1000) do |batch_of_stops|
+      Stop.conflate_with_osm(batch_of_stops)
+    end
   end
 end
