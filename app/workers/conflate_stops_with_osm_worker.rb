44f62c05aeefb560e0ff1c2765cf4388421d3e2b
Drew Dara-Abrams <drew@mapzen.com>
2016-01-06 11:49:07 -0800

    fixing commit b1b6ea0

--- app/workers/conflate_stops_with_osm_worker.rb
+++ app/workers/conflate_stops_with_osm_worker.rb
@@ -2,7 +2,7 @@ class ConflateStopsWithOsmWorker
   include Sidekiq::Worker
 
   def perform(stop_ids = [])
-    stops = Stop.where(id: stop_ids).find_in_batches(batch_size: 1000) do |batch_of_stops|
+    Stop.where(id: stop_ids).find_in_batches do |batch_of_stops|
       Stop.conflate_with_osm(batch_of_stops)
     end
   end
