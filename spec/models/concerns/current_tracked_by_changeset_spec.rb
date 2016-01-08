16592a853921518b2e222a3de5863a5be3c9fa3f
Ian Rees <ian@ianrees.net>
2016-01-08 13:31:55 -0800

    #as_change specs

---
+++ spec/models/concerns/current_tracked_by_changeset_spec.rb
@@ -0,0 +1,22 @@
+describe CurrentTrackedByChangeset do
+  let(:stop) { create(:stop) }
+
+  context '#as_change' do
+    it 'converts to camelCase' do
+      expect(stop.as_change[:onestopId]).to eq(stop.onestop_id)
+    end
+
+    it 'only includes changeable attributes' do
+      expect(stop.as_change[:createdAt]).to be nil
+      expect(stop.as_change[:updatedAt]).to be nil
+    end
+
+    it 'includes virtual attributes' do
+      expect(stop.as_change.include?(:servedBy)).to be true
+    end
+
+    it 'does not include foreign keys' do
+      expect(stop.as_change[:created_or_updated_in_changeset]).to be nil
+    end
+  end
+end
