e75d51dc9990cee6528c81555377b74ed40088d8
Alexander Smith <alex@mapzen.com>
2016-01-19 11:47:06 -0800

    first commit of initializers/rgeo testing

---
+++ spec/initializers/rgeo_spec.rb
@@ -0,0 +1,13 @@
+describe RGeo do
+  context 'with additional LineStringMethods' do
+    it 'splits at line segment at a point' do
+
+    end
+  end
+
+  context 'with PointLocator' do
+    it 'can be created' do
+      
+    end
+  end
+end
