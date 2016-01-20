3c17c35b9b3f84d13e44056f0aab4165d0367084
Alexander Smith <alex@mapzen.com>
2016-01-20 11:45:18 -0800

    fixed global find and replace error in documentation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  # Enable locale fallbacks for I18n (makes handler_by_strings for any locale fall back to
+  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
