20cf93dc2c380ad7da8650ddb525fce8da3b7646
Drew Dara-Abrams <drew@mapzen.com>
2016-01-19 20:44:14 -0800

    enable CORS headers for DELETE actions
    so Dispatcher app can destroy changesets and change payloads https://github.com/transitland/dispatcher/issues/5

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module TransitlandDatastore
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :delete, :options]
       end
     end
 
