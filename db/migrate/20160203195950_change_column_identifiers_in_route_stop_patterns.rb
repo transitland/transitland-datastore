0aed2ab28c2ff43c357e591186c08146d57689be
Alexander Smith <alex@mapzen.com>
2016-02-04 10:08:06 -0800

    deleting old migration file

--- db/migrate/20160203195950_change_column_identifiers_in_route_stop_patterns.rb
+++
@@ -1,8 +0,0 @@
-class ChangeColumnIdentifiersInRouteStopPatterns < ActiveRecord::Migration
-  def change
-    ["current","old"].each do |version|
-      remove_column "#{version}_route_stop_patterns", :identifiers, array: true, index: true, default: []
-      add_column "#{version}_route_stop_patterns", :identifiers, :string, array: true, index: true, default: [], using: :gin
-    end
-  end
-end
