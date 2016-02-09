c9d550f31a86e07271f2a62608e39db70852a97a
Ian Rees <ian@ianrees.net>
2016-02-08 16:21:20 -0800

    FeedEaterActivationWorker

---
+++ app/workers/feed_eater_activation_worker.rb
@@ -0,0 +1,8 @@
+class FeedEaterScheduleWorker
+  include Sidekiq::Worker
+
+  def perform(feed_onestop_id, feed_version_sha1)
+    logger.info "FeedEaterActivationWorker #{feed_onestop_id}: activating #{feed_version_sha1}"
+    Feed.find_by_onestop_id!(feed_onestop_id).activate_feed_version(feed_version_sha1)
+  end
+end
