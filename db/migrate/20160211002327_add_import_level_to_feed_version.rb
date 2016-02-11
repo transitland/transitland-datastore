ed4d1faa4e90e90075c63878f85561599a9ec813
Ian Rees <ian@ianrees.net>
2016-02-10 17:38:11 -0800

    FeedVersion import_level index

--- db/migrate/20160211002327_add_import_level_to_feed_version.rb
+++ db/migrate/20160211002327_add_import_level_to_feed_version.rb
@@ -1,5 +1,5 @@
 class AddImportLevelToFeedVersion < ActiveRecord::Migration
   def change
-    add_column :feed_versions, :import_level, :integer, default: 0
+    add_column :feed_versions, :import_level, :integer, index: true, default: 0
   end
 end
