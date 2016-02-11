a32ff03f77692b6f68c31164feb590b299eb57c3
Ian Rees <ian@ianrees.net>
2016-02-10 18:12:41 -0800

    Remove route webhooks#feed_activation

--- config/routes.rb
+++ config/routes.rb
@@ -41,7 +41,6 @@ Rails.application.routes.draw do
       post '/feeds/fetch_info', to: 'feeds#fetch_info'
       post '/webhooks/feed_fetcher', to: 'webhooks#feed_fetcher'
       post '/webhooks/feed_eater', to: 'webhooks#feed_eater'
-      post '/webhooks/feed_activation', to: 'webhooks#feed_activation'
       # TODO: expose user authentication endpoints in the future
       # devise_for :users
       resources :users
