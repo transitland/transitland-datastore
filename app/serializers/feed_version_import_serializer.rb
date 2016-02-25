94feb76de076785af4e5a2818b68628de9ff1c7c
Drew Dara-Abrams <drew@mapzen.com>
2016-02-24 16:16:19 -0800

    we can't actually associated FeedVersionImport's and the Changeset's created

--- app/serializers/feed_version_import_serializer.rb
+++ app/serializers/feed_version_import_serializer.rb
@@ -27,8 +27,7 @@ class FeedVersionImportSerializer < ApplicationSerializer
              :exception_log,
              :validation_report,
              :created_at,
-             :updated_at,
-             :imported_from_changesets
+             :updated_at
 
   has_many :feed_schedule_imports
 
@@ -46,9 +45,5 @@ class FeedVersionImportSerializer < ApplicationSerializer
 
   def feed_version_url
     api_v1_feed_version_url(object.feed_version.sha1)
-  end
-
-  def imported_from_changesets
-    object.imported_from_changesets.map(&:id)
   end
 end
--- spec/controllers/api/v1/feed_version_imports_controller_spec.rb
+++ spec/controllers/api/v1/feed_version_imports_controller_spec.rb
@@ -1,3 +1,17 @@
 describe Api::V1::FeedVersionImportsController do
-  pending 'write some specs'
+  context 'GET index' do
+    it 'returns all FeedVersionImports when no parameters provided' do
+      create_list(:feed_version_import, 2)
+      get :index, total: true
+      expect_json_types({ feed_version_imports: :array })
+      expect_json({
+        feed_version_imports: -> (feed_version_imports) {
+          expect(feed_version_imports.length).to eq 1
+        },
+        meta: {
+          total: 2
+        }
+      })
+    end
+  end
 end
