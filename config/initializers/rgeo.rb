7bbb97001da24325c8f6c6b737ad7e1a88c48d1b
Alexander Smith <alex@mapzen.com>
2016-02-26 12:20:16 -0800

    caching distance value

--- config/initializers/rgeo.rb
+++ config/initializers/rgeo.rb
@@ -75,12 +75,13 @@ module RGeo
       def distance_from_segment
         return 0 if segment.contains_point?(target)
         dist = distance_on_segment
+        dist_from_departure = target_distance_from_departure
         return target.distance(segment.e) if dist >= segment.length
         return target.distance(segment.s) if dist <= 0
-        diff = target_distance_from_departure - dist
+        diff = dist_from_departure - dist
         # sometimes there can be a precision mismatch
         return 0 if (diff < 0 && diff.abs < 0.00001 )
-        ::Math.sqrt( target_distance_from_departure ** 2 - dist ** 2 )
+        ::Math.sqrt( dist_from_departure ** 2 - dist ** 2 )
       end
 
       def target_distance_from_departure
