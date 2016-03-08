2b4ac5a502aab72f85bb333cbc754666f74f38d6
Alexander Smith <alex@mapzen.com>
2016-03-08 14:21:50 -0800

    instance variable for before/after stops

--- app/models/route_stop_pattern.rb
+++ app/models/route_stop_pattern.rb
@@ -32,7 +32,7 @@ class BaseRouteStopPattern < ActiveRecord::Base
 
   include IsAnEntityImportedFromFeeds
 
-  attr_accessor :traversed_by, :distance_issues
+  attr_accessor :traversed_by, :distance_issues, :first_stop_before_geom, :last_stop_after_geom
 end
 
 class RouteStopPattern < BaseRouteStopPattern
@@ -200,6 +200,8 @@ class RouteStopPattern < BaseRouteStopPattern
 
   def tl_geometry(stop_points, issues)
     # modify rsp geometry based on issues array from evaluate_geometry
+    self.first_stop_before_geom = false
+    self.last_stop_after_geom = false
     if issues.include?(:empty)
       # create a new geometry from the trip stop points
       self.geometry = RouteStopPattern.line_string(RouteStopPattern.simplify_geometry(stop_points))
@@ -207,14 +209,10 @@ class RouteStopPattern < BaseRouteStopPattern
       self.is_modified = true
     end
     if issues.include?(:has_before_stop)
-      points = self.geometry[:coordinates].unshift(RouteStopPattern.set_precision([stop_points[0]])[0])
-      self.geometry = RouteStopPattern.line_string(RouteStopPattern.simplify_geometry(points))
-      self.is_modified = true
+      self.first_stop_before_geom = true
     end
     if issues.include?(:has_after_stop)
-      points = self.geometry[:coordinates] << RouteStopPattern.set_precision([stop_points[-1]])[0]
-      self.geometry = RouteStopPattern.line_string(RouteStopPattern.simplify_geometry(points))
-      self.is_modified = true
+      self.last_stop_after_geom = true
     end
     # more geometry modification can go here
   end
