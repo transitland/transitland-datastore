75b27804fa3f1b2cdec92e5c3af9ed356aa70998
Alexander Smith <alex@mapzen.com>
2016-03-24 18:10:22 -0700

    renaming spec description

--- spec/models/route_stop_pattern_spec.rb
+++ spec/models/route_stop_pattern_spec.rb
@@ -255,7 +255,7 @@ describe RouteStopPattern do
       expect(@feed.imported_route_stop_patterns[1].calculate_distances).to match_array([3.2, 8141.2])
     end
 
-    it 'accurately calculates the distances of a route line that loops back over itself' do
+    it 'accurately calculates the distances of a route with stops along the part that traversed over itself in the opposite direction' do
       # see docs/actransit-route-677.png
       @feed, @feed_version = load_feed(feed_version_name: :feed_version_actransit, import_level: 2)
       expect(@feed.imported_route_stop_patterns[0].calculate_distances[14..18]).to match_array([4809.3, 5147.2, 5619.5, 6404.2, 8569.0])
