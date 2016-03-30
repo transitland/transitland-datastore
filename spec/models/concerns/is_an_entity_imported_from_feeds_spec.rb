fa7116c6a036f738cb8e91b0072dbe36e28e88b8
Ian Rees <ian@ianrees.net>
2016-03-30 12:17:14 -0700

    Spec to verify distinct import_level

--- spec/models/concerns/is_an_entity_imported_from_feeds_spec.rb
+++ spec/models/concerns/is_an_entity_imported_from_feeds_spec.rb
@@ -2,19 +2,27 @@ describe IsAnEntityImportedFromFeeds do
   context '.where_import_level' do
     before(:each) do
       stops = []
-      [1,2].each do |import_level|
-        feed_version = create(:feed_version)
-        feed_version.feed.activate_feed_version(feed_version.sha1, import_level)
-        stop = create(:stop)
-        stops << stop
-        feed_version.entities_imported_from_feed.create!(entity: stop)
-      end
-      @stop1, @stop2 = stops
+      @stop1 = create(:stop)
+      @stop2 = create(:stop)
+      # create feeds
+      feed = create(:feed)
+      fv1 = create(:feed_version, feed: feed)
+      fv2 = create(:feed_version, feed: feed)
+      # add EIFFs; stop1 in fv1, fv2; stop2 in fv2
+      fv1.entities_imported_from_feed.create!(entity: @stop1)
+      fv2.entities_imported_from_feed.create!(entity: @stop1)
+      fv2.entities_imported_from_feed.create!(entity: @stop2)
+      # activate
+      feed.activate_feed_version(fv1.sha1, 1)
+      feed.activate_feed_version(fv2.sha1, 2)
     end
 
     it 'matches single import_level' do
       expect(Stop.where_import_level(1)).to match_array([@stop1])
-      expect(Stop.where_import_level(2)).to match_array([@stop2])
+      expect(Stop.where_import_level(2)).to match_array([@stop1, @stop2])
+    end
+
+    it 'chains with where_active' do
     end
 
     it 'excludes non matching' do
@@ -24,5 +32,12 @@ describe IsAnEntityImportedFromFeeds do
     it 'allows multiple import_levels' do
       expect(Stop.where_import_level([1,2])).to match_array([@stop1, @stop2])
     end
+
+    it 'filters distinct' do
+      expect(@stop1.imported_from_feed_versions.count).to eq(2)
+      expect(@stop2.imported_from_feed_versions.count).to eq(1)
+      expect(Stop.where_import_level([2])).to match_array([@stop1, @stop2])
+    end
+
   end
 end
