1a6bd7d67d00187f4b637f9ebfb31abf97056441
Ian Rees <ian@ianrees.net>
2016-04-04 16:23:13 -0700

    Spec for Entity#merge

--- spec/models/concerns/current_tracked_by_changeset_spec.rb
+++ spec/models/concerns/current_tracked_by_changeset_spec.rb
@@ -1,6 +1,23 @@
 describe CurrentTrackedByChangeset do
   let(:stop) { create(:stop) }
 
+  context '#merge' do
+    it 'merges changeable attributes' do
+      stop1 = create(:stop)
+      stop2 = create(:stop, name: 'Test')
+      stop1.merge(stop2)
+      expect(stop1.name).to eq(stop2.name)
+    end
+
+    it 'does not merge non-changeable attributes' do
+      stop1 = create(:stop, version: 1)
+      stop2 = create(:stop, version: 2, name: 'Test')
+      stop1.merge(stop2)
+      expect(stop1.version).to eq(1)
+      expect(stop2.version).to eq(2)
+    end
+  end
+
   context "#as_change" do
     it "converts to camelCase" do
       expect(stop.as_change[:onestopId]).to eq(stop.onestop_id)
