4c1f615beaba11d6d86bace854e4d64bf271abcc
Alexander Smith <alex@mapzen.com>
2016-05-02 13:52:25 -0700

    splitting base and sub classes

--- app/services/quality_check.rb
+++ app/services/quality_check.rb
@@ -8,15 +8,6 @@ class QualityCheck
     @creator_changeset = changeset
   end
 
-  def outlier_stop(stop, rsp)
-    if rsp.outlier_stop(stop[:geometry])
-      issue = Issue.new
-      issue.feed_version = @feed_version
-      issue.description = "Stop #{stop.onestop_id} is an outlier"
-      @issue_entities[issue] = issue.entities_with_issues.new(entity: stop)
-    end
-  end
-
   def save
     @issue_entities.keys.each { |issue| issue.created_by_changeset = @creator_changeset }
     Issue.import @issue_entities.keys
@@ -26,4 +17,21 @@ class QualityCheck
     end
     EntityWithIssues.import @issue_entities.values
   end
+end
+
+class StopQualityCheck < QualityCheck
+  def outlier_stop(stop, rsp)
+    if rsp.outlier_stop(stop[:geometry])
+      issue = Issue.new
+      issue.feed_version = @feed_version
+      issue.description = "Stop #{stop.onestop_id} is an outlier"
+      @issue_entities[issue] = issue.entities_with_issues.new(entity: stop)
+    end
+  end
+end
+
+class RouteQualityCheck < QualityCheck
+end
+
+class RouteStopPatternQualityCheck < QualityCheck
 end
