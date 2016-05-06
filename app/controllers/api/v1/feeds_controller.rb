f34365bb585ebd1e1287c6149cc646a8ecf7c196
Ian Rees <ian@ianrees.net>
2016-05-06 14:29:06 -0700

    Feeds controller update for new expiration options

--- app/controllers/api/v1/feeds_controller.rb
+++ app/controllers/api/v1/feeds_controller.rb
@@ -44,6 +44,18 @@ class Api::V1::FeedsController < Api::V1::BaseApiController
       @feeds = @feeds.geometry_within_bbox(params[:bbox])
     end
 
+    if params[:active_feed_version_valid].present?
+      @feeds = @feeds.where_active_feed_version_valid(params[:active_feed_version_valid])
+    end
+
+    if params[:active_feed_version_expired].present?
+      @feeds = @feeds.where_active_feed_version_expired(params[:active_feed_version_expired])
+    end
+
+    if params[:active_feed_version_update].presence == 'true'
+      @feeds = @feeds.where_active_feed_version_update
+    end
+
     respond_to do |format|
       format.json do
         render paginated_json_collection(
@@ -54,7 +66,14 @@ class Api::V1::FeedsController < Api::V1::BaseApiController
           params[:offset],
           params[:per_page],
           params[:total],
-          params.slice(:tag_key, :tag_value)
+          params.slice(
+            :tag_key,
+            :tag_value,
+            :last_imported_since,
+            :active_feed_version_valid,
+            :active_feed_version_expired,
+            :active_feed_version_update
+          )
         )
       end
       format.geojson do
