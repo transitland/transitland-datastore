cb8afd0e6d2f79409108e4ab7422e0b69c349c91
Alexander Smith <alex@mapzen.com>
2016-05-17 13:31:47 -0700

    issues route now displaying json

--- app/controllers/api/v1/issues_controller.rb
+++ app/controllers/api/v1/issues_controller.rb
@@ -1,7 +1,21 @@
-class Api::Controller::IssuesController < Api::V1::BaseApiController
-
+class Api::V1::IssuesController < Api::V1::BaseApiController
+  include JsonCollectionPagination
   def index
+    @issues = Issue.where('')
 
+    respond_to do |format|
+      format.json do
+        render paginated_json_collection(
+          @issues,
+          Proc.new { |params| api_v1_routes_url(params) },
+          params[:sort_key],
+          params[:sort_order],
+          params[:offset],
+          params[:per_page],
+          params[:total],
+          params
+        )
+      end
+    end
   end
-
 end
