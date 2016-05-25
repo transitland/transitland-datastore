101df8adaf3208d9ebcbd8e1113c44475fc343f1
Ian Rees <ian@ianrees.net>
2016-05-25 15:16:30 -0700

    Serializer improvements

--- app/serializers/stop_station_serializer.rb
+++ app/serializers/stop_station_serializer.rb
@@ -35,23 +35,47 @@ class StopStationSerializer < CurrentEntitySerializer
              :tags,
              :timezone,
              :created_at,
-             :updated_at,
-             :stop_egresses
-
-  has_many :operators_serving_stop
-  has_many :routes_serving_stop
-  class StopPlatformSerializer < ActiveModel::Serializer
-    attributes :onestop_id
+             :updated_at
+  # Platform / Egress Serializers
+  class StopPlatformSerializer < CurrentEntitySerializer
+    attributes :onestop_id,
+               :geometry,
+               :name,
+               :tags,
+               :created_at,
+               :updated_at
+               :last_conflated_at
+     has_many :operators_serving_stop
+     has_many :routes_serving_stop
   end
-  class StopEgressSerializer < ActiveModel::Serializer
-    attributes :onestop_id
+  class StopEgressSerializer < CurrentEntitySerializer
+    attributes :onestop_id,
+               :geometry,
+               :name,
+               :tags,
+               :created_at,
+               :updated_at
+               :last_conflated_at
   end
+  # Create phantom platforms / egresses
   def stop_egresses
-    object.stop_egresses.presence || [StopEgress.new(onestop_id: "#{object.onestop_id}>")]
+    object.stop_egresses.presence || [StopEgress.new(
+      onestop_id: "#{object.onestop_id}>",
+      geometry: object.geometry,
+      name: object.name,
+      last_conflated_at: object.last_conflated_at,
+      tags: object.tags.slice(:osm_way_id)
+    )]
   end
   def stop_platforms
-    object.stop_platforms.presence || [StopPlatform.new(onestop_id: "#{object.onestop_id}<")]
+    object.stop_platforms.presence || [StopPlatform.new(
+      onestop_id: "#{object.onestop_id}<",
+      geometry: object.geometry,
+      name: object.name,
+      tags: {}
+    )]
   end
+  # Platform / Egress Relation
   has_many :stop_platforms, serializer: StopPlatformSerializer
   has_many :stop_egresses, serializer: StopEgressSerializer
 end
