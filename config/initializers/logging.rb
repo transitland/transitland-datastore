9179e5a56f512ed454f1e86fd73c514281fd52d6
Alexander Smith <alex@mapzen.com>
2016-06-09 16:20:32 -0700

    flexible logging in initializer log function

--- config/initializers/logging.rb
+++ config/initializers/logging.rb
@@ -1,8 +1,8 @@
-def log(msg)
+def log(msg, level = :info)
   if Sidekiq::Logging.logger
-    Sidekiq::Logging.logger.info msg
+    Sidekiq::Logging.logger.send level, msg
   elsif Rails.logger
-    Rails.logger.info msg
+    Rails.logger.send level, msg
   else
     puts msg
   end
