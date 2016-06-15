e29b795f3623b6212b62921e72d9480bdac1e2b9
Ian Rees <ian@ianrees.net>
2016-06-14 21:54:49 -0700

    StopPlatform spec

--- spec/models/stop_spec.rb
+++ spec/models/stop_spec.rb
@@ -164,3 +164,61 @@ describe Stop do
     pending 'write some specs'
   end
 end
+
+
+describe StopPlatform do
+  context 'changeset' do
+    it 'can be created' do
+      stop = create(:stop)
+      onestop_id = "#{stop.onestop_id}<test"
+      payload = {changes: [{
+        action: 'createUpdate',
+        stopPlatform: {
+          onestopId: onestop_id,
+          timezone: 'America/Los_Angeles',
+          parentStopOnestopId: stop.onestop_id
+        }
+      }]}
+      changeset = Changeset.create(payload: payload)
+      changeset.apply!
+      stop_platform = StopPlatform.find_by_onestop_id!(onestop_id)
+      expect(stop_platform.onestop_id).to eq(onestop_id)
+      expect(stop_platform.parent_stop).to eq(stop)
+    end
+
+    it 'can be associated with a different parent stop' do
+      stop1 = create(:stop)
+      stop2 = create(:stop)
+      stop_platform = StopPlatform.create!(
+        onestop_id: "#{stop1.onestop_id}<test",
+        timezone: stop1.timezone,
+        parent_stop: stop1
+      )
+      payload = {changes: [{
+        action: 'createUpdate',
+        stopPlatform: {
+          onestopId: stop_platform.onestop_id,
+          parentStopOnestopId: stop2.onestop_id
+        }
+      }]}
+      expect(stop_platform.parent_stop).to eq(stop1)
+      changeset = Changeset.create(payload: payload)
+      changeset.apply!
+      expect(stop_platform.reload.parent_stop).to eq(stop2)
+    end
+
+    it 'requires valid parentStopOnestopId' do
+      payload = {changes: [{
+        action: 'createUpdate',
+        stopPlatform: {
+          onestopId: 's-123-foo<bar',
+          timezone: 'America/Los_Angeles',
+          parentStopOnestopId: 's-123-foo'
+        }
+      }]}
+      changeset = Changeset.create()
+      changeset.change_payloads.create!(payload: payload)
+      expect{changeset.apply!}.to raise_error(Changeset::Error)
+    end
+  end
+end
