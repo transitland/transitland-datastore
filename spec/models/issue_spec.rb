efc6fa74cfcc2e8a5809f71977f33ee498f7f27d
Alexander Smith <alex@mapzen.com>
2016-06-15 10:00:37 -0700

    specs for issue find_by_equivalent

--- spec/models/issue_spec.rb
+++ spec/models/issue_spec.rb
@@ -15,7 +15,42 @@
 
 describe Issue do
   it 'can be created' do
-    changeset = create(:issue_changeset)
+    changeset = create(:changeset_creating_issue)
     issue = Issue.new(created_by_changeset: changeset)
+  end
+
+  context 'methods' do
+    before(:each) do
+      @feed, @feed_version = load_feed(feed_version_name: :feed_version_example_issues, import_level: 1)
+    end
+
+    context 'find_by_equivalent' do
+      before(:each) do
+        @test_issue = Issue.new(created_by_changeset: @feed_version.changesets_imported_from_this_feed_version.first,
+                              issue_type: 'stop_rsp_distance_gap')
+      end
+
+      it 'finds equivalent issue when entities with issues are matching' do
+        @test_issue.entities_with_issues << EntityWithIssues.new(entity_id: 1, entity_type: 'Stop', issue: @test_issue, entity_attribute: 'geometry')
+        @test_issue.entities_with_issues << EntityWithIssues.new(entity_id: 3, entity_type: 'RouteStopPattern', issue: @test_issue, entity_attribute: 'geometry')
+        expect(Issue.find_by_equivalent(@test_issue)).to eq Issue.first
+      end
+
+      it 'returns nil when entities with issues are not matching exactly' do
+        @test_issue.entities_with_issues << EntityWithIssues.new(entity_id: 1, entity_type: 'Stop', issue: @test_issue, entity_attribute: 'geometry')
+        expect(Issue.find_by_equivalent(@test_issue)).to be nil
+      end
+
+      it 'returns nil when Issue attributes are not matching' do
+        other_issue = Issue.new(created_by_changeset: @feed_version.changesets_imported_from_this_feed_version.first,
+                              issue_type: 'stop_position_inaccurate')
+        other_issue.entities_with_issues << EntityWithIssues.new(entity_id: 1, entity_type: 'Stop', issue: @test_issue, entity_attribute: 'geometry')
+        expect(Issue.find_by_equivalent(other_issue)).to be nil
+      end
+    end
+
+    it 'changeset_from_entities' do
+
+    end
   end
 end
