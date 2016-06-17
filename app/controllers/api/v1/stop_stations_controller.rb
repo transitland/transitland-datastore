dadca21f111a1a0d1d083c100e0b5f5c318d4e1c
Ian Rees <ian@ianrees.net>
2016-06-16 17:14:40 -0700

    Build out StopStation Controller

--- app/controllers/api/v1/stop_stations_controller.rb
+++ app/controllers/api/v1/stop_stations_controller.rb
@@ -1,33 +1,114 @@
-# == Schema Information
-#
-# Table name: current_stops
-#
-#  id                                 :integer          not null, primary key
-#  onestop_id                         :string
-#  geometry                           :geography({:srid geometry, 4326
-#  tags                               :hstore
-#  created_at                         :datetime
-#  updated_at                         :datetime
-#  name                               :string
-#  created_or_updated_in_changeset_id :integer
-#  version                            :integer
-#  identifiers                        :string           default([]), is an Array
-#  timezone                           :string
-#  last_conflated_at                  :datetime
-#  type                               :string
-#  parent_stop_id                     :integer
-#
-# Indexes
-#
-#  #c_stops_cu_in_changeset_id_index      (created_or_updated_in_changeset_id)
-#  index_current_stops_on_geometry        (geometry)
-#  index_current_stops_on_identifiers     (identifiers)
-#  index_current_stops_on_onestop_id      (onestop_id)
-#  index_current_stops_on_parent_stop_id  (parent_stop_id)
-#  index_current_stops_on_tags            (tags)
-#  index_current_stops_on_updated_at      (updated_at)
-#
+class Api::V1::StopStationsController < Api::V1::BaseApiController
+  include JsonCollectionPagination
+  include DownloadableCsv
+  include AllowFiltering
+  include Geojson
+  GEOJSON_ENTITY_PROPERTIES = Proc.new { |properties, entity|
+    # title property to follow GeoJSON simple style spec
+    properties[:title] = entity.name
 
-class Api::V1::StopStationsController < Api::V1::StopsController
+    properties[:timezone] = entity.timezone
+    properties[:operators_serving_stop] = entity.operators.map(&:onestop_id).try(:uniq)
+    properties[:routes_serving_stop] = entity.routes.map(&:onestop_id).try(:uniq)
+  }
+
+  before_action :set_stop, only: [:show]
   SERIALIZER = StopStationSerializer
+
+  def index
+    @stops = Stop.where(type: nil)
+
+    @stops = AllowFiltering.by_onestop_id(@stops, params)
+    @stops = AllowFiltering.by_tag_keys_and_values(@stops, params)
+    @stops = AllowFiltering.by_identifer_and_identifier_starts_with(@stops, params)
+    @stops = AllowFiltering.by_updated_since(@stops, params)
+
+    if params[:served_by].present? || params[:servedBy].present?
+      # we previously allowed `servedBy`, so we'll continue to honor that for the time being
+      operator_onestop_ids = []
+      operator_onestop_ids += params[:served_by].split(',') if params[:served_by].present?
+      operator_onestop_ids += params[:servedBy].split(',') if params[:servedBy].present?
+      operator_onestop_ids.uniq!
+      @stops = @stops.served_by(operator_onestop_ids)
+    end
+    if [params[:lat], params[:lon]].map(&:present?).all?
+      point = Stop::GEOFACTORY.point(params[:lon], params[:lat])
+      r = params[:r] || 100 # meters TODO: move this to a more logical place
+      @stops = @stops.where{st_dwithin(geometry, point, r)}.order{st_distance(geometry, point)}
+    end
+    if params[:bbox].present?
+      @stops = @stops.geometry_within_bbox(params[:bbox])
+    end
+    if params[:import_level].present?
+      @stops = @stops.where_import_level(AllowFiltering.param_as_array(params, :import_level))
+    end
+
+    @stops = @stops.includes{[
+      operators_serving_stop,
+      operators_serving_stop.operator,
+      routes_serving_stop,
+      routes_serving_stop.route,
+      routes_serving_stop.route.operator,
+      imported_from_feeds,
+      imported_from_feed_versions,
+      stop_transfers,
+      stop_platforms,
+      stop_egresses
+    ]} # TODO: check performance against eager_load, joins, etc.
+
+    respond_to do |format|
+      format.json do
+        render paginated_json_collection(
+          @stops,
+          Proc.new { |params| api_v1_stops_url(params) },
+          params[:sort_key],
+          params[:sort_order],
+          params[:offset],
+          params[:per_page],
+          params[:total],
+          params.slice(
+            :identifier,
+            :identifier_starts_with,
+            :served_by,
+            :servedBy,
+            :lat,
+            :lon,
+            :r,
+            :bbox,
+            :onestop_id,
+            :tag_key,
+            :tag_value,
+            :import_level
+          )
+        )
+      end
+      format.geojson do
+        render json: Geojson.from_entity_collection(@stops, &GEOJSON_ENTITY_PROPERTIES)
+      end
+      format.csv do
+        return_downloadable_csv(@stops, 'stops')
+      end
+    end
+  end
+
+  def show
+    respond_to do |format|
+      format.json do
+        if self.class::SERIALIZER
+          render json: @stop, serializer: self.class::SERIALIZER
+        else
+          render json: @stop
+        end
+      end
+      format.geojson do
+        render json: Geojson.from_entity(@stop, &GEOJSON_ENTITY_PROPERTIES)
+      end
+    end
+  end
+
+  private
+
+  def set_stop
+    @stop = Stop.find_by_onestop_id!(params[:id])
+  end
 end
