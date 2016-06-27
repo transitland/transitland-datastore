68bb0691df9fdba978f2b23d38ea93ec244042a9
Alexander Smith <alex@mapzen.com>
2016-06-27 13:28:14 -0700

    stops_served_by_route in geojson and spec

--- app/controllers/api/v1/routes_controller.rb
+++ app/controllers/api/v1/routes_controller.rb
@@ -12,6 +12,7 @@ class Api::V1::RoutesController < Api::V1::BaseApiController
     properties[:color] = entity.color
     properties[:operated_by_onestop_id] = entity.operator.try(:onestop_id)
     properties[:operated_by_name] = entity.operator.try(:name)
+    properties[:stops_served_by_route] = entity.stops.map { |stop| {onestop_id: stop.onestop_id, name: stop.name } }
     properties[:route_stop_patterns_by_onestop_id] = entity.route_stop_patterns.map(&:onestop_id)
   }
 
--- spec/controllers/api/v1/routes_controller_spec.rb
+++ spec/controllers/api/v1/routes_controller_spec.rb
@@ -163,14 +163,15 @@ describe Api::V1::RoutesController do
 
   describe 'GET show' do
     context 'as JSON' do
-      it 'returns stops by OnestopID' do
+      it 'returns routes by OnestopID' do
         get :show, id: 'r-9q8y-richmond~dalycity~millbrae'
         expect_json_types({
           onestop_id: :string,
           geometry: :object,
           name: :string,
           created_at: :date,
-          updated_at: :date
+          updated_at: :date,
+          stops_served_by_route: :array
         })
         expect_json({ onestop_id: -> (onestop_id) {
           expect(onestop_id).to eq 'r-9q8y-richmond~dalycity~millbrae'
