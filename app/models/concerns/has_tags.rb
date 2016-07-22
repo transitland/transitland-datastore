122301268b90c8a19993ed1511ec85c3995d838f
Ian Rees <ian@ianrees.net>
2016-07-22 01:12:09 -0700

    guard against tags == nil

--- app/models/concerns/has_tags.rb
+++ app/models/concerns/has_tags.rb
@@ -6,7 +6,7 @@ module HasTags
     scope :with_tag, -> (key) { where("#{self.table_name}.tags ? '#{key}'") }
     scope :with_tag_equals, -> (key, value) { where("#{self.table_name}.tags -> '#{key}' = '#{value}'") }
     def before_validation_compact_tags
-      self.tags.compact!
+      self.tags.compact! if self.tags
     end
   end
 end
