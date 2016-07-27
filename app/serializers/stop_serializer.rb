57391311e2759dcc1def1b1c34ea95b78ea27395
Ian Rees <ian@ianrees.net>
2016-07-27 16:21:32 -0700

    Stop serializer: served_by_vehicle_types

--- app/serializers/stop_serializer.rb
+++ app/serializers/stop_serializer.rb
@@ -36,11 +36,17 @@ class StopSerializer < CurrentEntitySerializer
              :tags,
              :timezone,
              :osm_way_id,
+             :served_by_vehicle_types,
              :created_at,
              :updated_at
 
   has_many :operators_serving_stop
   has_many :routes_serving_stop
+
+  def served_by_vehicle_types
+    routes_serving_stop.map(&:route).map(&:vehicle_type).uniq
+  end
+
 end
 
 class StopPlatformSerializer < StopSerializer
