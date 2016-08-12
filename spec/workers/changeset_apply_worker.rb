7b57c87f61df9efc2584d566982858d60d3845c0
Ian Rees <ian@ianrees.net>
2016-08-11 17:06:34 -0700

    ChangesetApplyWorker spec

---
+++ spec/workers/changeset_apply_worker.rb
@@ -0,0 +1,23 @@
+describe ChangesetApplyWorker do
+  it 'applies a changeset' do
+    onestop_id = 's-9q9-test'
+    payload = {
+      changes: [
+        {
+          action: "createUpdate",
+          stop: {
+            onestopId: onestop_id,
+            name: '1st Ave. & Holloway St.',
+            timezone: 'America/Los_Angeles'
+          }
+        }
+      ]
+    }
+    changeset = create(:changeset, payload: payload)
+    Sidekiq::Testing.inline! do
+      ChangesetApplyWorker.perform_async(changeset.id)
+    end
+    expect(changeset.reload.applied).to eq(true)
+    expect(Stop.find_by_onestop_id(onestop_id)).to be_truthy
+  end
+end
