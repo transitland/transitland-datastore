c3267c06acd27f50c087713e639330722431a7cd
Ian Rees <ian@ianrees.net>
2016-08-15 15:04:18 -0700

    Entity#where_inactive scope

--- app/models/concerns/is_an_entity_imported_from_feeds.rb
+++ app/models/concerns/is_an_entity_imported_from_feeds.rb
@@ -25,5 +25,13 @@ module IsAnEntityImportedFromFeeds
         })
         .distinct
     }
+
+    scope :where_inactive, -> {
+      active_feed_version_ids = FeedVersion.where_active.select(:id).distinct.pluck(:id)
+      referenced_ids = EntityImportedFromFeed.where(entity_type: self, feed_version_id: active_feed_version_ids).select(:entity_id).distinct.pluck(:entity_id)
+      all_ids = self.select(:id).pluck(:id)
+      where(id: (all_ids - referenced_ids))
+    }
+
   end
 end
