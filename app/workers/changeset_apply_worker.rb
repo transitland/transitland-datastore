b01028307436fabecf74c93f6f9fad34109d6108
Ian Rees <ian@ianrees.net>
2016-08-16 16:46:24 -0700

    ChangesetApplyWorker sidekiq options

--- app/workers/changeset_apply_worker.rb
+++ app/workers/changeset_apply_worker.rb
@@ -1,5 +1,10 @@
 class ChangesetApplyWorker
   include Sidekiq::Worker
+  sidekiq_options unique: :until_and_while_executing,
+                  unique_job_expiration: 60 * 60, # 1 hour
+                  log_duplicate_payload: true,
+                  queue: :feed_eater,
+                  retry: false
 
   def perform(changeset_id)
     logger.info "ChangesetApplyWorker: #{changeset_id}"
