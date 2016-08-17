0efe656fa84f299226a66a01905a58dd45fbe964
Ian Rees <ian@ianrees.net>
2016-08-17 16:32:03 -0700

    ChangesetApplyWorker specs

--- spec/workers/changeset_apply_worker_spec.rb
+++ spec/workers/changeset_apply_worker_spec.rb
@@ -1,23 +1,53 @@
 describe ChangesetApplyWorker do
-  it 'applies a changeset' do
-    onestop_id = 's-9q9-test'
+
+  before(:each) do
     payload = {
       changes: [
         {
           action: "createUpdate",
           stop: {
-            onestopId: onestop_id,
+            onestopId:  's-9q9-test',
             name: '1st Ave. & Holloway St.',
             timezone: 'America/Los_Angeles'
           }
         }
       ]
     }
-    changeset = create(:changeset, payload: payload)
-    Sidekiq::Testing.inline! do
-      ChangesetApplyWorker.perform_async(changeset.id, 'test')
-    end
-    expect(changeset.reload.applied).to eq(true)
-    expect(Stop.find_by_onestop_id(onestop_id)).to be_truthy
+    @changeset = create(:changeset, payload: payload)
+    @cachekey = "changesets/#{@changeset.id}/apply_async"
+    Rails.cache.delete(@cachekey)
   end
+
+  after(:each) do
+    Rails.cache.delete(@cachekey)
+  end
+
+  it 'applies a changeset' do
+    Sidekiq::Testing.inline! do
+      ChangesetApplyWorker.perform_async(@changeset.id, @cachekey)
+    end
+    expect(@changeset.reload.applied).to eq(true)
+  end
+
+  it 'writes status' do
+    Sidekiq::Testing.inline! do
+      ChangesetApplyWorker.perform_async(@changeset.id, @cachekey)
+    end
+    cachedata = Rails.cache.fetch(@cachekey)
+    expect(cachedata[:status]).to eq('complete')
+  end
+
+  it 'returns errors' do
+    # missing timezone
+    payload = {changes: [{action: 'createUpdate', stop: {onestopId: 's-9q9-test'}}]}
+    @changeset.change_payloads.first.update(payload: payload)
+    Sidekiq::Testing.inline! do
+      ChangesetApplyWorker.perform_async(@changeset.id, @cachekey)
+    end
+    cachedata = Rails.cache.fetch(@cachekey)
+    expect(cachedata[:status]).to eq('error')
+    expect(cachedata[:errors].size).to eq(1)
+    expect(cachedata[:errors].first[:exception]).to eq('ChangesetError')
+  end
+
 end
