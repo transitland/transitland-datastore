519c41619d31fad59975d62113822bc69361a7ac
Alexander Smith <alex@mapzen.com>
2016-09-02 19:11:01 -0700

    adding warning comment

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -40,6 +40,7 @@ class Issue < ActiveRecord::Base
   end
 
   def outdated?
+    # This can create false negatives if different changesets w/ same entities are made less than 1 second apart.
     entities_with_issues.any? { |ewi| ewi.entity.created_or_updated_in_changeset.updated_at.to_i > created_by_changeset.applied_at.to_i}
   end
 
