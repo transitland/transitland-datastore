b859ff6d1cf4ea0f0f9b068cb37c5d7e57aeaac7
Ian Rees <ian@ianrees.net>
2016-09-12 16:25:36 -0700

    FeedMaintenanceService spec

---
+++ spec/services/feed_maintenance_service_spec.rb
@@ -0,0 +1,31 @@
+describe FeedMaintenanceService do
+  before(:each) do
+    @feed_version = create(:feed_version,
+      earliest_calendar_date: '2016-01-01',
+      latest_calendar_date: '2016-06-01'
+    )
+    @feed_version.feed.update!(active_feed_version: @feed_version)
+  end
+
+  context '.extend_feed_version' do
+    it 'extends a feed' do
+      FeedMaintenanceService.extend_feed_version(
+        @feed_version,
+        extend_from_date: '2016-05-01',
+        extend_to_date: '2016-12-31'
+      )
+      @feed_version.reload
+      expect(@feed_version.tags['extend_from_date']).to eq('2016-05-01')
+      expect(@feed_version.tags['extend_to_date']).to eq('2016-12-31')
+    end
+
+    it 'defaults to -1 month, +1 year' do
+      FeedMaintenanceService.extend_feed_version(
+        @feed_version
+      )
+      @feed_version.reload
+      expect(@feed_version.tags['extend_from_date']).to eq('2016-05-01')
+      expect(@feed_version.tags['extend_to_date']).to eq('2017-06-01')
+    end
+  end
+end
