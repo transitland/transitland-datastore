55564620bc99c523eed44f3ed6ab2ad5323a8c4f
Ian Rees <ian@ianrees.net>
2016-09-13 11:22:20 -0700

    Move maintenance task to FeedMaintenanceService

--- app/services/feed_maintenance_service.rb
+++ app/services/feed_maintenance_service.rb
@@ -9,7 +9,40 @@ class FeedMaintenanceService
     Rails.logger
   end
 
-  def self.extend_expired_feed_versions(expired_on_date)
+  def self.enqueue_next_feed_versions(date, import_level: nil, max_imports: nil)
+    # Find feed versions that can be updated
+    queue = []
+    Feed.find_each do |feed|
+      # Enqueue FeedEater job for feed.find_next_feed_version
+      # Skip this feed is tag 'manual_import' is 'true'
+      next if feed.tags['manual_import'] == 'true'
+      # Use the previous import_level, or default to 2
+      import_level ||= feed.active_feed_version.try(:import_level) || 2
+      # Find the next feed_version
+      next_feed_version = feed.find_next_feed_version(date)
+      next unless next_feed_version
+      # Return if it's been imported before
+      next if next_feed_version.feed_version_imports.last
+      # Enqueue
+      queue << [feed, next_feed_version, import_level]
+    end
+    # The maximum number of feeds to enqueue
+    max_imports ||= queue.size
+    logger.info "enqueue_next_feed_versions: found #{queue.size} feeds to update; max_imports = #{max_imports}"
+    # Sort by last_imported_at, asc.
+    queue = queue.sort_by { |feed, _, _| feed.last_imported_at }.first(max_imports)
+    # Enqueue
+    queue.each do |feed, next_feed_version, import_level|
+      logger.info "enqueue_next_feed_versions: adding #{feed.onestop_id} #{next_feed_version.sha1} #{import_level}"
+      FeedEaterWorker.perform_async(
+        feed.onestop_id,
+        next_feed_version.sha1,
+        import_level
+      )
+    end
+  end
+
+  def self.extend_expired_feed_versions(expired_on_date: nil)
     expired_on_date ||= (DateTime.now + DEFAULT_EXPIRED_ON_DATE)
     feed_versions = FeedVersion.where_active.where('latest_calendar_date <= ?', expired_on_date)
     feed_versions.each do |feed_version|
