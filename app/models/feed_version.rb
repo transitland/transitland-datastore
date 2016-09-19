19a883c6f8dd7423e1b722db13fbaa1b23c9cc0a
Ian Rees <ian@ianrees.net>
2016-09-19 15:10:07 -0700

    FeedVersion: include HasTags concern

--- app/models/feed_version.rb
+++ app/models/feed_version.rb
@@ -27,6 +27,8 @@
 #
 
 class FeedVersion < ActiveRecord::Base
+  include HasTags
+
   belongs_to :feed, polymorphic: true
   has_many :feed_version_imports, -> { order 'created_at DESC' }, dependent: :destroy
   has_many :changesets_imported_from_this_feed_version, class_name: 'Changeset'
