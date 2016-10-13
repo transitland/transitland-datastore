569a22a360cc4375df3ad9a7d5fbc4319da579b0
Ian Rees <ian@ianrees.net>
2016-10-12 23:28:30 -0700

    Tidy

--- app/controllers/concerns/json_collection_pagination.rb
+++ app/controllers/concerns/json_collection_pagination.rb
@@ -4,38 +4,44 @@ module JsonCollectionPagination
   SERIALIZER = nil
 
   def paginated_json_collection(collection, path_helper, sort_key, sort_order, offset, per_page, total, params)
-    params ||= {}
-    params[:sort_key] = sort_key
-    params[:sort_order] = sort_order
-    params[:offset] = offset
-    params[:per_page] = per_page
-    params[:total] = total
-    self.params = params
+    # Backwards compatibility
+    self.params ||= {}
+    self.params[:sort_key] = sort_key
+    self.params[:sort_order] = sort_order
+    self.params[:offset] = offset
+    self.params[:per_page] = per_page
+    self.params[:total] = total
     paginated_json_collection_new(collection)
   end
 
   def paginated_json_collection_new(collection)
-    collection = sort_collection(collection)
-
     # Meta
     per_page = sort_per_page
     offset = sort_offset
     include_total = sort_total
-    meta = sort_meta
+    meta = {
+      sort_key: sort_key,
+      sort_order: sort_order,
+      offset: offset,
+      per_page: per_page
+    }
 
+    # Reorder
+    collection = sort_reorder(collection)
+
+    # Setup prev/next links
     if per_page
       data = collection.offset(offset).limit(per_page+1).to_a
       # Previous and next page
-      if offset > 0
-        meta[:prev] = url_for(query_params.merge(meta).merge({
-          offset: (offset - per_page) >= 0 ? (offset - per_page) : 0,
-        }))
-      end
-      if data.size > per_page
-        meta[:next] = url_for(query_params.merge(meta).merge({
-          offset: offset + per_page,
-        }))
-      end
+      meta_prev = url_for(query_params.merge(meta).merge({
+        offset: (offset - per_page) >= 0 ? (offset - per_page) : 0,
+      }))
+      meta_next = url_for(query_params.merge(meta).merge({
+        offset: offset + per_page,
+      }))
+      (meta[:prev] = meta_prev) if offset > 0
+      (meta[:next] = meta_next) if data.size > per_page
+      # Slice data
       data_on_page = data[0...per_page]
     else
       data_on_page = collection
@@ -67,19 +73,10 @@ module JsonCollectionPagination
     params[:sort_order].to_s == 'desc' ? :desc : :asc
   end
 
-  def sort_collection(collection)
+  def sort_reorder(collection)
     key = sort_key
     fail ArgumentError.new('Invalid sort_key') unless collection.column_names.include?(key.to_s)
     collection.reorder(key => sort_order)
-  end
-
-  def sort_meta
-    {
-      sort_key: sort_key,
-      sort_order: sort_order,
-      offset: sort_offset,
-      per_page: sort_per_page
-    }
   end
 
   def sort_offset
