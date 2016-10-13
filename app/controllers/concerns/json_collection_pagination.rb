434bc0964d05500c743ba02823fdf835ccfbff92
Ian Rees <ian@ianrees.net>
2016-10-12 23:07:00 -0700

    Backwards compat

--- app/controllers/concerns/json_collection_pagination.rb
+++ app/controllers/concerns/json_collection_pagination.rb
@@ -4,68 +4,14 @@ module JsonCollectionPagination
   SERIALIZER = nil
 
   def paginated_json_collection(collection, path_helper, sort_key, sort_order, offset, per_page, total, params)
-    #changing from order to reorder to discard previous ordering
-    sort_key = (sort_key.presence || :id).to_sym
-    sort_order = sort_order.to_s == 'desc' ? :desc : :asc
-    fail ArgumentError.new('Invalid sort_key') unless collection.column_names.include?(sort_key.to_s)
-    collection = collection.reorder(sort_key => sort_order)
-
-    # Meta
-    offset = (offset.presence || 0).to_i
-    include_total = (total == true || total == 'true')
-    meta = {
-        sort_key: sort_key,
-        sort_order: sort_order,
-        offset: offset
-    }
-
-    if [false, 'false', '∞'].include?(per_page)
-      # allow pagination to be disabled
-      # worst case: the query takes longer than 2 minutes and is
-      # killed at the database and the load balancer
-      meta[:per_page] = per_page
-      data_on_page = collection
-    else
-      per_page = (per_page.presence || self.class::PER_PAGE).to_i
-      meta[:per_page] = per_page
-      # Get the current page of results.
-      #  Add +1 to limit to see if there is a next page.
-      #  This will be dropped in the return.
-      data = collection.offset(offset).limit(per_page+1).to_a
-
-      # Previous and next page
-      if offset > 0
-        meta[:prev] = path_helper.call(params.merge({
-          sort_key: sort_key,
-          sort_order: sort_order,
-          offset: (offset - per_page) >= 0 ? (offset - per_page) : 0,
-          per_page: per_page,
-          total: total
-        }))
-      end
-      if data.size > per_page
-        meta[:next] = path_helper.call(params.merge({
-          sort_key: sort_key,
-          sort_order: sort_order,
-          offset: offset + per_page,
-          per_page: per_page,
-          total: total
-        }))
-      end
-
-      data_on_page = data[0...per_page]
-    end
-
-    if include_total
-      meta[:total] = collection.count
-    end
-
-    # Return results + meta
-    if self.class::SERIALIZER
-      { json: data_on_page, meta: meta, each_serializer: self.class::SERIALIZER }
-    else
-      { json: data_on_page, meta: meta }
-    end
+    params ||= {}
+    params[:sort_key] = sort_key
+    params[:sort_order] = sort_order
+    params[:offset] = offset
+    params[:per_page] = per_page
+    params[:total] = total
+    self.params = params
+    paginated_json_collection_new(collection)
   end
 
   def paginated_json_collection_new(collection, path_helper)
@@ -110,7 +56,7 @@ module JsonCollectionPagination
   private
 
   def query_params
-    ['asd']
+    params.slice()
   end
 
   def sort_key
