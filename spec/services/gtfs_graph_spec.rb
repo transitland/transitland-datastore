86b44a2efe5f0a57c64e93a0ad3ff2e72e95f4f6
Ian Rees <ian@ianrees.net>
2016-10-28 18:00:46 -0700

    spec update

--- spec/services/gtfs_graph_spec.rb
+++ spec/services/gtfs_graph_spec.rb
@@ -280,7 +280,7 @@ describe GTFSGraph do
       expect(s.frequency_start_time).to eq('6:00:00')
       expect(s.frequency_end_time).to eq('7:59:59')
       expect(s.frequency_headway_seconds).to eq(1800)
-      expect(s.frequency_exact_times).to eq(nil)
+      expect(s.frequency_type).to eq('window')
     end
 
     it 'can process a trip with 1 unique stop but at least 2 stop times' do
