83062648029662c3a1d5d0a71adcada5dc4a2149
Ian Rees <ian@ianrees.net>
2016-11-23 15:23:51 -0800

    -> { distinct }

--- app/models/concerns/is_an_entity_imported_from_feeds.rb
+++ app/models/concerns/is_an_entity_imported_from_feeds.rb
@@ -2,8 +2,9 @@ module IsAnEntityImportedFromFeeds
   extend ActiveSupport::Concern
   included do
     has_many :entities_imported_from_feed, as: :entity
-    has_many :imported_from_feeds, through: :entities_imported_from_feed, source: :feed
-    has_many :imported_from_feed_versions, through: :entities_imported_from_feed, source: :feed_version
+    has_many :imported_from_feeds, -> { distinct }, through: :entities_imported_from_feed, source: :feed
+    has_many :imported_from_feed_versions, -> { distinct }, through: :entities_imported_from_feed, source: :feed_version
+
     scope :where_import_level, -> (import_level) {
       joins(:entities_imported_from_feed)
         .where(entities_imported_from_feed: {
