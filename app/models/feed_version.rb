75d046ebf95560669a0cb7813dad22af4f7e0b8b
Ian Rees <ian@ianrees.net>
2016-11-29 12:54:07 -0800

    FeedVersion calendar scopes

--- app/models/feed_version.rb
+++ app/models/feed_version.rb
@@ -52,6 +52,22 @@ class FeedVersion < ActiveRecord::Base
     joins('INNER JOIN current_feeds ON feed_versions.id = current_feeds.active_feed_version_id')
   }
 
+  scope :calendar_coverage_begins_at_or_before, -> (date) {
+    date = date.is_a?(Date) ? date : Date.parse(date)
+    where('earliest_calendar_date <= ?', date)
+  }
+
+  scope :calendar_coverage_begins_at_or_after, -> (date) {
+    date = date.is_a?(Date) ? date : Date.parse(date)
+    where('earliest_calendar_date >= ?', date)
+  }
+
+  scope :calendar_coverage_includes, -> (date) {
+    date = date.is_a?(Date) ? date : Date.parse(date)
+    where('earliest_calendar_date <= ?', date)
+      .where('latest_calendar_date >= ?', date)
+  }
+
   def succeeded(timestamp)
     self.update(imported_at: timestamp)
     self.feed.update(last_imported_at: self.imported_at)
