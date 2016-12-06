386fa4ecbf8458c63e671061cacf1961d5682961
Ian Rees <ian@ianrees.net>
2016-12-06 14:14:26 -0800

    Update EIFF serializer

--- app/serializers/entity_serializer.rb
+++ app/serializers/entity_serializer.rb
@@ -1,7 +1,12 @@
 class EntitySerializer < ApplicationSerializer
   attributes :identifiers,
+             :imported_from_feeds,
              :imported_from_feed_onestop_ids,
              :imported_from_feed_version_sha1s
+
+  def imported_from_feeds
+    object.imported_from_feeds.map { |eiff| {feed_onestop_id: eiff.feed.onestop_id, feed_version_sha1: eiff.feed_version.sha1, gtfs_id: eiff.gtfs_id} }
+  end
 
   def imported_from_feed_onestop_ids
     object.imported_from_feeds.map(&:onestop_id).uniq
