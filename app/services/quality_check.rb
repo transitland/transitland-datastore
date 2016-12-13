93a3c7c8be2693084723579d54540df84c1e6830
Alexander Smith <alex@mapzen.com>
2016-12-13 10:47:16 -0800

    consecutive stop geometry check and spec

--- app/services/quality_check.rb
+++ app/services/quality_check.rb
@@ -74,6 +74,7 @@ end
 class QualityCheck::GeometryQualityCheck < QualityCheck
 
   LAST_STOP_DISTANCE_LENIENCY = 5.0 #meters
+  MINIMUM_DIST_BETWEEN_STOP_PARENTS = 10.0
 
   # some aggregate stats on rsp distance calculation
   attr_accessor :distance_issues, :distance_issue_tests
@@ -90,11 +91,11 @@ class QualityCheck::GeometryQualityCheck < QualityCheck
     self.distance_issue_tests = 0
 
     import = !self.changeset.imported_from_feed.nil?
-    distance_rsps = Set.new
+    rsps_to_evaluate = Set.new
     stop_rsp_gap_pairs =  Set.new
 
     self.changeset.route_stop_patterns_created_or_updated.each do |rsp|
-      distance_rsps << rsp.onestop_id
+      rsps_to_evaluate << rsp.onestop_id
       Stop.where(onestop_id: rsp.stop_pattern).each do |stop|
         stop_rsp_gap_pairs << [rsp.onestop_id, stop.onestop_id]
       end
@@ -111,7 +112,7 @@ class QualityCheck::GeometryQualityCheck < QualityCheck
     self.changeset.stops_created_or_updated.each do |stop|
       unless import
         RouteStopPattern.where{ stop_pattern.within(stop.onestop_id) }.each do |rsp|
-          distance_rsps << rsp.onestop_id
+          rsps_to_evaluate << rsp.onestop_id
           stop_rsp_gap_pairs << [rsp.onestop_id, stop.onestop_id]
         end
       end
@@ -125,11 +126,12 @@ class QualityCheck::GeometryQualityCheck < QualityCheck
       # other checks on stop-exclusive attributes go here
     end
 
-    distance_rsps.each do |onestop_id|
+    rsps_to_evaluate.each do |onestop_id|
       rsp = RouteStopPattern.find_by_onestop_id!(onestop_id)
       # handle the case of 1 stop trips
       next if rsp.geometry[:coordinates].uniq.size == 1
       rsp.stop_pattern.each_index do |i|
+        self.stop_compare(rsp, i)
         self.stop_distance(rsp, i)
       end
     end
@@ -141,7 +143,7 @@ class QualityCheck::GeometryQualityCheck < QualityCheck
       self.stop_rsp_distance_gap(stop, rsp) unless rsp.geometry[:coordinates].uniq.size == 1
     end
 
-    self.distance_issue_tests = distance_rsps.map {|onestop_id| RouteStopPattern.find_by_onestop_id!(onestop_id).stop_pattern.size }.reduce(:+)
+    self.distance_issue_tests = rsps_to_evaluate.map {|onestop_id| RouteStopPattern.find_by_onestop_id!(onestop_id).stop_pattern.size }.reduce(:+)
     self.distance_issues = Set.new(self.issues.select {|ewi| ['stop_rsp_distance_gap', 'distance_calculation_inaccurate'].include?(ewi.issue_type) }.each {|issue| issue.entities_with_issues.map(&:entity_id) }).size
     distance_score
 
@@ -156,6 +158,21 @@ class QualityCheck::GeometryQualityCheck < QualityCheck
       issue.entities_with_issues.new(entity: stop, issue: issue, entity_attribute: 'geometry')
       issue.entities_with_issues.new(entity: rsp, issue: issue, entity_attribute: 'geometry')
       self.issues << issue
+    end
+  end
+
+  def stop_compare(rsp, index)
+    if (index != 0)
+      stop1 = Stop.find_by_onestop_id!(rsp.stop_pattern[index])
+      stop2 = Stop.find_by_onestop_id!(rsp.stop_pattern[index-1])
+      if (stop1[:geometry].distance(stop2[:geometry]) < MINIMUM_DIST_BETWEEN_STOP_PARENTS)
+        issue = Issue.new(created_by_changeset: self.changeset,
+                          issue_type: 'stop_position_inaccurate',
+                          details: "RouteStopPattern #{rsp.onestop_id}. Stop #{stop1.onestop_id}, pos #{index-1}, has a geometry (#{stop1[:geometry].to_s}) too close to stop #{stop2.onestop_id}, pos #{index},  geometry (#{stop2[:geometry].to_s})")
+        issue.entities_with_issues.new(entity: stop1, issue: issue, entity_attribute: 'geometry')
+        issue.entities_with_issues.new(entity: stop2, issue: issue, entity_attribute: 'geometry')
+        self.issues << issue
+      end
     end
   end
 
