3abce3b6a9cd8831734e968533b026fe8c0576af
Ian Rees <ian@ianrees.net>
2016-12-23 14:07:01 -0800

    Geometry specs

--- spec/models/concerns/has_a_geographic_geometry_spec.rb
+++ spec/models/concerns/has_a_geographic_geometry_spec.rb
@@ -1,9 +1,56 @@
 describe HasAGeographicGeometry do
-  before(:each) do
-  end
+  let (:geometry_point) {
+    {
+      type: "Point",
+      coordinates: [
+        -122.39327430725099,
+        37.78991914361586
+      ]
+    }
+  }
+  let (:geometry_polygon) {
+    {
+      type: "Polygon",
+      coordinates: [
+        [
+          [
+            -122.39363908767702,
+            37.78973685348181
+          ],
+          [
+            -122.39363908767702,
+            37.79004420286635
+          ],
+          [
+            -122.39303827285767,
+            37.79004420286635
+          ],
+          [
+            -122.39303827285767,
+            37.78973685348181
+          ],
+          [
+            -122.39363908767702,
+            37.78973685348181
+          ]
+        ]
+      ]
+    }
+  }
 
   context '.convex_hull' do
 
+  end
+
+  context '.geometry_from_geojson' do
+    it 'converts from string' do
+      g = HasAGeographicGeometry.geometry_from_geojson(JSON.dump(geometry_point))
+      expect(g.geometry_type).to eq(RGeo::Feature::Point)
+    end
+    it 'converts from hash' do
+      g = HasAGeographicGeometry.geometry_from_geojson(geometry_point)
+      expect(g.geometry_type).to eq(RGeo::Feature::Point)
+    end
   end
 
   context '#geometry' do
@@ -11,7 +58,7 @@ describe HasAGeographicGeometry do
   end
 
   context '#geometry_centroid' do
-    
+
   end
 
   context '#validate' do
@@ -19,11 +66,33 @@ describe HasAGeographicGeometry do
   end
 
   context '#validate_geometry_point' do
-
+    it 'requires point' do
+      s = build(:stop, geometry: geometry_point)
+      expect(s.send(:validate_geometry_point)).to be true
+    end
+    it 'returns false otherwise' do
+      s = build(:stop, geometry: geometry_polygon)
+      expect(s.send(:validate_geometry_point)).to be false
+    end
+    it 'returns false if nil' do
+      s = build(:stop, geometry: nil)
+      expect(s.send(:validate_geometry_point)).to be false
+    end
   end
 
   context '#validate_geometry_polygon' do
-
+    it 'requires polygon' do
+      s = build(:stop, geometry: geometry_polygon)
+      expect(s.send(:validate_geometry_polygon)).to be true
+    end
+    it 'returns false otherwise' do
+      s = build(:stop, geometry: geometry_point)
+      expect(s.send(:validate_geometry_polygon)).to be false
+    end
+    it 'returns false if nil' do
+      s = build(:stop, geometry: nil)
+      expect(s.send(:validate_geometry_polygon)).to be false
+    end
   end
 
 end
