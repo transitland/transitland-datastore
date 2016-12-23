4d1dd863f6625ef33b3fa9743239167fd3ec8d5b
Ian Rees <ian@ianrees.net>
2016-12-23 13:32:48 -0800

    Initial spec

---
+++ spec/models/concerns/has_a_geographic_geometry_spec.rb
@@ -0,0 +1,29 @@
+describe HasAGeographicGeometry do
+  before(:each) do
+  end
+
+  context '.convex_hull' do
+
+  end
+
+  context '#geometry' do
+
+  end
+
+  context '#geometry_centroid' do
+    
+  end
+
+  context '#validate' do
+
+  end
+
+  context '#validate_geometry_point' do
+
+  end
+
+  context '#validate_geometry_polygon' do
+
+  end
+
+end
