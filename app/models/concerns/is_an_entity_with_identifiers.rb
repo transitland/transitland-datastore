1a13ff4541abfa137de34088b28584ec61165d0d
Ian Rees <ian@ianrees.net>
2017-01-13 21:55:31 -0800

    Add attr_reader back in

--- app/models/concerns/is_an_entity_with_identifiers.rb
+++ app/models/concerns/is_an_entity_with_identifiers.rb
@@ -2,6 +2,7 @@ module IsAnEntityWithIdentifiers
   extend ActiveSupport::Concern
 
   included do
+    attr_reader :identified_by, :not_identified_by
     scope :with_identifier, -> (search_string) { where{identifiers.within(search_string)} }
     scope :with_identifier_or_name, -> (search_string) { where{(identifiers.within(search_string)) | (name =~ search_string)} }
     scope :with_identifier_starting_with, -> (search_string) { where("'|' || array_to_string(identifiers, '|') LIKE ?", "%|#{search_string}%") }
