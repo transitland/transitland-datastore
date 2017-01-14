c91fda890cfc6b9dad567bd1af30a147f4784c6e
Ian Rees <ian@ianrees.net>
2017-01-13 16:10:20 -0800

    Temp disable identifier before/after save

--- app/models/concerns/is_an_entity_with_identifiers.rb
+++ app/models/concerns/is_an_entity_with_identifiers.rb
@@ -8,10 +8,10 @@ module IsAnEntityWithIdentifiers
     scope :with_identifier_or_name, -> (search_string) { where{(identifiers.within(search_string)) | (name =~ search_string)} }
     scope :with_identifier_starting_with, -> (search_string) { where("'|' || array_to_string(identifiers, '|') LIKE ?", "%|#{search_string}%") }
 
-    def self.before_create_making_history(new_model, changeset)
-      new_model.identifiers = new_model.identified_by
-      true
-    end
+    # def self.before_create_making_history(new_model, changeset)
+    #   new_model.identifiers = new_model.identified_by
+    #   true
+    # end
   end
 
   def add_identifier(identifier)
@@ -24,14 +24,14 @@ module IsAnEntityWithIdentifiers
     self.not_identified_by << identifier
   end
 
-  def before_update_making_history(changeset)
-    current_identifiers = self.identifiers.try(:dup) || []
-    identifiers_to_add = self.try(:identified_by) || []
-    identifiers_to_remove = self.try(:not_identified_by) || []
-    current_identifiers += identifiers_to_add if identifiers_to_add.length > 0
-    current_identifiers -= identifiers_to_remove if identifiers_to_remove.length > 0
-    current_identifiers.uniq!
-    self.identifiers = current_identifiers
-    true
-  end
+  # def before_update_making_history(changeset)
+  #   current_identifiers = self.identifiers.try(:dup) || []
+  #   identifiers_to_add = self.try(:identified_by) || []
+  #   identifiers_to_remove = self.try(:not_identified_by) || []
+  #   current_identifiers += identifiers_to_add if identifiers_to_add.length > 0
+  #   current_identifiers -= identifiers_to_remove if identifiers_to_remove.length > 0
+  #   current_identifiers.uniq!
+  #   self.identifiers = current_identifiers
+  #   true
+  # end
 end
