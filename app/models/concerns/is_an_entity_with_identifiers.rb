0c6bcd0a9b709adb553dbfcc67310e79a0834506
Ian Rees <ian@ianrees.net>
2017-01-13 17:45:15 -0800

    Identifiers cleanup

--- app/models/concerns/is_an_entity_with_identifiers.rb
+++ app/models/concerns/is_an_entity_with_identifiers.rb
@@ -2,36 +2,16 @@ module IsAnEntityWithIdentifiers
   extend ActiveSupport::Concern
 
   included do
-    attr_accessor :identified_by, :not_identified_by
-
     scope :with_identifier, -> (search_string) { where{identifiers.within(search_string)} }
     scope :with_identifier_or_name, -> (search_string) { where{(identifiers.within(search_string)) | (name =~ search_string)} }
     scope :with_identifier_starting_with, -> (search_string) { where("'|' || array_to_string(identifiers, '|') LIKE ?", "%|#{search_string}%") }
-
-    # def self.before_create_making_history(new_model, changeset)
-    #   new_model.identifiers = new_model.identified_by
-    #   true
-    # end
   end
 
-  def add_identifier(identifier)
-    self.identified_by ||= Set.new
-    self.identified_by << identifier
+  def identified_by=(values)
+    self.identifiers = (self.identifiers + values).uniq
   end
 
-  def remove_identifier(identifier)
-    self.not_identified_by ||= Set.new
-    self.not_identified_by << identifier
+  def not_identified_by=(values)
+    self.identifiers = (self.identifiers - values).uniq
   end
-
-  # def before_update_making_history(changeset)
-  #   current_identifiers = self.identifiers.try(:dup) || []
-  #   identifiers_to_add = self.try(:identified_by) || []
-  #   identifiers_to_remove = self.try(:not_identified_by) || []
-  #   current_identifiers += identifiers_to_add if identifiers_to_add.length > 0
-  #   current_identifiers -= identifiers_to_remove if identifiers_to_remove.length > 0
-  #   current_identifiers.uniq!
-  #   self.identifiers = current_identifiers
-  #   true
-  # end
 end
