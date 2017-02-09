2d40509dc7d3d52a2a36eee16a46e26ee600f4f8
Ian Rees <ian@ianrees.net>
2017-02-08 16:22:54 -0800

    has_many issues

--- app/models/concerns/is_an_entity_with_issues.rb
+++ app/models/concerns/is_an_entity_with_issues.rb
@@ -2,5 +2,6 @@ module IsAnEntityWithIssues
   extend ActiveSupport::Concern
   included do
     has_many :entities_with_issues, as: :entity, dependent: :destroy
+    has_many :issues, -> { distinct }, through: :entities_with_issues, source: :issue
   end
 end
