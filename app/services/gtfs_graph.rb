f0c34b9f1039c75ef7b5f2bbd2b665360f6e8756
Ian Rees <ian@ianrees.net>
2017-02-14 20:07:16 -0800

    Make sure all Stops referenced by a transfer are included

--- app/services/gtfs_graph.rb
+++ app/services/gtfs_graph.rb
@@ -100,6 +100,7 @@ class GTFSGraph
       stop = @onestop_id_to_entity[stop_onestop_id]
       stops << stop
       stops << @onestop_id_to_entity[stop.parent_stop_onestop_id] if stop.parent_stop_onestop_id
+      stop.includes_stop_transfers.each { |ist| puts "STOP TRANSFER: #{ist[:toStopOnestopId]}"; stops << @onestop_id_to_entity[ist[:toStopOnestopId]] }
     }}
 
     # Update route geometries
