cc60256ee0f0fec2995fd59e6d6d9c3f54e91071
Alexander Smith <alex@mapzen.com>
2017-02-14 18:59:25 -0800

    adding keyword arg to ssp apply_params

--- app/models/schedule_stop_pair.rb
+++ app/models/schedule_stop_pair.rb
@@ -258,7 +258,7 @@ class ScheduleStopPair < BaseScheduleStopPair
       find(attrs[:id])
     end
   end
-  def self.apply_params(params, cache={})
+  def self.apply_params(params, cache={}, changeset: nil)
     params = super(params, cache)
     {
       origin_onestop_id: :origin,
