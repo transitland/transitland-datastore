a20c57202a054a774bacdedc499a92b441410e9d
Alexander Smith <alex@mapzen.com>
2017-02-16 23:00:02 -0800

    can no longer update current entity attrs using old entity onestop id

--- app/models/concerns/current_tracked_by_changeset.rb
+++ app/models/concerns/current_tracked_by_changeset.rb
@@ -36,7 +36,7 @@ module CurrentTrackedByChangeset
       existing_model = find_existing_model(change)
       attrs_to_apply = apply_params(change, cache, changeset: changeset)
       if existing_model
-        if existing_model.onestop_id.eql?(change[:onestop_id]) || !changeset.import?
+        if existing_model.onestop_id.eql?(change[:onestop_id])
           existing_model.update_making_history(changeset: changeset, new_attrs: attrs_to_apply.update({ 'onestop_id': existing_model.onestop_id }))
         end
       else
