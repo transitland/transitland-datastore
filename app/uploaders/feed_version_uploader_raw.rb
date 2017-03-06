a97c920857a67cbf98282a80ca5cc182017fe8b8
Drew Dara-Abrams <drew@mapzen.com>
2017-03-06 09:58:24 -0800

    fixes for new version of Carrierwave

--- app/uploaders/feed_version_uploader_raw.rb
+++ app/uploaders/feed_version_uploader_raw.rb
@@ -1,6 +1,6 @@
 class FeedVersionUploaderRaw < FeedVersionUploader
   def filename
     return unless file
-    "#{model.sha1}-raw.#{file.extension}"
+    "#{model.sha1}-raw.#{file.extension}" if original_filename.present?
   end
 end
