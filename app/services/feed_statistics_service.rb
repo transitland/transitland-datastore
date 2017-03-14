fcc97016a71008c064af80a2fbc1dc8c5c96922e
Ian Rees <ian@ianrees.net>
2017-03-13 17:34:47 -0700

    ColumnStatistics

--- app/services/feed_statistics_service.rb
+++ app/services/feed_statistics_service.rb
@@ -1,3 +1,37 @@
+class ColumnStatistics
+  attr_accessor :name
+  attr_accessor :instance_variable
+  attr_accessor :total
+  attr_accessor :unique
+
+  def initialize(name)
+    self.name = name.to_s.sub('@','').to_sym
+    self.instance_variable = name
+    self.total = 0
+    self.unique = Set.new
+  end
+
+  def add_value(value)
+    return if value.nil?
+    self.total += 1
+    self.unique << value
+  end
+
+  def add(entity)
+    self.add_value(entity.instance_variable_get(self.instance_variable))
+  end
+
+  def output
+    sorted_values = self.unique.sort
+    {
+      total: self.total,
+      unique: sorted_values.size,
+      min: sorted_values.first,
+      max: sorted_values.last
+    }
+  end
+end
+
 class FeedStatisticsService
   include Singleton
 
