c6adc8529c0516b0ba0343b898504fd7ad26fc88
Ian Rees <ian@ianrees.net>
2017-03-17 16:01:15 -0700

    Annotation

--- spec/factories/feed_version_info_factory.rb
+++ spec/factories/feed_version_info_factory.rb
@@ -2,6 +2,13 @@
 #
 # Table name: feed_version_infos
 #
+#  id                :integer          not null, primary key
+#  statistics        :json
+#  scheduled_service :json
+#  filenames         :string           is an Array
+#  created_at        :datetime
+#  updated_at        :datetime
+#
 
 FactoryGirl.define do
   factory :feed_version_info do
