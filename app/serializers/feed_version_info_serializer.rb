193dc0cdea980430aabc4c82c796de35f79fa2b0
Ian Rees <ian@ianrees.net>
2017-03-16 17:33:03 -0700

    FeedVersionInfoSerializer updates

--- app/serializers/feed_version_info_serializer.rb
+++ app/serializers/feed_version_info_serializer.rb
@@ -1,7 +1,18 @@
 class FeedVersionInfoSerializer < ApplicationSerializer
-  attributes :statistics,
+  attributes :id,
+             :statistics,
              :scheduled_service,
              :filenames,
+             :feed_onestop_id,
+             :feed_version_sha1,
              :created_at,
              :updated_at
+
+  def feed_onestop_id
+    object.feed.onestop_id
+  end
+
+  def feed_version_sha1
+    object.feed_version.sha1
+  end
 end
