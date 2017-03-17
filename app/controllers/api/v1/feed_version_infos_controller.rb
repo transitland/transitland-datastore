d00a4ab0426fe59284c17c22f13767fb0334760b
Ian Rees <ian@ianrees.net>
2017-03-17 15:42:46 -0700

    Annotations [ci skip]

--- app/controllers/api/v1/feed_version_infos_controller.rb
+++ app/controllers/api/v1/feed_version_infos_controller.rb
@@ -1,25 +1,6 @@
 # == Schema Information
 #
-# Table name: feed_versions
-#
-#  id                     :integer          not null, primary key
-#  feed_id                :integer
-#  feed_type              :string
-#  file                   :string
-#  earliest_calendar_date :date
-#  latest_calendar_date   :date
-#  sha1                   :string
-#  md5                    :string
-#  tags                   :hstore
-#  fetched_at             :datetime
-#  imported_at            :datetime
-#  created_at             :datetime
-#  updated_at             :datetime
-#  import_level           :integer          default(0)
-#
-# Indexes
-#
-#  index_feed_versions_on_feed_type_and_feed_id  (feed_type,feed_id)
+# Table name: feed_version_infos
 #
 
 class Api::V1::FeedVersionInfosController < Api::V1::BaseApiController
--- app/serializers/feed_version_info_serializer.rb
+++ app/serializers/feed_version_info_serializer.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: feed_version_infos
+#
+#  id                :integer          not null, primary key
+#  statistics        :json
+#  scheduled_service :json
+#  filenames         :string           is an Array
+#  created_at        :datetime
+#  updated_at        :datetime
+#
+
 class FeedVersionInfoSerializer < ApplicationSerializer
   attributes :id,
              :statistics,
