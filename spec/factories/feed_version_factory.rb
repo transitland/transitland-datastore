534cbb15aeb82b7b681c640d4335f592195b8f3f
Alexander Smith <alextsmith@Alexs-MacBook-Pro.local>
2017-03-22 11:59:23 -0500

    adding alleghany transit to tests

--- spec/factories/feed_version_factory.rb
+++ spec/factories/feed_version_factory.rb
@@ -101,6 +101,11 @@ FactoryGirl.define do
       association :feed, factory: :feed_nj_path
     end
 
+    factory :feed_version_alleghany do
+      file { File.open(Rails.root.join('spec/support/example_gtfs_archives/alleghany_gtfs.zip')) }
+      association :feed, factory: :feed_alleghany
+    end
+
     factory :feed_version_example do
       file { File.open(Rails.root.join('spec/support/example_gtfs_archives/example.zip')) }
       association :feed, factory: :feed_example
