d4643e16e84e35ae5187562a602b0a386dc54648
Ian Rees <ian@ianrees.net>
2017-03-22 12:27:07 -0700

    Run Conveyal gtfs-lib validator

--- app/services/feed_validation_service.rb
+++ app/services/feed_validation_service.rb
@@ -2,6 +2,28 @@ class FeedValidationService
   include Singleton
 
   GOOGLE_VALIDATOR_PATH = './virtualenv/bin/feedvalidator.py'
+  CONVEYAL_VALIDATOR_PATH = './bin/gtfs-lib.jar'
+
+  def self.run_conveyal_validator(filename)
+    outfile = nil
+    Tempfile.open(['conveyal', '.json']) do |tmpfile|
+    outfile = tmpfile.path
+    end
+
+    # Run feedvalidator
+    output = nil
+    IO.popen(['java', '-Xmx6G', '-jar', CONVEYAL_VALIDATOR_PATH, '-validate', filename, outfile], "w+") do |io|
+      io.write("\n")
+      io.close_write
+      output = io.read
+    end
+
+    return unless File.exists?(outfile)
+    # Unlink temporary file
+    f = File.open(outfile)
+    File.unlink(outfile)
+    f
+  end
 
   def self.run_google_validator(filename)
     # Create a tempfile to use the filename.
@@ -21,9 +43,9 @@ class FeedValidationService
 
     return unless File.exists?(outfile)
     # Unlink temporary file
-    file_feedvalidator = File.open(outfile)
+    f = File.open(outfile)
     File.unlink(outfile)
-    file_feedvalidator
+    f
   end
 
   def self.run_validators(feed_version)
@@ -34,6 +56,9 @@ class FeedValidationService
     # Run validators
     if Figaro.env.run_google_validator.presence == 'true'
       file_google = run_google_validator(gtfs_filename)
+    end
+    if Figaro.env.run_conveyal_validator.presence == 'true'
+      file_conveyal = run_conveyal_validator(gtfs_filename)
     end
 
     # Cleanup
