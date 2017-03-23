961c96fd07b07208fe9c5805aee93b337c5aeb90
Ian Rees <ian@ianrees.net>
2017-03-23 16:50:00 -0700

    Spec update

--- spec/services/gtfs_statistics_service_spec.rb
+++ spec/services/gtfs_statistics_service_spec.rb
@@ -3,11 +3,12 @@ describe GTFSStatisticsService do
     it '.create_feed_version_info' do
       feed_version = create(:feed_version_example)
       feed_version_info = GTFSStatisticsService.create_feed_version_info(feed_version)
-      expect(feed_version_info.filenames.size).to eq(12)
-      expect(feed_version_info.scheduled_service.size).to eq(1460)
-      expect(feed_version_info.statistics.size).to eq(12)
-      expect(feed_version_info.statistics['routes.txt']['agency_id']['total']).to eq(5)
-      expect(feed_version_info.statistics['routes.txt']['agency_id']['unique']).to eq(1)
+      data = feed_version_info.data
+      expect(data['filenames'].size).to eq(12)
+      expect(data['scheduled_service'].size).to eq(1460)
+      expect(data['statistics'].size).to eq(12)
+      expect(data['statistics']['routes.txt']['agency_id']['total']).to eq(5)
+      expect(data['statistics']['routes.txt']['agency_id']['unique']).to eq(1)
     end
   end
 end
--- spec/workers/gtfs_statistics_worker_spec.rb
+++ spec/workers/gtfs_statistics_worker_spec.rb
@@ -7,7 +7,8 @@ describe GTFSStatisticsWorker do
       feed_version = create(:feed_version_example)
       Sidekiq::Testing.inline! do
         GTFSStatisticsWorker.perform_async(feed_version.sha1)
-        expect(feed_version.reload.feed_version_info.filenames.size).to eq(12)
+        fvi = feed_version.reload.feed_version_infos.first
+        expect(fvi.data['filenames'].size).to eq(12)
       end
     end
   end
