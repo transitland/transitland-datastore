7ead8bc9bce60ac68f111f48b4fb18fe9587eb3b
Ian Rees <ian@ianrees.net>
2017-03-23 16:49:55 -0700

    Annotations

--- spec/models/feed_version_info_spec.rb
+++ spec/models/feed_version_info_spec.rb
@@ -3,13 +3,12 @@
 #
 # Table name: feed_version_infos
 #
-#  id                :integer          not null, primary key
-#  statistics        :json
-#  scheduled_service :json
-#  filenames         :string           is an Array
-#  feed_version_id   :integer
-#  created_at        :datetime
-#  updated_at        :datetime
+#  id              :integer          not null, primary key
+#  type            :string
+#  data            :json
+#  feed_version_id :integer
+#  created_at      :datetime
+#  updated_at      :datetime
 #
 # Indexes
 #
