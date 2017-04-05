13f5f32081bf01d97489ec6050c93918a14d12ea
Ian Rees <ian@ianrees.net>
2017-04-04 20:30:10 -0700

    import_level 2

--- spec/support/load_feed.rb
+++ spec/support/load_feed.rb
@@ -7,9 +7,9 @@ def load_feed(feed_version_name: nil, feed_version: nil, import_level: 1, block_
   block_before_level_1.call(graph)
   graph.create_change_osr
   block_before_level_2.call(graph)
-  # if import_level >= 2
-  #   GTFSScheduleImport.new(feed, feed_version).load_schedule
-  # end
+  if import_level >= 2
+    GTFSScheduleImport.new(feed, feed_version).load_schedule
+  end
   feed.activate_feed_version(feed_version.sha1, import_level)
   return feed, feed_version
 end
