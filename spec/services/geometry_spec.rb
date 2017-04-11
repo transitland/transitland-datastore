fe174994c2078f0eac692d5243c4d7d8b8a22505
Alexander Smith <saalexantay@gmail.com>
2017-04-11 16:44:08 -0500

    validation method and setting up spec

--- spec/services/geometry_spec.rb
+++ spec/services/geometry_spec.rb
@@ -99,6 +99,11 @@ describe Geometry do
         feed, feed_version = load_feed(feed_version_name: :feed_version_wmata_48587, import_level: 1)
         expect(RouteStopPattern.first.stop_distances).to match_array([0.0, 155.2, 863.5, 1794.1, 2913.2, 3187.8, 3733.7, 3918.1, 4762.5])
       end
+
+      it 'discards invalid shape_dist_traveled' do
+        feed, feed_version = load_feed(feed_version_name: :feed_version_alleghany, import_level: 1)
+        
+      end
     end
 
     it 'no distance issues with RSPs generated from trip stop points' do
