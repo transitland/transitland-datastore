6a1a1edc953e2489d414a9d9db6c67e4df9c21ba
Ian Rees <ian@ianrees.net>
2017-04-13 15:17:48 -0700

    Tidy

--- app/serializers/feed_version_info_serializer.rb
+++ app/serializers/feed_version_info_serializer.rb
@@ -23,10 +23,6 @@ class FeedVersionInfoSerializer < ApplicationSerializer
              :created_at,
              :updated_at
 
-  # def feed_onestop_id
-  #   object.feed.onestop_id
-  # end
-
   def feed_version_sha1
     object.feed_version.sha1
   end
