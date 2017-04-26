030a983c85b16acbf3ea5d30a16f14c854da8bcf
Ian Rees <ian@ianrees.net>
2017-04-26 15:32:53 -0700

    Break up schedule import

--- spec/support/load_feed.rb
+++ spec/support/load_feed.rb
@@ -1,4 +1,5 @@
 def load_feed(feed_version_name: nil, feed_version: nil, import_level: 1, block_before_level_1: nil, block_before_level_2: nil)
+  stop_times_max_load = Figaro.env.feed_eater_stop_times_max_load.try(:to_i) || 100_000
   block_before_level_1 ||= Proc.new { |graph| }
   block_before_level_2 ||= Proc.new { |graph| }
   feed_version = create(feed_version_name) if feed_version.nil?
@@ -8,7 +9,9 @@ def load_feed(feed_version_name: nil, feed_version: nil, import_level: 1, block_
   graph.create_change_osr
   block_before_level_2.call(graph)
   if import_level >= 2
-    GTFSScheduleImport.new(feed, feed_version).load_schedule
+    graph.gtfs.trip_chunks(stop_times_max_load) do |trips|
+      GTFSScheduleImport.new(feed, feed_version).load_schedule(trip_ids: trips.map(&:id))
+    end
   end
   feed.activate_feed_version(feed_version.sha1, import_level)
   return feed, feed_version
