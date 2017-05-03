039bac9ed1f8a7e4693bafaf19925f8a7cbab3ff
Ian Rees <ian@ianrees.net>
2017-05-03 15:08:55 -0700

    Renames

--- spec/support/load_feed.rb
+++ spec/support/load_feed.rb
@@ -4,13 +4,13 @@ def load_feed(feed_version_name: nil, feed_version: nil, import_level: 1, block_
   block_before_level_2 ||= Proc.new { |graph| }
   feed_version = create(feed_version_name) if feed_version.nil?
   feed = feed_version.feed
-  graph = GTFSGraph2.new(feed, feed_version)
+  graph = GTFSGraphImporter.new(feed, feed_version)
   block_before_level_1.call(graph)
   graph.create_change_osr
   block_before_level_2.call(graph)
   if import_level >= 2
     # graph.gtfs.trip_chunks(stop_times_max_load) do |trips|
-    GTFSScheduleImport.new(feed, feed_version).load_schedule
+    GTFSScheduleImporter.new(feed, feed_version).load_schedule
     # end
   end
   feed.activate_feed_version(feed_version.sha1, import_level)
