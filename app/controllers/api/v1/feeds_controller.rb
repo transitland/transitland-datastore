3f72551001894fffdd57091e3b4b25ccc0ea60de
Ian Rees <ian@ianrees.net>
2017-06-02 15:28:38 -0700

    Convert Feeds controller

--- app/controllers/api/v1/feeds_controller.rb
+++ app/controllers/api/v1/feeds_controller.rb
@@ -1,64 +1,42 @@
-class Api::V1::FeedsController < Api::V1::BaseApiController
-  include JsonCollectionPagination
-  include DownloadableCsv
-  include AllowFiltering
-
+class Api::V1::FeedsController < Api::V1::EntityController
+  MODEL = Feed
   before_action :set_feed, only: [:show, :download_latest_feed_version]
 
-  def index
-    # Entity
-    @feeds = Feed.where('')
-    @feeds = AllowFiltering.by_onestop_id(@feeds, params)
-    @feeds = AllowFiltering.by_tag_keys_and_values(@feeds, params)
-    @feeds = AllowFiltering.by_updated_since(@feeds, params)
-    @feeds = AllowFiltering.by_attribute_array(@feeds, params, :url, case_sensitive: true)
-
-    # Geometry
-    if [params[:lat], params[:lon]].map(&:present?).all?
-      point = Feed::GEOFACTORY.point(params[:lon], params[:lat])
-      r = params[:r] || 100 # meters TODO: move this to a more logical place
-      @feeds = @feeds.where{st_dwithin(geometry, point, r)}.order{st_distance(geometry, point)}
-    end
-    if params[:bbox].present?
-      @feeds = @feeds.geometry_within_bbox(params[:bbox])
-    end
-
+  def index_query
+    super
     # Feeds
-    @feeds = AllowFiltering.by_attribute_since(@feeds, params, :last_imported_since, :last_imported_at)
+    @collection = AllowFiltering.by_attribute_since(@collection, params, :last_imported_since, :last_imported_at)
     if params[:latest_fetch_exception].present?
-      @feeds = @feeds.where_latest_fetch_exception(AllowFiltering.to_boolean(params[:latest_fetch_exception]))
+      @collection = @collection.where_latest_fetch_exception(AllowFiltering.to_boolean(params[:latest_fetch_exception]))
     end
     if params[:active_feed_version_valid].present?
-      @feeds = @feeds.where_active_feed_version_valid(params[:active_feed_version_valid])
+      @collection = @collection.where_active_feed_version_valid(params[:active_feed_version_valid])
     end
     if params[:active_feed_version_expired].present?
-      @feeds = @feeds.where_active_feed_version_expired(params[:active_feed_version_expired])
+      @collection = @collection.where_active_feed_version_expired(params[:active_feed_version_expired])
     end
     if params[:active_feed_version_update].presence == 'true'
-      @feeds = @feeds.where_active_feed_version_update
+      @collection = @collection.where_active_feed_version_update
     end
     if params[:active_feed_version_import_level].present?
-      @feeds = @feeds.where_active_feed_version_import_level(params[:active_feed_version_import_level])
+      @collection = @collection.where_active_feed_version_import_level(params[:active_feed_version_import_level])
     end
     if params[:latest_feed_version_import_status].present?
-      @feeds = @feeds.where_latest_feed_version_import_status(AllowFiltering.to_boolean(params[:latest_feed_version_import_status]))
+      @collection = @collection.where_latest_feed_version_import_status(AllowFiltering.to_boolean(params[:latest_feed_version_import_status]))
     end
+  end
 
+  def index_includes
+    super
     # Includes
-    @feeds = @feeds.includes{[
+    @collection = @collection.includes{[
       operators_in_feed,
       operators_in_feed.operator,
       changesets_imported_from_this_feed,
       active_feed_version,
       feed_versions
     ]}
-    @feeds = @feeds.includes(:issues) if AllowFiltering.to_boolean(params[:embed_issues])
-
-    respond_to do |format|
-      format.json { render paginated_json_collection(@feeds).merge({ scope: { embed_issues: AllowFiltering.to_boolean(params[:embed_issues]) } }) }
-      format.geojson { render paginated_geojson_collection(@feeds) }
-      format.csv { return_downloadable_csv(@feeds, 'feeds') }
-    end
+    @collection = @collection.includes(:issues) if AllowFiltering.to_boolean(params[:embed_issues])
   end
 
   def show
