ff36899317ac77c9ae25d594de2b8ee5ab4ee5af
Ian Rees <ian@ianrees.net>
2017-07-07 10:39:22 -0700

    Spec to ensure both min_platforms and min_egresses can be used

--- spec/models/stop_spec.rb
+++ spec/models/stop_spec.rb
@@ -185,16 +185,21 @@ describe Stop do
       # Handled in JSON pagination concern
       expect(Stop.with_min_platforms(1).count.size).to eq(2)
     end
+
+    it 'works with min_egresses' do
+      @s3e1 = create(:stop_egress, parent_stop: @s3)
+      expect(Stop.with_min_platforms(1).with_min_egresses(1)).to eq([@s3])
+    end
   end
 
   context '.with_min_egresses' do
     before(:each) do
       @s1 = create(:stop)
       @s2 = create(:stop)
-      @s2p1 = create(:stop_egress, parent_stop: @s2)
+      @s2e1 = create(:stop_egress, parent_stop: @s2)
       @s3 = create(:stop)
-      @s3p1 = create(:stop_egress, parent_stop: @s3)
-      @s3p2 = create(:stop_egress, parent_stop: @s3)
+      @s3e1 = create(:stop_egress, parent_stop: @s3)
+      @s3e2 = create(:stop_egress, parent_stop: @s3)
     end
 
     it 'returns only Stops with StopEgresses' do
@@ -208,6 +213,11 @@ describe Stop do
     it 'works with count' do
       # Handled in JSON pagination concern
       expect(Stop.with_min_egresses(1).count.size).to eq(2)
+    end
+
+    it 'works with min_platforms' do
+      @s3p1 = create(:stop_platform, parent_stop: @s3)
+      expect(Stop.with_min_platforms(1).with_min_egresses(1)).to eq([@s3])
     end
   end
 
