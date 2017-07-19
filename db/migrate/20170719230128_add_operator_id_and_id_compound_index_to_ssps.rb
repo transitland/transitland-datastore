26622481011d67c4653d0932fb143ae32969d3a9
Ian Rees <ian@ianrees.net>
2017-07-19 16:04:58 -0700

    Add compound operator_id and id index to SSPs

---
+++ db/migrate/20170719230128_add_operator_id_and_id_compound_index_to_ssps.rb
@@ -0,0 +1,6 @@
+class AddOperatorIdAndIdCompoundIndexToSSPs < ActiveRecord::Migration
+  def change
+    add_index :current_schedule_stop_pairs, [:operator_id, :id] unless index_exists?(:current_schedule_stop_pairs, [:operator_id, :id])
+    remove_index :current_schedule_stop_pairs, :operator_id
+  end
+end
