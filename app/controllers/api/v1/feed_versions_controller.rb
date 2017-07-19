8c228cda3c44830a90bcc0e21f89f7ff2ed57434
Ian Rees <ian@ianrees.net>
2017-07-18 18:04:33 -0700

    Use ActionDispatch::Http::UploadedFile tempfile.path

--- app/controllers/api/v1/feed_versions_controller.rb
+++ app/controllers/api/v1/feed_versions_controller.rb
@@ -100,8 +100,9 @@ class Api::V1::FeedVersionsController < Api::V1::BaseApiController
 
   def create
     feed = Feed.find_by_onestop_id!(feed_version_params[:feed_onestop_id])
+    file = feed_version_params[:file].tempfile.path
     url = feed_version_params[:url] || feed.url
-    feed_version = FeedFetcherService.create_feed_version(feed, url, file: feed_version_params[:file])
+    feed_version = FeedFetcherService.create_feed_version(feed, url, file: file)
     render json: feed_version
   end
 
