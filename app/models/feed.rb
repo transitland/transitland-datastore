de83fd54c1b42480d81c31a7a320179335ab9c6a
Ian Rees <ian@ianrees.net>
2017-08-09 13:43:45 -0700

    Feed: sort feed_versions by earliest_calendar_date

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -57,7 +57,7 @@ class Feed < BaseFeed
   include IsAnEntityWithIssues
   include IsAnEntityImportedFromFeeds
 
-  has_many :feed_versions, -> { order 'created_at DESC' }, dependent: :destroy, as: :feed
+  has_many :feed_versions, -> { order 'earliest_calendar_date' }, dependent: :destroy, as: :feed
   has_many :feed_version_imports, -> { order 'created_at DESC' }, through: :feed_versions
   belongs_to :active_feed_version, class_name: 'FeedVersion'
 
