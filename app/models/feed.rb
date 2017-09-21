d1472200d4617137a64a85eb4bb5d3946ef7dfd0
Ian Rees <ian@ianrees.net>
2017-09-21 01:39:50 -0700

    Move find_next_feed_version to FeedMaintenanceService

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -224,43 +224,6 @@ class Feed < BaseFeed
     return true
   end
 
-  def find_next_feed_version(date)
-    # Find a feed_version where:
-    #   1. newer than active_feed_version
-    #   2. service begins on or later than active_feed_version
-    #   3. service begins on or before specified date
-    active_feed_version = self.active_feed_version
-    return unless active_feed_version
-    return unless active_feed_version.imported_at
-
-    next_fv = self.feed_versions
-      .where('created_at > ?', active_feed_version.created_at)
-      .where('earliest_calendar_date >= ?', active_feed_version.earliest_calendar_date)
-      .where('earliest_calendar_date <= ?', date)
-      .reorder(earliest_calendar_date: :desc, created_at: :desc)
-      .first
-
-    return unless next_fv
-
-    # Feed import policy settings
-    policy = self.tags['import_policy']
-    policy ||= 'immediately' # default policy
-    days_since_last_import = (next_fv.created_at.to_date - active_feed_version.imported_at.to_date)
-    if policy == 'manual'
-      return
-    elsif policy == 'immediately'
-      return next_fv
-    elsif policy == 'daily'
-      return next_fv if days_since_last_import >= 1
-    elsif policy == 'weekly'
-      return next_fv if days_since_last_import >= 7
-    elsif policy == 'monthly'
-      return next_fv if days_since_last_import >= 30 # todo: use calendar month
-    elsif policy == 'yearly'
-      return next_fv if days_since_last_import >= 365 # todo: use calendar year
-    end
-  end
-
   def activate_feed_version(feed_version_sha1, import_level)
     feed_version = self.feed_versions.find_by!(sha1: feed_version_sha1)
     self.transaction do
