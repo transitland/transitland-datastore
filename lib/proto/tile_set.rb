ebed934e0a37e6d19c3c2a39eb90e419dfec8262
Ian Rees <ian@ianrees.net>
2017-10-18 17:50:12 -0700

    bbox_to_level_tiles

--- lib/proto/tile_set.rb
+++ lib/proto/tile_set.rb
@@ -2,8 +2,22 @@ require 'fileutils'
 require 'google/protobuf'
 load 'lib/proto/transit_pb.rb'
 
+SIZES = [4.0, 1.0, 0.25]
+LEVEL_BITS = 3
+TILE_INDEX_BITS = 22
+ID_INDEX_BITS = 21
+LEVEL_MASK = (2**LEVEL_BITS) - 1
+TILE_INDEX_MASK = (2**TILE_INDEX_BITS) - 1
+ID_INDEX_MASK = (2**ID_INDEX_BITS) - 1
+INVALID_ID = (ID_INDEX_MASK << (TILE_INDEX_BITS + LEVEL_BITS)) | (TILE_INDEX_MASK << LEVEL_BITS) | LEVEL_MASK
+
+def int(value)
+  # Simplify porting
+  value.to_i
+end
+
 class Tile
-  attr_accessor :level, :tile, :message
+  attr_reader :level, :tile, :message
   def initialize(level, tile, data: nil)
     @level = level
     @tile = tile
@@ -32,6 +46,11 @@ class Tile
   def next_index
     (@index.keys.max || 0) + 1
   end
+
+  def bbox
+    GraphID.level_tile_to_bbox(@level, @tile)
+  end
+
 end
 
 class TileSet
@@ -79,15 +98,6 @@ class TileSet
 end
 
 class GraphID
-  SIZES = [4.0, 1.0, 0.25]
-  LEVEL_BITS = 3
-  TILE_INDEX_BITS = 22
-  ID_INDEX_BITS = 21
-  LEVEL_MASK = (2**LEVEL_BITS) - 1
-  TILE_INDEX_MASK = (2**TILE_INDEX_BITS) - 1
-  ID_INDEX_MASK = (2**ID_INDEX_BITS) - 1
-  INVALID_ID = (ID_INDEX_MASK << (TILE_INDEX_BITS + LEVEL_BITS)) | (TILE_INDEX_MASK << LEVEL_BITS) | LEVEL_MASK
-
   attr_accessor :value
   def initialize(value: nil, **kwargs)
     @value = value || (self.class.make_id(**kwargs))
@@ -102,15 +112,49 @@ class GraphID
 
   def self.lll_to_tile(tile_level, lat, lon)
     size = SIZES[tile_level]
-    width = (360 / size).to_i
-    ((lat + 90) / size).to_i * width + ((lon + 180 ) / size).to_i
+    width = int(360 / size)
+    int((lat + 90) / size) * width + int((lon + 180 ) / size)
   end
 
-  def get_ll
+  def self.level_tile_to_bbox(level, tile)
     size = SIZES[level]
-    width = (360 / size).to_i
-    height = (180 / size).to_i
-    [(tile / width).to_i * size - 90, (tile % width) * size - 180]
+    width = int(360 / size)
+    height = int(180 / size)
+    ymin = int(tile / width) * size - 90
+    xmin = (tile % width) * size - 180
+    xmax = xmin + size
+    ymax = ymin + size
+    [xmin, ymin, xmax, ymax]
+  end
+
+  def self.bbox_to_level_tiles(xmin, ymin, xmax, ymax)
+    # if this is crossing the anti meridian split it up and combine
+    left, bottom, right, top = ymin, xmin, ymax, xmax
+    if left > right
+      east = tiles_for_bbox(left, bottom, 180.0, top)
+      west = tiles_for_bbox(-180.0, bottom, right, top)
+      return east + west
+    end
+    #move these so we can compute percentages
+    left += 180
+    right += 180
+    bottom += 90
+    top += 90
+    tiles = []
+    SIZES.each_index do |level|
+      size = SIZES[level]
+      (int(left/size)..(int(right/size))).each do |x|
+        (int(bottom/size)..(int(top/size))).each do |y|
+          tile = int(y * (360.0 / size) + x)
+          tiles << [level, tile]
+        end
+      end
+    end
+    tiles
+  end
+
+  def bbox
+    self.class.level_tile_to_bbox(level, tile)
   end
 
   def level
