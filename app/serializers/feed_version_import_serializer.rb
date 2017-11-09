fed9ce506c42f62dce63f69d416c48133061cdfb
Ian Rees <ian@ianrees.net>
2017-11-08 18:11:10 -0800

    Add FVI OIF to serializer

--- app/serializers/feed_version_import_serializer.rb
+++ app/serializers/feed_version_import_serializer.rb
@@ -24,6 +24,7 @@ class FeedVersionImportSerializer < ApplicationSerializer
              :feed_version,
              :feed_url,
              :feed_version_url,
+             :operators_in_feed,
              :success,
              :import_log,
              :import_level,
