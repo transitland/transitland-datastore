1b4c5040b73cf1e4c1d400f3bfc17f70aeb9314e
Ian Rees <irees@mbp.local>
2018-01-10 13:00:03 -0800

    worker boot

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,9 @@
 #!/usr/bin/env puma
 port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
+
+on_worker_boot do
+  ActiveSupport.on_load(:active_record) do
+    ActiveRecord::Base.establish_connection
+  end
+end
