9e9917c8d8cf384d509ca016ec5e8b662741cb31
Ian Rees <irees@mbp.local>
2018-01-10 16:40:25 -0800

    preload app, simpler connection call

--- config/puma.rb
+++ config/puma.rb
@@ -2,8 +2,7 @@
 port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 
+preload_app!
 on_worker_boot do
-  ActiveSupport.on_load(:active_record) do
-    ActiveRecord::Base.establish_connection
-  end
+  ActiveRecord::Base.establish_connection
 end
