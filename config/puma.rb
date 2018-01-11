2cbb3c33c40df0cf8e6901aff2bacd6da1df3ba3
Ian Rees <irees@mbp.local>
2018-01-10 17:45:10 -0800

    Tidy

--- config/puma.rb
+++ config/puma.rb
@@ -2,7 +2,10 @@
 port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 
+# Preload app
 preload_app!
+
+# Configure connections
 on_worker_boot do
   ActiveRecord::Base.establish_connection
 end
