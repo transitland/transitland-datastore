4749250c71e5669136be16f28dfaabe965ec2760
Ian Rees <irees@mbp.local>
2018-01-19 10:37:51 -0800

    More env

--- build-push.sh
+++ build-push.sh
@@ -1,5 +1,5 @@
 source virtualenv/bin/activate
-eval $(aws --profile ${TRANSITLAND_AWS_PROFILE} ecr get-login --no-include-email --region us-east-1)
+eval $(aws --profile ${TRANSITLAND_AWS_PROFILE} ecr get-login --no-include-email --region ${TRANSITLAND_AWS_REGION})
 
 # Check specs
 set -e
@@ -12,5 +12,5 @@ docker push ${TRANSITLAND_AWS_ECR}/transitland-datastore:latest
 
 # ecs run --profile ${TRANSITLAND_AWS_PROFILE} ${TRANSITLAND_CLUSTER} td-sidekiq -c transitland-datastore "bundle exec rake db:migrate"
 # poll for succussful completion...
-# ecs deploy --profile ${TRANSITLAND_AWS_PROFILE} td td-rails-fargate
-# ecs deploy --profile ${TRANSITLAND_AWS_PROFILE} td td-sidekiq
+# ecs deploy --profile ${TRANSITLAND_AWS_PROFILE} ${TRANSITLAND_CLUSTER} td-rails-fargate
+# ecs deploy --profile ${TRANSITLAND_AWS_PROFILE} ${TRANSITLAND_CLUSTER} td-sidekiq
