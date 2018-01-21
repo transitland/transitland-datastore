2c92aecc56353f89010e58f6fadad8a55a11796d
Ian Rees <ian@ianrees.net>
2018-01-21 01:50:11 -0800

    GTFSStopTime associations

--- app/models/gtfs_stop_time.rb
+++ app/models/gtfs_stop_time.rb
@@ -31,4 +31,7 @@
 
 class GTFSStopTime < ActiveRecord::Base
   belongs_to :feed_version
+  belongs_to :trip, class_name: 'GTFSTrip'
+  belongs_to :origin, class_name: 'GTFSStop'
+  belongs_to :destination, class_name: 'GTFSStop'
 end
