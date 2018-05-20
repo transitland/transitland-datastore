282957760721f4b0715307d8a7705e8096e05fd7
Ian Rees <ian@ianrees.net>
2018-05-20 15:29:57 -0700

    Improvements to not null and indexes

--- db/migrate/20180113021001_create_gtfs_tables.rb
+++ db/migrate/20180113021001_create_gtfs_tables.rb
@@ -1,10 +1,10 @@
 class CreateGTFSTables < ActiveRecord::Migration
   def change
     create_table :gtfs_agencies do |t|
-      t.string :agency_id, index: true, null: false
-      t.string :agency_name, index: true
-      t.string :agency_url
-      t.string :agency_timezone
+      t.string :agency_id, index: true
+      t.string :agency_name, index: true, null: false
+      t.string :agency_url, null: false
+      t.string :agency_timezone, null: false
       t.string :agency_lang
       t.string :agency_phone
       t.string :agency_fare_url
@@ -17,20 +17,20 @@ class CreateGTFSTables < ActiveRecord::Migration
     add_index :gtfs_agencies, [:feed_version_id, :agency_id], unique: true, name: 'index_gtfs_agencies_unique'
 
     create_table :gtfs_stops do |t|
-      t.string :stop_id, index: true, null: false
+      t.string :stop_id, null: false, index: true
       t.string :stop_code, index: true
-      t.string :stop_name, index: true
+      t.string :stop_name, null: false, index: true
       t.string :stop_desc, index: true
       # t.float :stop_lat
       # t.float :stop_lon
-      t.string :zone_id
+      t.string :zone_id, index: true
       t.string :stop_url
       t.integer :location_type, index: true
       # t.string :parent_station
       t.string :stop_timezone
       t.integer :wheelchair_boarding
       #
-      t.st_point :geometry, index: true
+      t.st_point :geometry, null: false, index: true
       t.timestamps null: false
       t.references :feed_version, index: true, null: false
       t.references :entity, references: :current_stops, index: true
@@ -41,10 +41,10 @@ class CreateGTFSTables < ActiveRecord::Migration
     create_table :gtfs_routes do |t|
       t.string :route_id, index: true, null: false
       # t.string :agency_id
-      t.string :route_short_name, index: true
-      t.string :route_long_name, index: true
+      t.string :route_short_name, null: false, index: true
+      t.string :route_long_name, null: false, index: true
       t.string :route_desc, index: true
-      t.integer :route_type, index: true
+      t.integer :route_type, null: false, index: true
       t.string :route_url
       t.string :route_color
       t.string :route_text_color
@@ -52,13 +52,13 @@ class CreateGTFSTables < ActiveRecord::Migration
       t.timestamps null: false
       t.references :feed_version, index: true, null: false
       t.references :entity, references: :current_routes, index: true
-      t.references :agency, references: :gtfs_agencies, index: true
+      t.references :agency, references: :gtfs_agencies, null: false, index: true
     end
     add_index :gtfs_routes, [:feed_version_id, :route_id], unique: true, name: 'index_gtfs_routes_unique'
 
     create_table :gtfs_trips do |t|
       # t.string :route_id
-      t.string :service_id, indx: true
+      t.string :service_id, null: false, index: true
       t.string :trip_id, index: true, null: false
       t.string :trip_headsign, index: true
       t.string :trip_short_name, index: true
@@ -71,7 +71,7 @@ class CreateGTFSTables < ActiveRecord::Migration
       t.timestamps null: false
       t.references :feed_version, index: true, null: false
       t.references :entity, references: :current_route_stop_patterns, index: true
-      t.references :route, references: :gtfs_routes, index: true
+      t.references :route, references: :gtfs_routes, null: false, index: true
       t.references :shape, references: :gtfs_shapes, index: true
     end
     add_index :gtfs_trips, [:feed_version_id, :trip_id], unique: true, name: 'index_gtfs_trips_unique'
@@ -91,24 +91,24 @@ class CreateGTFSTables < ActiveRecord::Migration
       t.timestamps null: false
       t.references :feed_version, index: true, null: false
       t.references :trip, references: :gtfs_trips, index: true, null: false
-      t.references :origin, references: :gtfs_stops, index: true
-      t.references :destination, references: :gtfs_stops, index: true
-      t.integer :origin_departure_time
-      t.integer :destination_arrival_time
+      t.references :origin, references: :gtfs_stops, index: true, null: false
+      t.references :destination, references: :gtfs_stops, index: true, null: false
+      t.integer :origin_departure_time, null: false, index: true
+      t.integer :destination_arrival_time, null: false, index: true
     end
     add_index :gtfs_stop_times, [:feed_version_id, :trip_id, :stop_sequence], unique: true, name: 'index_gtfs_stop_times_unique'
 
     create_table :gtfs_calendars do |t|
       t.string :service_id, index: true, null: false
-      t.boolean :monday, index: true
-      t.boolean :tuesday, index: true
-      t.boolean :wednesday, index: true
-      t.boolean :thursday, index: true
-      t.boolean :friday, index: true
-      t.boolean :saturday, index: true
-      t.boolean :sunday, index: true
-      t.date :start_date, index: true
-      t.date :end_date, index: true
+      t.boolean :monday, null: false, index: true
+      t.boolean :tuesday, null: false, index: true
+      t.boolean :wednesday, null: false, index: true
+      t.boolean :thursday, null: false, index: true
+      t.boolean :friday, null: false, index: true
+      t.boolean :saturday, null: false, index: true
+      t.boolean :sunday, null: false, index: true
+      t.date :start_date, null: false, index: true
+      t.date :end_date, null: false, index: true
       t.timestamps null: false
       #
       t.references :feed_version, index: true, null: false
@@ -118,7 +118,7 @@ class CreateGTFSTables < ActiveRecord::Migration
     create_table :gtfs_calendar_dates do |t|
       t.string :service_id, index: true, null: false
       t.date :date, index: true, null: false
-      t.integer :exception_type, index: true
+      t.integer :exception_type, null: false, index: true
       #
       t.timestamps null: false
       t.references :feed_version, index: true, null: false
@@ -127,10 +127,10 @@ class CreateGTFSTables < ActiveRecord::Migration
 
     create_table :gtfs_fare_attributes do |t|
       t.string :fare_id, index: true, null: false
-      t.float :price
-      t.string :currency_type
-      t.integer :payment_method
-      t.integer :transfers
+      t.float :price, null: false
+      t.string :currency_type, null: false
+      t.integer :payment_method, null: false
+      t.integer :transfers, null: false
       # t.string :agency_id
       t.integer :transfer_duration
       #
@@ -160,7 +160,7 @@ class CreateGTFSTables < ActiveRecord::Migration
       # t.float :shape_pt_lon
       # t.integer :shape_pt_sequence
       # f.float :shape_dist_traveled
-      t.line_string :geometry, geographic: true, has_m: true
+      t.line_string :geometry, null: false, geographic: true, has_m: true
       #
       t.timestamps null: false
       t.references :feed_version, index: true, null: false
@@ -169,33 +169,33 @@ class CreateGTFSTables < ActiveRecord::Migration
 
     create_table :gtfs_frequencies do |t|
       # t.string :trip_id
-      t.integer :start_time, index: true
-      t.integer :end_time, index: true
-      t.integer :headway_secs, index: true
+      t.integer :start_time, null: false, index: true
+      t.integer :end_time, null: false, index: true
+      t.integer :headway_secs, null: false, index: true
       t.integer :exact_times
       t.timestamps null: false
       #
       t.timestamps null: false
       t.references :feed_version, index: true, null: false
-      t.references :trip, references: :gtfs_trip, index: true
+      t.references :trip, references: :gtfs_trip, null: false, index: true
     end
 
     create_table :gtfs_transfers do |t|
       # t.string :from_stop_id
       # t.string :to_stop_id
-      t.integer :transfer_type, index: true
+      t.integer :transfer_type, null: false, index: true
       t.integer :min_transfer_time, index: true
       #
       t.timestamps null: false
       t.references :feed_version, index: true, null: false
-      t.references :from_stop, references: :gtfs_stop, index: true
-      t.references :to_stop, references: :gtfs_stop, index: true
+      t.references :from_stop, references: :gtfs_stop, null: false, index: true
+      t.references :to_stop, references: :gtfs_stop, null: false, index: true
     end
 
     create_table :gtfs_feed_infos do |t|
-      t.string :feed_publisher_name
-      t.string :feed_publisher_url
-      t.string :feed_lang
+      t.string :feed_publisher_name, null: false
+      t.string :feed_publisher_url, null: false
+      t.string :feed_lang, null: false
       t.date :feed_start_date
       t.date :feed_end_date
       t.string :feed_version_name
