a3a33e5d85919ac4bcbab3acf0fb3005b01939e9
Ian Rees <ian@ianrees.net>
2018-07-19 18:53:47 -0700

    Improvements

--- app/services/gtfs_importer.rb
+++ app/services/gtfs_importer.rb
@@ -7,11 +7,11 @@ class GTFSImporter
   end
 
   def debug(msg)
-    log(msg, :debug)
+    log(msg)
   end
 
   def self.debug(msg)
-    log(msg, :debug)
+    log(msg)
   end
 
   def initialize(feed_version)
@@ -49,17 +49,69 @@ class GTFSImporter
 
   def import
     # Import order is important.
-    t_import = Time.now
-    time('agencies') { import_agencies }
-    time('stops') { import_stops }
-    time('routes') { import_routes }
-    time('shapes') { import_shapes }
-    time('trips') { import_trips }
-    time('calendar') { import_calendar }
-    time('calendar_dates') { import_calendar_dates }
-    time('optional') { import_optional }
-    time('stop_times') { import_stop_times }
-    log("total: #{((Time.now-t_import).round(2))}")
+    time('import') { import2 }
+    binding.pry
+    # t_import = Time.now
+    # time('agencies') { import_agencies }
+    # time('stops') { import_stops }
+    # time('routes') { import_routes }
+    # time('calendar') { import_calendar }
+    # time('calendar_dates') { import_calendar_dates }
+    # time('optional') { import_optional }
+    # time('shapes') { import_shapes }
+    # time('trips_and_stop_times') { import_trips_and_stop_times }
+    # log("total: #{((Time.now-t_import).round(2))}")
+  end
+
+  def import2
+    log('finding selected entities...')
+    # list of agency_ids to import
+    log('...agencies')
+    selected_agency_ids = Set.new # Set.new([@gtfs.agencies.first.id])
+    @gtfs.each_agency do |e|
+      selected_agency_ids << e.id
+    end
+    # agency associated routes
+    log('...routes')
+    selected_route_ids = Set.new
+    @gtfs.each_route do |e|
+      next unless selected_agency_ids.include?(e.agency_id)
+      selected_route_ids << e.id
+    end
+    # trips associated with selected routes
+    log('...trips')
+    selected_trip_ids = Set.new
+    @gtfs.each_trip do |e|
+      next unless selected_route_ids.include?(e.route_id)
+      selected_trip_ids << e.id
+    end
+    # stops associated with selected trips, and trip counter for pruning
+    log('...stops')
+    selected_stop_ids = Set.new
+    trip_stop_counter = Hash.new { |h,k| h[k] = 0 }
+    @gtfs.each_stop_time do |e|
+      next unless selected_trip_ids.include?(e.trip_id)
+      selected_stop_ids << e.stop_id
+      trip_stop_counter[e.trip_id] += 1
+    end
+    # include stop parent_stations
+    @gtfs.each_stop do |e|
+      next unless selected_stop_ids.include?(e.id)
+      selected_stop_ids << e.parent_station if e.parent_station
+    end
+    # pass through trips again for services and shapes
+    log('...services, shapes, pruning trips')
+    selected_service_ids = Set.new
+    selected_shape_ids = Set.new
+    @gtfs.each_trip do |e|
+      if trip_stop_counter[e.id] > 0
+        selected_service_ids << e.service_id
+        selected_shape_ids << e.shape_id
+      else
+        selected_trip_ids.remove(e.id)
+      end
+    end
+    # fares and transfers
   end
 
   def import_agencies
@@ -147,21 +199,6 @@ class GTFSImporter
   end
 
   def import_trips
-    @gtfs.each_trip do |trip|
-      params = {}
-      params[:feed_version_id] = @feed_version.id
-      params[:trip_id] = trip.trip_id
-      params[:service_id] = trip.service_id
-      params[:trip_headsign] = trip.trip_headsign
-      params[:trip_short_name] = trip.trip_short_name
-      params[:direction_id] = gtfs_int(trip.direction_id)
-      params[:block_id] = trip.block_id
-      params[:wheelchair_accessible] = gtfs_int(trip.wheelchair_accessible)
-      params[:bikes_allowed] = gtfs_int(trip.bikes_allowed)
-      params[:route_id] = @route_ids.fetch(trip.route_id)
-      params[:shape_id] = @shape_ids[trip.shape_id] # optional
-      @trip_ids[trip.trip_id] = GTFSTrip.create!(params).id
-    end
   end
 
   def import_calendar_dates
@@ -224,82 +261,84 @@ class GTFSImporter
     return unless @gtfs.file_present?('fare_attributes.txt')
   end
 
-
-  def import_stop_times
-    total_stop_times = 0
+  def import_trips_and_stop_times
+    @gtfs.trips # load trips
+    @stop_pattern_shape_ids = {} # stop_pattern shape_ids
     @gtfs.trip_id_chunks(STOP_TIME_CHUNK_SIZE) do |trip_id_chunk|
       log("processing trip_id_chunks: #{trip_id_chunk}")
-      chunk_stop_times = []
       @gtfs.each_trip_stop_times(trip_id_chunk) do |trip_id, stop_times|
-        log("stop_times: trip #{trip_id} stop_times #{stop_times.size}")
-        trip_stop_times = []
-        stop_times.each_index do |i|
-          origin = stop_times[i]
-          destination = stop_times[i+1] # last stop is nil
-          params = {}
-          params[:feed_version_id] = @feed_version.id
-          params[:stop_sequence] = gtfs_int(origin.stop_sequence)
-          params[:stop_headsign] = origin.stop_headsign
-          params[:pickup_type] = gtfs_int(origin.pickup_type)
-          params[:drop_off_type] = gtfs_int(origin.drop_off_type)
-          params[:shape_dist_traveled] = gtfs_float(origin.shape_dist_traveled)
-          params[:trip_id] = @trip_ids.fetch(origin.trip_id)
-          # params[:timepoint] = gtfs_int(origin.timepoint)
-          # where
-          params[:stop_id] = @stop_ids.fetch(origin.stop_id)
-          params[:arrival_time] = gtfs_time(origin.arrival_time)
-          params[:departure_time] = gtfs_time(origin.departure_time)
-          # for convenience
-          params[:destination_id] = @stop_ids.fetch(destination.stop_id) if destination
-          params[:destination_arrival_time] = gtfs_time(destination.arrival_time) if destination
-          trip_stop_times << GTFSStopTime.new(params)
-        end
-        # Interpolate
-        self.class.interpolate_stop_times(trip_stop_times)
-        # Validate
-        if !trip_stop_times.map(&:valid?).all?
-          log("invalid stop_times!")
-        end
-        chunk_stop_times += trip_stop_times
-        total_stop_times += trip_stop_times.size
-        chunk_stop_times = import_chunk(chunk_stop_times)
+        import_trip_stop_times(@gtfs.trip(trip_id), stop_times)
       end
-      chunk_stop_times = import_chunk(chunk_stop_times, 0)
     end
-    log("total stop_times: #{total_stop_times}")
   end
 
-  def import_chunk(chunk, chunk_size=nil)
+  def import_trip_stop_times(trip, stop_times)
+    log("stop_times: trip #{trip.id} stop_times #{stop_times.size}")
+    trip_stop_times = []
+    stop_times.each_index do |i|
+      origin = stop_times[i]
+      destination = stop_times[i+1] # last stop is nil
+      params = {}
+      params[:feed_version_id] = @feed_version.id
+      params[:stop_sequence] = gtfs_int(origin.stop_sequence)
+      params[:stop_headsign] = origin.stop_headsign
+      params[:pickup_type] = gtfs_int(origin.pickup_type)
+      params[:drop_off_type] = gtfs_int(origin.drop_off_type)
+      params[:shape_dist_traveled] = gtfs_float(origin.shape_dist_traveled)
+      # params[:trip_id] = @trip_ids.fetch(origin.trip_id)
+      # params[:timepoint] = gtfs_int(origin.timepoint)
+      # where
+      params[:stop_id] = @stop_ids.fetch(origin.stop_id)
+      params[:arrival_time] = gtfs_time(origin.arrival_time)
+      params[:departure_time] = gtfs_time(origin.departure_time)
+      # for convenience
+      params[:destination_id] = @stop_ids.fetch(destination.stop_id) if destination
+      params[:destination_arrival_time] = gtfs_time(destination.arrival_time) if destination
+      trip_stop_times << GTFSStopTime.new(params)
+    end
+
+    # Create trip
+    stop_pattern = trip_stop_times.map(&:stop_id)
+    params = {}
+    params[:feed_version_id] = @feed_version.id
+    params[:trip_id] = trip.trip_id
+    params[:service_id] = trip.service_id
+    params[:trip_headsign] = trip.trip_headsign
+    params[:trip_short_name] = trip.trip_short_name
+    params[:direction_id] = gtfs_int(trip.direction_id)
+    params[:block_id] = trip.block_id
+    params[:wheelchair_accessible] = gtfs_int(trip.wheelchair_accessible)
+    params[:bikes_allowed] = gtfs_int(trip.bikes_allowed)
+    params[:route_id] = @route_ids.fetch(trip.route_id)
+    shape_id = @shape_ids[trip.shape_id]
+    if shape_id.nil?
+      shape_id = @stop_pattern_shape_ids[stop_pattern] || create_shape_from_stop_pattern(stop_pattern).try(:id)
+      @stop_pattern_shape_ids[stop_pattern] = shape_id
+    end
+    params[:shape_id] = shape_id
+    new_trip = GTFSTrip.create!(params)
+
+    # Assign trip_ids
+    trip_stop_times.each { |i| i.trip_id = new_trip.id }
+
+    # Interpolate stop_times
+    GTFSStopTimeInterpolater.interpolate_stop_times(trip_stop_times, shape_id)
+
+    # Validate
+    if !trip_stop_times.map(&:valid?).all?
+      log("invalid stop_times!")
+    end
+    import_chunk(trip_stop_times)
+  end
+
+  def import_chunk(chunk, chunk_size=nil, idmap=nil)
     chunk_size = chunk_size || IMPORT_CHUNK_SIZE
     if chunk.size > chunk_size
       log("... import #{chunk.size}")
-      chunk.first.class.import(chunk)
+      m = chunk.first.class.import(chunk)
       chunk = []
     end
     return chunk
-  end
-
-  def self.interpolate_stop_times(stop_times)
-    stop_times = clean_stop_times(stop_times)
-    # Return early if possible
-    gaps = interpolate_find_gaps(stop_times)
-    return stop_times if gaps.size == 0
-    # Measure stops along line
-    shape_id = GTFSTrip.find(stop_times.first.trip_id).try(:shape_id)
-    trip_pattern = stop_times.map(&:stop_id)
-    # First pass: line interpolation
-    distances = get_shape_stop_distances(trip_pattern, shape_id)
-    gaps.each do |gap|
-      o, c = gap
-      interpolate_gap_distance(stop_times[o..c], distances)
-    end
-    # Second pass: distance interpolation
-    gaps = interpolate_find_gaps(stop_times)
-    gaps.each do |gap|
-      o, c = gap
-      interpolate_gap_linear(stop_times[o..c])
-    end
-    return stop_times
   end
 
   private
@@ -325,120 +364,9 @@ class GTFSImporter
     gtfs_int(value) == 1 ? true : false
   end
 
-  def self.get_shape_stop_distances(trip_pattern, shape_id)
-    # Calculate line percent from closest point to stop
-    distances = {}
-    s = 'gtfs_stops.id, ST_LineLocatePoint(shapes.geometry::geometry, ST_ClosestPoint(shapes.geometry::geometry, ST_SetSRID(gtfs_stops.geometry, 4326))) AS line_s'
-    g = GTFSStop.select(s)
-    # Create shape if necessary
-    if shape_id
-      g = g.joins('INNER JOIN gtfs_shapes AS shapes ON true')
-    else
-      shape_id = 0
-      g = g.joins("INNER JOIN (SELECT 0 as id, ST_SetSRID(ST_MakeLine(geometry), 4326) AS geometry FROM (SELECT geometry FROM gtfs_stops INNER JOIN (SELECT unnest,ordinality FROM unnest( ARRAY[#{trip_pattern.join(',')}] ) WITH ORDINALITY) as unnest ON gtfs_stops.id = unnest ORDER BY ordinality) as q) AS shapes ON true")
-    end
-    # Filter
-    g = g.where('shapes.id': shape_id, id: trip_pattern)
-    # Run
-    g.each do |row|
-      distances[row.id] = row.line_s
-    end
-    return distances
-  end
-
-  def self.clean_stop_times(stop_times)
-    # Sort by stop_sequence
-    stop_times.sort_by! { |st| st.stop_sequence }
-
-    # If we only have 1 time, assume it is both arrival and departure
-    stop_times.each do |st|
-      (st.arrival_time = st.departure_time) if st.arrival_time.nil?
-      (st.departure_time = st.arrival_time) if st.departure_time.nil?
-    end
-
-    # Ensure time is positive
-    current = stop_times.first.arrival_time
-    stop_times.each do |st|
-      s = st.arrival_time
-      fail Exception.new('cannot go backwards in time') if s && s < current
-      current = s if s
-      s = st.departure_time
-      fail Exception.new('cannot go backwards in time') if s && s < current
-      current = s if s
-    end
-
-    # These two values are required by spec
-    fail Exception.new('missing first departure time') if stop_times.first.departure_time.nil?
-    fail Exception.new('missing last arrival time') if stop_times.last.arrival_time.nil?
-    return stop_times
-  end
-
-  def self.interpolate_find_gaps(stop_times)
-    gaps = []
-    o, c = nil, nil
-    stop_times.each_with_index do |st, i|
-      # close an open gap
-      # puts "i: #{i} st: #{st.stop_sequence} stop: #{st.stop_id} arrival_time: #{st.arrival_time} departure_time: #{st.departure_time}"
-      if o && st.arrival_time
-        gaps << [o, i] if (i-o > 1)
-        o = nil
-      end
-      # open a new gap
-      if o.nil? && st.departure_time
-        o = i
-      end
-    end
-    return gaps
-  end
-
-  def self.interpolate_gap_distance(stop_times, distances)
-    debug("trip: #{stop_times.first.trip_id} interpolate_gap_distance: #{stop_times.first.stop_sequence} -> #{stop_times.last.stop_sequence}")
-    # open and close times
-    o_time = stop_times.first.departure_time
-    c_time = stop_times.last.arrival_time
-    # open and close distances
-    o_distance = distances[stop_times.first.stop_id]
-    c_distance = distances[stop_times.last.stop_id]
-    # check that we can interpolate reasonably
-    p_distance = o_distance
-    stop_times.each do |st|
-      i_distance = distances[st.stop_id]
-      return unless i_distance
-      return if i_distance < p_distance # cannot backtrack
-      return if i_distance > c_distance # cannot exceed end
-      p_distance = i_distance
-    end
-    # interpolate on distance
-    debug("\tlength: #{c_distance - o_distance} duration: #{c_time - o_time}")
-    debug("\to_distance: #{o_distance} o_time: #{o_time}")
-    stop_times[1...-1].each do |st|
-      i_distance = distances[st.stop_id]
-      pct = (i_distance - o_distance) / (c_distance - o_distance)
-      i_time = (c_time - o_time) * pct + o_time
-      debug("\ti_distance: #{i_distance} pct: #{pct} i_time: #{i_time}")
-      st.arrival_time = i_time
-      st.departure_time = i_time
-    end
-    debug("\tc_distance: #{c_distance} c_time: #{c_time}")
-    return true
-  end
-
-  def self.interpolate_gap_linear(stop_times)
-    debug("trip: #{stop_times.first.trip_id} interpolate_gap_linear: #{stop_times.first.stop_sequence} -> #{stop_times.last.stop_sequence}")
-    # open and close times
-    o_time = stop_times.first.departure_time
-    c_time = stop_times.last.arrival_time
-    # interpolate on time
-    p_time = o_time
-    debug("\tduration: #{c_time - o_time}")
-    debug("\ti: 0 o_time: #{o_time}")
-    stop_times[1...-1].each_with_index do |st,i|
-      pct = pct = (i+1) / (stop_times.size.to_f-1)
-      i_time = (c_time - o_time) * pct + o_time
-      debug("\ti: #{i+1} pct: #{pct} i_time: #{i_time} ")
-      st.arrival_time = i_time
-      st.departure_time = i_time
-    end
-    debug("\ti: #{stop_times.size-1} c_time: #{c_time}")
+  def create_shape_from_stop_pattern(stop_pattern)
+    q = "SELECT ST_Force3DM(ST_MakeLine(geometry::geometry)) AS geometry FROM (SELECT geometry FROM gtfs_stops INNER JOIN (SELECT unnest,ordinality FROM unnest( ARRAY[#{stop_pattern.join(',')}] ) WITH ORDINALITY) AS unnest ON gtfs_stops.id = unnest ORDER BY ordinality) AS q"    
+    geometry = ActiveRecord::Base.connection.exec_query(q).rows.first.first
+    GTFSShape.create!(shape_id: Random.rand, geometry: geometry, generated: true, feed_version: @feed_version)
   end
 end
