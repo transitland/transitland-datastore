a67c30182576dcf7f19e8b06f2a3c80b5d484784
Ian Rees <ian@ianrees.net>
2019-04-14 17:24:15 -0700

    Fix tests

--- db/migrate/20190411214421_add_type_to_feeds.rb
+++ db/migrate/20190411214421_add_type_to_feeds.rb
@@ -1,10 +1,10 @@
 class AddTypeToFeeds < ActiveRecord::Migration
   def change
-    add_column :current_feeds, :type, :string
-    add_column :old_feeds, :type, :string
-    add_column :current_feeds, :authorization, :hstore
-    add_column :old_feeds, :authorization, :hstore
-    add_column :current_feeds, :urls, :hstore
-    add_column :old_feeds, :urls, :hstore
+    add_column :current_feeds, :type, :string, index: true
+    add_column :old_feeds, :type, :string, index: true
+    add_column :current_feeds, :authorization, :hstore, index: true
+    add_column :old_feeds, :authorization, :hstore, index: true
+    add_column :current_feeds, :urls, :hstore, index: true
+    add_column :old_feeds, :urls, :hstore, index: true
   end
 end
